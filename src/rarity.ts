d203437e8172f72b052beab7b5393a7289597768
Viktor Kalinin <kalinin.viktor@gmail.com>
2023-12-05 00:02:29 +0200

    Add `Rarity` enum for the items

---
+++ src/rarity.ts
@@ -0,0 +1,8 @@
+enum Rarity {
+    Abundant = 'Abundant',
+    Common = 'Common',
+    Uncommon = 'Uncommon',
+    Rare = 'Rare',
+    VeryRare = 'Very Rare',
+    ExtremelyRare = 'Extremely Rare',
+}
