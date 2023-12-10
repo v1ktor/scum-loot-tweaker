508ed1bcb8632f8c393c4241c591ca55284a6f0e
Viktor Kalinin <kalinin.viktor@gmail.com>
2023-12-11 00:43:04 +0200

    Add 7.62x54mm Caliber Ammunition

---
+++ src/items/ammo/cal-762-54.ts
@@ -0,0 +1,37 @@
+import { Item } from "../item";
+
+export const AMMO_762_54_CALIBER: Item = {
+    id: 'Cal_7_62x54mmR',
+    name: '7.62x54mm Caliber',
+    picture: '',
+};
+
+export const AMMO_762_54_CALIBER_AMMO_BOX: Item = {
+    id: 'Cal_7_62x54mmR_Ammobox',
+    name: '7.62x54mm Caliber Ammo Box',
+    picture: '',
+};
+
+export const AMMO_762_54_CALIBER_AP: Item = {
+    id: 'Cal_7_62x54mmR_AP',
+    name: '7.62x54mm Caliber Armor Piercing',
+    picture: '',
+};
+
+export const AMMO_762_54_CALIBER_AP_AMMO_BOX: Item = {
+    id: 'Cal_7_62x54mmR_AP_Ammobox',
+    name: '7.62x54mm Caliber Armor Piercing Ammo Box',
+    picture: '',
+};
+
+export const AMMO_762_54_CALIBER_TR: Item = {
+    id: 'Cal_7_62x54mmR_TR',
+    name: '7.62x54mm Caliber Tracer',
+    picture: '',
+};
+
+export const AMMO_762_54_CALIBER_TR_AMMO_BOX: Item = {
+    id: 'Cal_7_62x54mmR_Ammobox_TR',
+    name: '7.62x54mm Caliber Tracer Ammo Box',
+    picture: '',
+}
