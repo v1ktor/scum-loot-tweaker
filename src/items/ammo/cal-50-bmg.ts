85bde79b3ab2c3c9fa8f672224f1f035c451bb22
Viktor Kalinin <kalinin.viktor@gmail.com>
2023-12-10 23:49:21 +0200

    Add 50 BMG Caliber Ammunition

---
+++ src/items/ammo/cal-50-bmg.ts
@@ -0,0 +1,37 @@
+import { Item } from "../item";
+
+export const AMMO_50_BMG_CALIBER: Item = {
+    id: 'Cal_50BMG',
+    name: '50 BMG Caliber',
+    picture: '',
+};
+
+export const AMMO_50_BMG_CALIBER_AMMO_BOX: Item = {
+    id: 'Cal_50BMG_Ammobox',
+    name: '50 BMG Caliber Ammo Box',
+    picture: '',
+};
+
+export const AMMO_50_BMG_CALIBER_AP: Item = {
+    id: 'Cal_50BMG_AP',
+    name: '50 BMG Caliber Armor Piercing',
+    picture: '',
+};
+
+export const AMMO_50_BMG_CALIBER_AP_AMMO_BOX: Item = {
+    id: 'Cal_50BMG_AP_Ammobox',
+    name: '50 BMG Caliber Armor Piercing Ammo Box',
+    picture: '',
+};
+
+export const AMMO_50_BMG_CALIBER_TR: Item = {
+    id: 'Cal_50BMG_TR',
+    name: '50 BMG Caliber Tracer',
+    picture: '',
+};
+
+export const AMMO_50_BMG_CALIBER_TR_AMMO_BOX: Item = {
+    id: 'Cal_50BMG_Ammobox_TR',
+    name: '50 BMG Caliber Tracer Ammo Box',
+    picture: '',
+}
