bf55e320e128688d59f1a254bed4dfd04f8e0d6e
Viktor Kalinin <kalinin.viktor@gmail.com>
2023-12-11 00:46:59 +0200

    Add 7.92x57mm Caliber Ammunition

---
+++ src/items/ammo/cal-792-57.ts
@@ -0,0 +1,37 @@
+import { Item } from "../item";
+
+export const AMMO_792_57_CALIBER: Item = {
+    id: 'Cal_7_92x57mm',
+    name: '7.92x57mm Caliber',
+    picture: '',
+};
+
+export const AMMO_792_57_CALIBER_AMMO_BOX: Item = {
+    id: 'Cal_7_92x57mm_Ammobox',
+    name: '7.92x57mm Caliber Ammo Box',
+    picture: '',
+}
+
+export const AMMO_792_57_CALIBER_AP: Item = {
+    id: 'Cal_7_92x57mm_AP',
+    name: '7.92x57mm Caliber Armor Piercing',
+    picture: '',
+};
+
+export const AMMO_792_57_CALIBER_AP_AMMO_BOX: Item = {
+    id: 'Cal_7_92x57mm_AP_Ammobox',
+    name: '7.92x57mm Caliber Armor Piercing Ammo Box',
+    picture: '',
+};
+
+export const AMMO_792_57_CALIBER_TR: Item = {
+    id: 'Cal_7_92x57mm_TR',
+    name: '7.92x57mm Caliber Tracer',
+    picture: '',
+};
+
+export const AMMO_792_57_CALIBER_TR_AMMO_BOX: Item = {
+    id: 'Cal_7_92x57mm_Ammobox_TR',
+    name: '7.92x57mm Caliber Tracer Ammo Box',
+    picture: '',
+}
