5c417f3680a89e6733a71500867bf68d16b681c4
Viktor Kalinin <kalinin.viktor@gmail.com>
2023-12-11 00:15:37 +0200

    Add 5.56x45mm Caliber Ammunition

---
+++ src/items/ammo/cal-556-45.ts
@@ -0,0 +1,37 @@
+import { Item } from "../item";
+
+export const AMMO_556_45_CALIBER: Item = {
+    id: 'Cal_5_56x45mm',
+    name: '5.56x45mm Caliber',
+    picture: '',
+}
+
+export const AMMO_556_45_CALIBER_AMMO_BOX: Item = {
+    id: 'Cal_5_56x45mm_Ammobox',
+    name: '5.56x45mm Caliber Ammo Box',
+    picture: '',
+}
+
+export const AMMO_556_45_CALIBER_AP: Item = {
+    id: 'Cal_5_56x45mm_AP',
+    name: '5.56x45mm Caliber Armor Piercing',
+    picture: '',
+}
+
+export const AMMO_556_45_CALIBER_AP_AMMO_BOX: Item = {
+    id: 'Cal_5_56x45mm_AP_Ammobox',
+    name: '5.56x45mm Caliber Armor Piercing Ammo Box',
+    picture: '',
+}
+
+export const AMMO_556_45_CALIBER_TR: Item = {
+    id: 'Cal_5_56x45mm_TR',
+    name: '5.56x45mm Caliber Tracer',
+    picture: '',
+}
+
+export const AMMO_556_45_CALIBER_TR_AMMO_BOX: Item = {
+    id: 'Cal_5_56x45mm_Ammobox_TR',
+    name: '5.56x45mm Caliber Tracer Ammo Box',
+    picture: '',
+}
