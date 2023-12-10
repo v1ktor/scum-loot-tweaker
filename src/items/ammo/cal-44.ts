2085c0b2c598a4eca1ae2eb6f23df9d9acd457fb
Viktor Kalinin <kalinin.viktor@gmail.com>
2023-12-10 23:33:58 +0200

    Add 44 Caliber ammo definitions and references

---
+++ src/items/ammo/cal-44.ts
@@ -0,0 +1,37 @@
+import { Item } from "../item";
+
+export const AMMO_44_CALIBER: Item = {
+    id: 'Cal_44_Viper',
+    name: '44 Caliber',
+    picture: '',
+};
+
+export const AMMO_44_CALIBER_AMMO_BOX: Item = {
+    id: 'Cal_44_Viper_Ammobox',
+    name: '44 Caliber Ammo Box',
+    picture: '',
+};
+
+export const AMMO_44_CALIBER_AP: Item = {
+    id: 'Cal_44_Viper_AP',
+    name: '44 Caliber Armor Piercing',
+    picture: '',
+};
+
+export const AMMO_44_CALIBER_AP_AMMO_BOX: Item = {
+    id: 'Cal_44_Viper_Ammobox_AP',
+    name: '44 Caliber Armor Piercing Ammo Box',
+    picture: '',
+};
+
+export const AMMO_44_CALIBER_TR: Item = {
+    id: 'Cal_44_Viper_TR',
+    name: '44 Caliber Tracer',
+    picture: '',
+};
+
+export const AMMO_44_CALIBER_TR_AMMO_BOX: Item = {
+    id: 'Cal_44_Viper_Ammobox_TR',
+    name: '44 Caliber Tracer Ammo Box',
+    picture: '',
+}
