9d5a1deee6b26d7202d6945ad93ca4e39ad5e637
Viktor Kalinin <kalinin.viktor@gmail.com>
2023-12-11 00:19:22 +0200

    Add 7.62x39mm Caliber Ammunition

---
+++ src/items/ammo/cal-762-39.ts
@@ -0,0 +1,38 @@
+import { Item } from "../item";
+
+export const AMMO_762_39_CALIBER: Item = {
+    id: 'Cal_7_62x39mm',
+    name: '7.62x39mm Caliber',
+    picture: '',
+};
+
+export const AMMO_762_39_CALIBER_AMMO_BOX: Item = {
+    id: 'Cal_7_62x39mm_Ammobox',
+    name: '7.62x39mm Caliber Ammo Box',
+    picture: '',
+};
+
+export const AMMO_762_39_CALIBER_AP: Item = {
+    id: 'Cal_7_62x39mm_AP',
+    name: '7.62x39mm Caliber Armor Piercing',
+    picture: '',
+};
+
+export const AMMO_762_39_CALIBER_AP_AMMO_BOX: Item = {
+    id: 'Cal_7_62x39mm_AP_Ammobox',
+    name: '7.62x39mm Caliber Armor Piercing Ammo Box',
+    picture: '',
+};
+
+export const AMMO_762_39_CALIBER_TR: Item = {
+    id: 'Cal_7_62x39mm_TR',
+    name: '7.62x39mm Caliber Tracer',
+    picture: '',
+};
+
+export const AMMO_762_39_CALIBER_TR_AMMO_BOX: Item = {
+    id: 'Cal_7_62x39mm_Ammobox_TR',
+    name: '7.62x39mm Caliber Tracer Ammo Box',
+    picture: '',
+}
+
