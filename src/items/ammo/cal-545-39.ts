ff2624977c88da832ca37839b7ccb65cdf15fbf7
Viktor Kalinin <kalinin.viktor@gmail.com>
2023-12-10 23:55:41 +0200

    Add 5.45x39mm Caliber Ammunition

---
+++ src/items/ammo/cal-545-39.ts
@@ -0,0 +1,37 @@
+import { Item } from "../item";
+
+export const AMMO_545_39_CALIBER: Item = {
+    id: 'Cal_5_45x39mm',
+    name: '5.45x39mm Caliber',
+    picture: '',
+};
+
+export const AMMO_545_39_CALIBER_AMMO_BOX: Item = {
+    id: 'Cal_5_45x39mm_Ammobox',
+    name: '5.45x39mm Caliber Ammo Box',
+    picture: '',
+};
+
+export const AMMO_545_39_CALIBER_AP: Item = {
+    id: 'Cal_5_45x39mm_AP',
+    name: '5.45x39mm Caliber Armor Piercing',
+    picture: '',
+};
+
+export const AMMO_545_39_CALIBER_AP_AMMO_BOX: Item = {
+    id: 'Cal_5_45x39mm_AP_Ammobox',
+    name: '5.45x39mm Caliber Armor Piercing Ammo Box',
+    picture: '',
+};
+
+export const AMMO_545_39_CALIBER_TR: Item = {
+    id: 'Cal_5_45x39mm_TR',
+    name: '5.45x39mm Caliber Tracer',
+    picture: '',
+};
+
+export const AMMO_545_39_CALIBER_TR_AMMO_BOX: Item = {
+    id: 'Cal_5_45x39mm_Ammobox_TR',
+    name: '5.45x39mm Caliber Tracer Ammo Box',
+    picture: '',
+}
