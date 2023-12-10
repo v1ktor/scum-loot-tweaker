983affb3944e49ace3c6cc6ccbf9cecab5f57cfa
Viktor Kalinin <kalinin.viktor@gmail.com>
2023-12-11 00:52:32 +0200

    Add 9mm Caliber Ammunition

---
+++ src/items/ammo/cal-9.ts
@@ -0,0 +1,37 @@
+import { Item } from "../item";
+
+export const AMMO_9_CALIBER: Item = {
+    id: 'Cal_9mm',
+    name: '9mm Caliber',
+    picture: '',
+}
+
+export const AMMO_9_CALIBER_AMMO_BOX: Item = {
+    id: 'Cal_9mm_Ammobox',
+    name: '9mm Caliber Ammo Box',
+    picture: '',
+}
+
+export const AMMO_9_CALIBER_AP: Item = {
+    id: 'Cal_9mm_AP',
+    name: '9mm Caliber Armor Piercing',
+    picture: '',
+}
+
+export const AMMO_9_CALIBER_AP_AMMO_BOX: Item = {
+    id: 'Cal_9mm_AP_Ammobox',
+    name: '9mm Caliber Armor Piercing Ammo Box',
+    picture: '',
+}
+
+export const AMMO_9_CALIBER_TR: Item = {
+    id: 'Cal_9mm_TR',
+    name: '9mm Caliber Tracer',
+    picture: '',
+}
+
+export const AMMO_9_CALIBER_TR_AMMO_BOX: Item = {
+    id: 'Cal_9mm_Ammobox_TR',
+    name: '9mm Caliber Tracer Ammo Box',
+    picture: '',
+}
