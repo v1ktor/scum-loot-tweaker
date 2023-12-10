ee0fa0627126a9f064f1492ecc75f21a339a5ece
Viktor Kalinin <kalinin.viktor@gmail.com>
2023-12-10 23:43:14 +0200

    Add 50 AE Caliber Ammunition

---
+++ src/items/ammo/cal-50-ae.ts
@@ -0,0 +1,37 @@
+import { Item } from "../item";
+
+export const AMMO_50_AE_CALIBER: Item = {
+    id: 'Cal_50_AE',
+    name: '50 AE Caliber',
+    picture: '',
+};
+
+export const AMMO_50_AE_CALIBER_AMMO_BOX: Item = {
+    id: 'Cal_50_AE_Ammobox',
+    name: '50 AE Caliber Ammo Box',
+    picture: '',
+};
+
+export const AMMO_50_AE_CALIBER_AP: Item = {
+    id: 'Cal_50_AE_AP',
+    name: '50 AE Caliber Armor Piercing',
+    picture: '',
+};
+
+export const AMMO_50_AE_CALIBER_AP_AMMO_BOX: Item = {
+    id: 'Cal_50_AE_AP_Ammobox',
+    name: '50 AE Caliber Armor Piercing Ammo Box',
+    picture: '',
+};
+
+export const AMMO_50_AE_CALIBER_TR: Item = {
+    id: 'Cal_50_AE_TR',
+    name: '50 AE Caliber Tracer',
+    picture: '',
+};
+
+export const AMMO_50_AE_CALIBER_TR_AMMO_BOX: Item = {
+    id: 'Cal_50_AE_Ammobox_TR',
+    name: '50 AE Caliber Tracer Ammo Box',
+    picture: '',
+}
