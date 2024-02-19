9871cf29a1b48f3dbd3ac0df3e786e6fb714ff43
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-02-20 00:21:16 +0200

    Copy game `.json` files when building application

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/scum-loot-tweaker/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'src/data/spawners/*.json', dest: 'data/spawners/' },
+        { src: 'src/data/nodes/*.json', dest: 'data/nodes/' },
+        { src: 'src/data/parameters/*.json', dest: 'data/parameters/' },
+      ]
+    })
+  ],
 })
