ff06b3634eca0096c5a084ebaf2651fbd89dbc00
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-02-20 19:34:33 +0200

    Display items, fixed items and nodes from the spawner json file

--- src/pages/spawners/Spawners.tsx
+++ src/pages/spawners/Spawners.tsx
@@ -1,6 +1,6 @@
 import Select, { GroupBase, SingleValue, StylesConfig } from "react-select";
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
-import { useState } from "react";
+import React, { useState } from "react";
 import { Option, Spawner } from "./Spawners.types.ts";
 import { config } from "../../config.ts";
 
@@ -10,7 +10,8 @@ const options: Option[] = [
     value: 'Buildings-Store-Hunting_Store-Examine_CashRegister.json',
     label: 'Buildings-Store-Hunting_Store-Examine_CashRegister.json'
   },
-  { value: 'Buildings-Livingroom-Examine_Vine_Cabinet.json', label: 'Buildings-Livingroom-Examine_Vine_Cabinet.json' }
+  { value: 'Buildings-Livingroom-Examine_Vine_Cabinet.json', label: 'Buildings-Livingroom-Examine_Vine_Cabinet.json' },
+  { value: 'Water-Shipwrecks-World_Shelf.json', label: 'Water-Shipwrecks-World_Shelf.json' }
 ]
 
 export function Spawners() {
@@ -126,64 +127,81 @@ export function Spawners() {
 
       <span>
         {selectedOption &&
-          <Tabs>
-            <TabList>
-              <Tab>Items</Tab>
-              <Tab>Fixed Items</Tab>
-              <Tab>Nodes</Tab>
-              <Tab>Settings</Tab>
-            </TabList>
+          <>
+            <Tabs>
+              <TabList>
+                <Tab>Items</Tab>
+                <Tab>Fixed Items</Tab>
+                <Tab>Nodes</Tab>
+                <Tab>Settings</Tab>
+              </TabList>
 
-            <TabPanel>
-              <p>here will be a form for adding items and probability</p>
-            </TabPanel>
-            <TabPanel>
-              <p>here will be a form for adding fixed items</p>
-            </TabPanel>
-            <TabPanel>
-              <p>here will be a form for adding pre-made nodes</p>
-            </TabPanel>
-            <TabPanel>
-              <div>
-                <label htmlFor="probability">Probability:</label>
-                <input type="text" id="probability" name="probability" defaultValue={jsonData?.Probability}/>
-              </div>
-              <div>
-                <label htmlFor="quantity-min">Quantity Min:</label>
-                <input type="text" id="quantity-min" name="quantity-min" defaultValue={jsonData?.QuantityMin}/>
-              </div>
-              <div>
-                <label htmlFor="quantity-max">Quantity Max:</label>
-                <input type="text" id="quantity-max" name="quantity-max" defaultValue={jsonData?.QuantityMax}/>
-              </div>
-              <div>
-                <label htmlFor="allow-duplicates">Allow duplicates:</label>
-                <input type="text" id="allow-duplicates" name="allow-duplicates"
-                       defaultValue={String(jsonData?.AllowDuplicates)}/>
-              </div>
-              <div>
-                <label htmlFor="should-filter-items-by-zone">Should filter items by zone:</label>
-                <input type="text" id="should-filter-items-by-zone" name="should-filter-items-by-zone"
-                       defaultValue={String(jsonData?.ShouldFilterItemsByZone)}/>
-              </div>
-              <div>
-                <label htmlFor="initial-damage">Initial damage:</label>
-                <input type="text" id="initial-damage" name="initial-damage" defaultValue={jsonData?.InitialDamage}/>
-              </div>
-              <div>
-                <label htmlFor="random-damage">Random damage:</label>
-                <input type="text" id="random-damage" name="random-damage" defaultValue={jsonData?.RandomDamage}/>
-              </div>
-              <div>
-                <label htmlFor="initial-usage">Initial usage:</label>
-                <input type="text" id="initial-usage" name="initial-usage" defaultValue={jsonData?.InitialUsage}/>
-              </div>
-              <div>
-                <label htmlFor="random-usage">Random usage:</label>
-                <input type="text" id="random-usage" name="random-usage" defaultValue={jsonData?.RandomDamage}/>
-              </div>
-            </TabPanel>
-          </Tabs>
+              <TabPanel>
+                {jsonData && jsonData.Items && jsonData.Items.map((item) => (
+                  <p key={item.Id}>Id: {item.Id} - Rarity: {item.Rarity}</p>
+                ))}
+                <p>here will be a form for adding items and probability</p>
+              </TabPanel>
+              <TabPanel>
+                {jsonData && jsonData.FixedItems && jsonData.FixedItems.map((item) => (
+                  <p key={item}>{item}</p>
+                ))}
+                <p>here will be a form for adding fixed items</p>
+              </TabPanel>
+              <TabPanel>
+                {jsonData && jsonData.Nodes && jsonData.Nodes.map((node, key) => (
+                  <React.Fragment key="node-wrapper">
+                    <p key={key}>Rarity: {node.Rarity}</p>
+                    {node.Ids.map((id) => (
+                      <p key={id}>{id}</p>
+                    ))}
+                  </React.Fragment>
+                ))}
+                <p>here will be a form for adding pre-made nodes</p>
+              </TabPanel>
+              <TabPanel>
+                <div>
+                  <label htmlFor="probability">Probability:</label>
+                  <input type="text" id="probability" name="probability" defaultValue={jsonData?.Probability}/>
+                </div>
+                <div>
+                  <label htmlFor="quantity-min">Quantity Min:</label>
+                  <input type="text" id="quantity-min" name="quantity-min" defaultValue={jsonData?.QuantityMin}/>
+                </div>
+                <div>
+                  <label htmlFor="quantity-max">Quantity Max:</label>
+                  <input type="text" id="quantity-max" name="quantity-max" defaultValue={jsonData?.QuantityMax}/>
+                </div>
+                <div>
+                  <label htmlFor="allow-duplicates">Allow duplicates:</label>
+                  <input type="text" id="allow-duplicates" name="allow-duplicates"
+                         defaultValue={String(jsonData?.AllowDuplicates)}/>
+                </div>
+                <div>
+                  <label htmlFor="should-filter-items-by-zone">Should filter items by zone:</label>
+                  <input type="text" id="should-filter-items-by-zone" name="should-filter-items-by-zone"
+                         defaultValue={String(jsonData?.ShouldFilterItemsByZone)}/>
+                </div>
+                <div>
+                  <label htmlFor="initial-damage">Initial damage:</label>
+                  <input type="text" id="initial-damage" name="initial-damage" defaultValue={jsonData?.InitialDamage}/>
+                </div>
+                <div>
+                  <label htmlFor="random-damage">Random damage:</label>
+                  <input type="text" id="random-damage" name="random-damage" defaultValue={jsonData?.RandomDamage}/>
+                </div>
+                <div>
+                  <label htmlFor="initial-usage">Initial usage:</label>
+                  <input type="text" id="initial-usage" name="initial-usage" defaultValue={jsonData?.InitialUsage}/>
+                </div>
+                <div>
+                  <label htmlFor="random-usage">Random usage:</label>
+                  <input type="text" id="random-usage" name="random-usage" defaultValue={jsonData?.RandomDamage}/>
+                </div>
+              </TabPanel>
+            </Tabs>
+            <a href="#" className="button text-weight-800" style={{marginTop: 32, display: "block"}}>Download</a>
+          </>
         }
       </span>
     </main>
