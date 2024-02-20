18cec09857b410fe8680475b4b0898304e182268
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-02-21 00:16:18 +0200

    Add discord link to the footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <footer className="primary-footer content-grid">
       <div className="primary-footer__layout breakout">
-        &copy; {new Date().getFullYear()}
+        &copy; {new Date().getFullYear()} <a href="https://discord.gg/8T6q6Xf945" style={{marginLeft: 24}} target="_blank">Discord</a>
       </div>
     </footer>
   )
