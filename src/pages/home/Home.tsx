92fe4ef8b13cbae177b522c38fd8116c2a3b971b
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-02-21 23:51:28 +0200

    Add upcoming features settings on the home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,8 +3,18 @@ export function Home() {
     <main className="flow content-grid">
 
       <h1 className='site-title'>Welcome to SCUM Loot Tweaker</h1>
-      <p>Here will be probably some explanation on how to export .json files from the game and how to customize
-        them</p>
+      <div className="alert" role="alert">
+        Upcoming features:
+        <ul>
+          <li>Adding & removing items to/from the spawners</li>
+          <li>Adding & removing fixed items to/from the spawners</li>
+          <li>Adding & removing nodes to/from the spawners</li>
+          <li>Copying settings, items and nodes between spawners (e.g. customize one puppet spawner and copy those settings to other puppet spawners)</li>
+          <li>Customizing existing nodes and your own</li>
+          <li>Customizing parameters (allowing certain items to be spawned in particular locations, tuning spawn cooldowns, etc)</li>
+          <li>Allowing to customize spawners for specific zones or sectors (e.g. selecting a region on the map and allowing to spawn only guns in that region)</li>
+        </ul>
+      </div>
 
     </main>
   );
