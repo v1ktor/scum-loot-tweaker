92f6beb5e3f412d95649cc135895b458b6a08d1a
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-02-24 20:15:33 +0200

    Add Google Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,11 @@ import { Parameters } from "./pages/parameters/Parameters.tsx";
 import { Nodes } from "./pages/nodes/Nodes.tsx";
 import { Header } from "./components/header/Header.tsx";
 import { Footer } from "./components/footer/Footer.tsx";
+import ReactGA from "react-ga4";
 
 export function App() {
+  ReactGA.initialize("G-81R7K0PWNF");
+
   return (
     <>
       <Header/>
