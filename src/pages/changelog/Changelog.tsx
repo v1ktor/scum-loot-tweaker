bf43f0bfa1b9de14a27dc04d72984645939c24d7
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-02-27 00:05:04 +0200

    Update changelog

--- src/pages/changelog/Changelog.tsx
+++ src/pages/changelog/Changelog.tsx
@@ -1,14 +1,25 @@
 import { Alert } from "../../components/alert/Alert.tsx";
 
 export function Changelog() {
-  const currentVersion = '0.0.1';
+  const currentVersion = '0.0.2';
 
   return (
     <main className="flow content-grid">
 
       <h1 className='site-title'>Changelog</h1>
+      <Alert children={`Current version: ${currentVersion}`}></Alert>
       <div>
-        <Alert children={`Current version: ${currentVersion}`}></Alert>
+        <h2 style={{ display: 'inline' }}>0.0.2</h2> - 27.02.2024
+        <ul>
+          <li>Display spawner items and their rarity.</li>
+          <li>It is now possible to edit existing items rarity.</li>
+          <li>It is now possible to remove items from the spawner.</li>
+          <li>It is now possible to add items to the spawner. Currently, only .50 BMG ammo is available. More items will
+            be added soon!
+          </li>
+        </ul>
+      </div>
+      <div>
         <h2 style={{ display: 'inline' }}>0.0.1</h2> - 26.02.2024
         <ul>
           <li>Initial release.</li>
