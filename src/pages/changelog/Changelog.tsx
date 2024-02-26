68e2f0d1106fb046b2d556bef9100eeb3965e2f3
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-02-26 02:46:19 +0200

    Update changelog

--- src/pages/changelog/Changelog.tsx
+++ src/pages/changelog/Changelog.tsx
@@ -11,13 +11,17 @@ export function Changelog() {
         <Alert children={`Current version: ${currentVersion}`}></Alert>
         <h2 style={{ display: 'inline' }}>0.0.1</h2> - 26.02.2024
         <ul>
-          <li>Initial release</li>
-          <li>Added spawner presets from the game</li>
-          <li>Now it's possible to inspect spawner Items, Fixed Items, Nodes, Subpresets and Settings</li>
-          <li>Now it's possible to modify existing spawner settings. Added tooltips for some settings</li>
-          <li>Now it's possible to remove fixed items from the spawner</li>
-          <li>Now it's possible to add fixed items to the spawner. Currently only .50 BMG ammo. More items to come soon!</li>
-          <li>Added nodes from the game. At the moment is displayed in JSON format. Pretty print with ability to modify will come soon!</li>
+          <li>Initial release.</li>
+          <li>Added spawner presets from the game.</li>
+          <li>It is now possible to inspect spawner Items, Fixed Items, Nodes, Subpresets, and Settings.</li>
+          <li>It is now possible to modify existing spawner settings. Added tooltips for some settings.</li>
+          <li>It is now possible to remove fixed items from the spawner.</li>
+          <li>It is now possible to add fixed items to the spawner. Currently, only .50 BMG ammo is available. More
+            items will be added soon!
+          </li>
+          <li>Added nodes from the game. Currently displayed in JSON format. Pretty printing with the ability to modify
+            will be available soon!
+          </li>
         </ul>
       </div>
 
