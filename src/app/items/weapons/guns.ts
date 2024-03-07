03c848bb4765fca2cfa1d24c97c669ea55cf2777
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-07 22:26:13 +0200

    Add guns to the items dropdown

---
+++ src/app/items/weapons/guns.ts
@@ -0,0 +1,385 @@
+import { Item } from "../item.ts";
+
+export const WEAPONS_590A11: Item = {
+  id: 'Weapon_590A11',
+  name: 'TEC01 490',
+  picture: '',
+} as const
+
+export const WEAPONS_98K_KARABINER: Item = {
+    id: 'Weapon_98k_Karabiner',
+    name: 'Kar98k',
+    picture: '',
+}
+
+export const WEAPONS_AK15: Item = {
+    id: 'Weapon_AK15',
+    name: 'AK-15',
+    picture: '',
+}
+
+export const WEAPONS_AK47: Item = {
+  id: 'Weapon_AK47',
+  name: 'AK-47',
+  picture: ''
+} as const;
+
+export const WEAPONS_AK47_ENGRAVED: Item = {
+  id: 'Weapon_AK47_Engraved',
+  name: 'AK-47 Engraved',
+  picture: ''
+} as const;
+
+export const WEAPONS_AKM: Item = {
+  id: 'Weapon_AKM',
+  name: 'AKM',
+  picture: ''
+} as const;
+
+export const WEAPONS_AKS_74U: Item = {
+  id: 'Weapon_AKS_74U',
+  name: 'AKS-74U',
+  picture: ''
+} as const;
+
+export const WEAPONS_AS_VAL: Item = {
+  id: 'Weapon_AS_Val',
+  name: 'AS Val',
+  picture: ''
+} as const;
+
+export const WEAPONS_AT4_HEAT: Item = {
+  id: 'Weapon_AT4_HEAT',
+  name: 'AT4 HEAT',
+  picture: ''
+} as const;
+
+export const WEAPONS_AWM: Item = {
+  id: 'Weapon_AWM',
+  name: 'AWM',
+  picture: ''
+} as const;
+
+export const WEAPONS_AWM_DESERT: Item = {
+  id: 'Weapon_AWM_Desert',
+  name: 'AWM Desert',
+  picture: ''
+} as const;
+
+export const WEAPONS_AWM_GREEN: Item = {
+  id: 'Weapon_AWM_Green',
+  name: 'AWM Green',
+  picture: ''
+} as const;
+
+export const WEAPONS_AWM_SNAKE: Item = {
+  id: 'Weapon_AWM_Snake',
+  name: 'AWM Snake',
+  picture: ''
+} as const;
+
+export const WEAPONS_AWP: Item = {
+  id: 'Weapon_AWP',
+  name: 'AWP',
+  picture: ''
+} as const;
+
+export const WEAPONS_AWP_DESERT: Item = {
+  id: 'Weapon_AWP_Desert',
+  name: 'AWP Desert',
+  picture: ''
+} as const;
+
+export const WEAPONS_AWP_GREEN: Item = {
+  id: 'Weapon_AWP_Green',
+  name: 'AWP Green',
+  picture: ''
+} as const;
+
+export const WEAPONS_AWP_SNAKE: Item = {
+  id: 'Weapon_AWP_Snake',
+  name: 'AWP Snake',
+  picture: ''
+} as const;
+
+export const WEAPONS_BLACKHAWK_CROSSBOW: Item = {
+  id: 'Weapon_BlackHawk_Crossbow',
+  name: 'Black Hawk Crossbow',
+  picture: ''
+} as const;
+
+export const WEAPONS_BLOCK21: Item = {
+  id: 'Weapon_Block21',
+  name: 'Block 21',
+  picture: ''
+} as const;
+
+export const WEAPONS_CARBONHUNTER: Item = {
+  id: 'Weapon_CarbonHunter',
+  name: 'Carbon Hunter',
+  picture: ''
+} as const;
+
+export const WEAPONS_DEAGLE_50: Item = {
+  id: 'Weapon_Deagle_50',
+  name: 'Desert Eagle .50',
+  picture: ''
+} as const;
+
+export const WEAPONS_DEAGLE_357: Item = {
+  id: 'Weapon_Deagle_357',
+  name: 'Desert Eagle .357',
+  picture: ''
+} as const;
+
+export const WEAPONS_DEAGLE_50GOLD: Item = {
+  id: 'Weapon_DEagle_50Gold',
+  name: 'Desert Eagle .50 Gold',
+  picture: ''
+} as const;
+
+export const WEAPONS_DT11B: Item = {
+  id: 'Weapon_DT11B',
+  name: 'DT11B',
+  picture: ''
+} as const;
+
+export const WEAPONS_DT11B_SAWED_OFF: Item = {
+  id: 'Weapon_DT11B_Sawed_Off',
+  name: 'DT11B Sawed Off',
+  picture: ''
+} as const;
+
+export const WEAPONS_FLAREGUN: Item = {
+  id: 'Weapon_FlareGun',
+  name: 'Flare Gun',
+  picture: ''
+} as const;
+
+export const WEAPONS_HS9: Item = {
+  id: 'Weapon_HS9',
+  name: 'HS9',
+  picture: ''
+} as const;
+
+export const WEAPONS_HUNTER85_V2: Item = {
+  id: 'Weapon_Hunter85_V2',
+  name: 'Hunter 85',
+  picture: ''
+} as const;
+
+export const WEAPONS_JUDGE44: Item = {
+  id: 'Weapon_Judge44',
+  name: 'Judge .44',
+  picture: ''
+} as const;
+
+export const WEAPONS_KRUEGER: Item = {
+  id: 'Weapon_Krueger',
+  name: 'Krueger',
+  picture: ''
+} as const;
+
+export const WEAPONS_M16A4: Item = {
+  id: 'Weapon_M16A4',
+  name: 'M16A4',
+  picture: ''
+} as const;
+
+export const WEAPONS_M1887: Item = {
+  id: 'Weapon_M1887',
+  name: 'M1887',
+  picture: ''
+} as const;
+
+export const WEAPONS_M1887_SAWED_OFF: Item = {
+  id: 'Weapon_M1887_Sawed_off',
+  name: 'M1887 Sawed Off',
+  picture: ''
+} as const;
+
+export const WEAPONS_M1911: Item = {
+  id: 'Weapon_M1911',
+  name: 'M1911',
+  picture: ''
+} as const;
+
+export const WEAPONS_M1911_ENGRAVED: Item = {
+  id: 'Weapon_M1911_Engraved',
+  name: 'M1911 Engraved',
+  picture: ''
+} as const;
+
+export const WEAPONS_M1911_GOLD: Item = {
+  id: 'Weapon_M1911_Gold',
+  name: 'M1911 Gold',
+  picture: ''
+} as const;
+
+export const WEAPONS_M1911_JAYW: Item = {
+  id: 'Weapon_M1911_JayW',
+  name: 'M1911 JayW',
+  picture: ''
+} as const;
+
+export const WEAPONS_M1_GARAND: Item = {
+  id: 'Weapon_M1_Garand',
+  name: 'M1 Garand',
+  picture: ''
+} as const;
+
+export const WEAPONS_M249: Item = {
+  id: 'Weapon_M249',
+  name: 'M249',
+  picture: ''
+} as const;
+
+export const WEAPONS_M82A1: Item = {
+  id: 'Weapon_M82A1',
+  name: 'M82A1',
+  picture: ''
+} as const;
+
+export const WEAPONS_M82A1_BLACK: Item = {
+  id: 'Weapon_M82A1',
+  name: 'M82A1 Black',
+  picture: ''
+} as const;
+
+export const WEAPONS_M82A1_DESERT: Item = {
+  id: 'Weapon_M82A1',
+  name: 'M82A1 Desert',
+  picture: ''
+} as const;
+
+export const WEAPONS_M82A1_SNOW: Item = {
+  id: 'Weapon_M82A1',
+  name: 'M82A1 Snow',
+  picture: ''
+} as const;
+
+export const WEAPONS_M9: Item = {
+  id: 'Weapon_M9',
+  name: 'M9',
+  picture: ''
+} as const;
+
+export const WEAPONS_M9_SILVER: Item = {
+  id: 'Weapon_M9_Silver',
+  name: 'M9 Silver',
+  picture: ''
+} as const;
+
+export const WEAPONS_MK18: Item = {
+  id: 'Weapon_MK18',
+  name: 'MK18',
+  picture: ''
+} as const;
+
+export const WEAPONS_MOSINNAGANT: Item = {
+  id: 'Weapon_MosinNagant',
+  name: 'Mosin Nagant',
+  picture: ''
+} as const;
+
+export const WEAPONS_MP5: Item = {
+  id: 'Weapon_MP5',
+  name: 'MP5',
+  picture: ''
+} as const;
+
+export const WEAPONS_MP5_K: Item = {
+  id: 'Weapon_MP5_K',
+  name: 'MP5 K',
+  picture: ''
+} as const;
+
+export const WEAPONS_MP5_SD: Item = {
+  id: 'Weapon_MP5_SD',
+  name: 'MP5 SD',
+  picture: ''
+} as const;
+
+export const WEAPONS_PEACEKEEPER38: Item = {
+  id: 'Weapon_PeaceKeeper38',
+  name: 'Peace Keeper .38',
+  picture: ''
+} as const;
+
+export const WEAPONS_RPG7: Item = {
+  id: 'Weapon_RPG7',
+  name: 'RPG7',
+  picture: ''
+} as const;
+
+export const WEAPONS_RPK_74: Item = {
+  id: 'Weapon_RPK-74',
+  name: 'RPK-74',
+  picture: ''
+} as const;
+
+export const WEAPONS_SCAR_DMR: Item = {
+  id: 'Weapon_SCAR_DMR',
+  name: 'SCAR DMR',
+  picture: ''
+} as const;
+
+export const WEAPONS_SDASS: Item = {
+  id: 'Weapon_SDASS',
+  name: 'SDASS',
+  picture: ''
+} as const;
+
+export const WEAPONS_SERPENT357: Item = {
+  id: 'Weapon_Serpent357',
+  name: 'Serpent .357',
+  picture: ''
+} as const;
+
+export const WEAPONS_SF19: Item = {
+  id: 'Weapon_SF19',
+  name: 'SF19',
+  picture: ''
+} as const;
+
+export const WEAPONS_SVD_DRAGUNOV: Item = {
+  id: 'Weapon_SVD_Dragunov',
+  name: 'SVD Dragunov',
+  picture: ''
+} as const;
+
+export const WEAPONS_UMP45: Item = {
+  id: 'Weapon_UMP45',
+  name: 'UMP45',
+  picture: ''
+} as const;
+
+export const WEAPONS_VHS2: Item = {
+  id: 'Weapon_VHS2',
+  name: 'VHS2',
+  picture: ''
+} as const;
+
+export const WEAPONS_VHS2_RAIL: Item = {
+  id: 'Weapon_VHS2_Rail',
+  name: 'VHS2 Rail',
+  picture: ''
+} as const;
+
+export const WEAPONS_VHS_BG: Item = {
+  id: 'Weapon_VHS_BG',
+  name: 'VHS BG',
+  picture: ''
+} as const;
+
+export const WEAPONS_VIPER_M357: Item = {
+  id: 'Weapon_Viper_M357',
+  name: 'Viper M357',
+  picture: ''
+} as const;
+
+export const WEAPONS_VSS_VZ: Item = {
+  id: 'Weapon_VSS_VZ',
+  name: 'VSS VZ',
+  picture: ''
+} as const;
