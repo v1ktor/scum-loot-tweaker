e9b8508bc646209338539ef65e9e550e604ca754
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-07 22:36:03 +0200

    Add bows to the items dropdown

---
+++ src/app/items/weapons/bows.ts
@@ -0,0 +1,79 @@
+import { Item } from "../item.ts";
+
+export const WEAPONS_COMPOUND_BOW: Item = {
+  id: 'Compound_Bow',
+  name: 'Compound Bow',
+  picture: '',
+} as const
+
+export const WEAPONS_COMPOUND_BOW_DESERT: Item = {
+  id: 'Compound_Bow_Desert',
+  name: 'Compound Bow Desert',
+  picture: '',
+} as const
+
+export const WEAPONS_COMPOUND_BOW_DIGITAL: Item = {
+  id: 'Compound_Bow_Digital',
+  name: 'Compound Bow Digital',
+  picture: '',
+} as const
+
+export const WEAPONS_COMPOUND_BOW_SNOW: Item = {
+  id: 'Compound_Bow_Snow',
+  name: 'Compound Bow Snow',
+  picture: '',
+} as const
+
+export const WEAPONS_COMPOUND_BOW_US_SCORPION: Item = {
+  id: 'Compound_Bow_US_Scorpion',
+  name: 'Compound Bow US Scorpion',
+  picture: '',
+} as const
+
+export const WEAPONS_COMPOUND_BOW_WOODLAND: Item = {
+  id: 'Compound_Bow_Woodland',
+  name: 'Compound Bow Woodland',
+  picture: '',
+} as const
+
+export const WEAPONS_RECURVE_BOW: Item = {
+  id: 'Recurve_Bow',
+  name: 'Recurve Bow',
+  picture: '',
+} as const
+
+export const WEAPONS_RECURVE_BOW_50: Item = {
+  id: 'Recurve_Bow_50',
+  name: 'Recurve Bow #50',
+  picture: '',
+} as const
+
+export const WEAPONS_RECURVE_BOW_60: Item = {
+  id: 'Recurve_Bow_60',
+  name: 'Recurve Bow #60',
+  picture: '',
+} as const
+
+export const WEAPONS_RECURVE_BOW_70: Item = {
+  id: 'Recurve_Bow_70',
+  name: 'Recurve Bow #70',
+  picture: '',
+} as const
+
+export const WEAPONS_RECURVE_BOW_80: Item = {
+  id: 'Recurve_Bow_80',
+  name: 'Recurve Bow #80',
+  picture: '',
+} as const
+
+export const WEAPONS_RECURVE_BOW_90: Item = {
+  id: 'Recurve_Bow_90',
+  name: 'Recurve Bow #90',
+  picture: '',
+} as const
+
+export const WEAPONS_RECURVE_BOW_100: Item = {
+  id: 'Recurve_Bow_100',
+  name: 'Recurve Bow #100',
+  picture: '',
+} as const
