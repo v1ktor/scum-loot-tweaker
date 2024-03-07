d0daa03b89719f35e814e69cda80e103c0ebbaa7
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-07 23:19:55 +0200

    Add gun magazines to the items dropdown

---
+++ src/app/items/weapons/magazines.ts
@@ -0,0 +1,157 @@
+import { Item } from "../item.ts";
+
+export const MAGAZINE_AK15: Item = {
+  id: 'Magazine_AK15',
+  name: 'AK-15 Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_AK47: Item = {
+  id: 'Magazine_AK47',
+  name: 'AK-47 Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_AKS_74U: Item = {
+  id: 'Magazine_AKS_74U',
+  name: 'AKS-74U Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_AS_VAL: Item = {
+  id: 'Magazine_AS_Val',
+  name: 'AS Val Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_AWM: Item = {
+  id: 'Magazine_AWM',
+  name: 'AWM Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_AWP: Item = {
+  id: 'Magazine_AWP',
+  name: 'AWP Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_BLOCK21: Item = {
+  id: 'Magazine_Block21',
+  name: 'Block21 Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_CARBON_HUNTER: Item = {
+  id: 'Magazine_CarbonHunter',
+  name: 'Carbon Hunter Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_DEAGLE_50: Item = {
+  id: 'Magazine_DEagle_50',
+  name: 'Desert Eagle .50 Magazine ',
+  picture: ''
+} as const;
+
+export const MAGAZINE_DEAGLE_357: Item = {
+  id: 'Magazine_DEagle_357',
+  name: 'Desert Eagle .357 Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_HS9: Item = {
+  id: 'Magazine_HS9',
+  name: 'HS9 Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_HUNTER85_V2: Item = {
+  id: 'Magazine_Hunter85_V2',
+  name: 'Hunter85 Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_KRUEGER: Item = {
+  id: 'Magazine_Krueger',
+  name: 'Krueger Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_M16: Item = {
+  id: 'Magazine_M16',
+  name: 'M16 Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_M1911: Item = {
+  id: 'Magazine_M1911',
+  name: 'M1911 Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_M249: Item = {
+  id: 'Magazine_M249',
+  name: 'M249 Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_M82A1: Item = {
+  id: 'Magazine_M82A1',
+  name: 'M82A1 Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_M9: Item = {
+  id: 'Magazine_M9',
+  name: 'M9 Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_MP5: Item = {
+  id: 'Magazine_MP5',
+  name: 'MP5 Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_RPK: Item = {
+  id: 'Magazine_RPK',
+  name: 'RPK Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_SCAR_DMR: Item = {
+  id: 'Magazine_SCAR_DMR',
+  name: 'SCAR DMR Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_SF19: Item = {
+  id: 'Magazine_SF19',
+  name: 'SF19 Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_SVD_DRAGUNOV: Item = {
+  id: 'Magazine_SVD_Dragunov',
+  name: 'SVD Dragunov Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_UMP45: Item = {
+  id: 'Magazine_UMP45',
+  name: 'UMP45 Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_VHS2: Item = {
+  id: 'Magazine_VHS2',
+  name: 'VHS2 Magazine',
+  picture: ''
+} as const;
+
+export const MAGAZINE_M1: Item = {
+  id: 'M1_Clip',
+  name: 'M1 Clip',
+  picture: ''
+} as const;
