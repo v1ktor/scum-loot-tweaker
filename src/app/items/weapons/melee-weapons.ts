783b5e1a232138fbf7defead80b71dafc9c2f4fc
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-07 15:15:25 +0200

    Add melee weapons

---
+++ src/app/items/weapons/melee-weapons.ts
@@ -0,0 +1,283 @@
+import { Item } from "../item.ts";
+
+export const WEAPONS_1H_BEAM_DILDO: Item = {
+  id: '1H_Beam_Dildo',
+  name: 'Beam Dildo',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_BRASS_KNUCKLES: Item = {
+  id: '1H_Brass_Knuckles',
+  name: 'Brass Knuckles',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_BUSHMAN: Item = {
+  id: '1H_Bushman',
+  name: 'Bushman',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_CLEAVER: Item = {
+  id: '1H_Cleaver',
+  name: 'Cleaver',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_CROWBAR: Item = {
+  id: '1H_Crowbar',
+  name: 'Crowbar',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_DILDO: Item = {
+  id: '1H_Dildo',
+  name: 'Dildo',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_EMPOSSIBLE_KNIFE: Item = {
+  id: '1H_EMpossible_Knife',
+  name: 'EMpossible Knife',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_HATCHET: Item = {
+  id: '1H_Hatchet',
+  name: 'Hatchet',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_HUNTER: Item = {
+  id: '1H_Hunter',
+  name: 'Hunter',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_KITCHEN_KNIFE: Item = {
+  id: '1H_KitchenKnife',
+  name: 'Kitchen Knife',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_KITCHEN_KNIFE_02: Item = {
+  id: '1H_KitchenKnife_02',
+  name: 'Kitchen Knife 02',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_KITCHEN_KNIFE_03: Item = {
+  id: '1H_KitchenKnife_03',
+  name: 'Kitchen Knife 03',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_KITCHEN_KNIFE_04: Item = {
+  id: '1H_KitchenKnife_04',
+  name: 'Kitchen Knife 04',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_Kunai: Item = {
+  id: '1H_Kunai',
+  name: 'Kunai',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_LITTLE_SPADE: Item = {
+  id: '1H_Little_Spade',
+  name: 'Little Spade',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_MEDIEVAL_SWORD: Item = {
+  id: '1H_Medieval_Sword',
+  name: 'Medieval Sword',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_METAL_PIPE: Item = {
+  id: '1H_Metal_Pipe',
+  name: 'Metal Pipe',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_METAL_SWORD: Item = {
+  id: '1H_Metal_Sword',
+  name: 'Metal Sword',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_MK5000_BLACK: Item = {
+  id: '1H_MK5000_Black',
+  name: 'MK5000 Black',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_MK5000_METAL: Item = {
+  id: '1H_MK5000_Metal',
+  name: 'MK5000 Metal',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_N9_BLACK: Item = {
+  id: '1H_N9_Black',
+  name: 'N9 Black',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_N9_WHITE: Item = {
+  id: '1H_N9_White',
+  name: 'N9 White',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_POLICE_BATON: Item = {
+  id: '1H_Police_Baton',
+  name: 'Police Baton',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_SCALPEL: Item = {
+  id: '1H_Scalpel',
+  name: 'Scalpel',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_SCOUT_BLACK: Item = {
+  id: '1H_Scout_Black',
+  name: 'Scout Black',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_SCOUT_WHITE: Item = {
+  id: '1H_Scout_White',
+  name: 'Scout White',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_SHURIKEN: Item = {
+  id: '1H_Shuriken',
+  name: 'Shuriken',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_SMALL_AXE: Item = {
+  id: '1H_Small_Axe',
+  name: 'Small Axe',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_TRAYNORS_AXE: Item = {
+  id: '1H_Traynors_Axe',
+  name: 'Traynors Axe',
+  picture: '',
+} as const
+
+export const WEAPONS_1H_WRENCH_PIPE: Item = {
+  id: '1H_Wrench_Pipe',
+  name: 'Wrench Pipe',
+  picture: '',
+} as const
+
+export const WEAPONS_2H_AXE: Item = {
+  id: '2H_Axe',
+  name: 'Axe',
+  picture: '',
+} as const
+
+export const WEAPONS_2H_BASEBALL_BAT: Item = {
+  id: '2H_Baseball_Bat',
+  name: 'Baseball Bat',
+  picture: '',
+} as const
+
+export const WEAPONS_2H_BASEBALL_BAT_BLAZE: Item = {
+  id: '2H_Baseball_Bat_Blaze',
+  name: 'Baseball Bat Blaze',
+  picture: '',
+} as const
+
+export const WEAPONS_2H_INDUSTRIAL_GARDERING_HOE: Item = {
+  id: '2H_Industrial_Gardening_Hoe',
+  name: 'Industrial Gardening Hoe',
+  picture: '',
+} as const
+
+export const WEAPONS_2H_KATANA: Item = {
+  id: '2H_Katana',
+  name: 'Katana',
+  picture: '',
+} as const
+
+export const WEAPONS_2H_KATANA_01: Item = {
+  id: '2H_Katana1',
+  name: 'Katana Gold',
+  picture: '',
+} as const
+
+export const WEAPONS_2H_METAL_AXE: Item = {
+  id: '2H_Metal_Axe',
+  name: 'Metal Axe',
+  picture: '',
+} as const
+
+export const WEAPONS_2H_METAL_BASEBALL_BAT: Item = {
+  id: '2H_Metal_Baseball_Bat',
+  name: 'Metal Baseball Bat',
+  picture: '',
+} as const
+
+export const WEAPONS_2H_PICKAXE: Item = {
+  id: '2H_Pickaxe',
+  name: 'Pickaxe',
+  picture: '',
+} as const
+
+export const WEAPONS_2H_PITCHFORK: Item = {
+  id: '2H_Pitchfork',
+  name: 'Pitchfork',
+  picture: '',
+} as const
+
+export const WEAPONS_2H_PITCHFORK_BENT: Item = {
+  id: '2H_Pitchfork_Bent',
+  name: 'Pitchfork Bent',
+  picture: '',
+} as const
+
+export const WEAPONS_2H_SHOVEL: Item = {
+  id: '2H_Shovel_01',
+  name: 'Shovel',
+  picture: '',
+} as const
+
+export const WEAPONS_2H_SHOVEL_02: Item = {
+  id: '2H_Shovel_02',
+  name: 'Shovel',
+  picture: '',
+} as const
+
+export const WEAPONS_2H_STONE_AXE: Item = {
+  id: '2H_Stone_Axe',
+  name: 'Stone Axe',
+  picture: '',
+} as const
+
+export const WEAPONS_2H_TANG_DAO: Item = {
+  id: '2H_Tang_Dao',
+  name: 'Tang Dao',
+  picture: '',
+} as const
+
+export const WEAPONS_2H_TANG_DAO_BLACK_DRAGON: Item = {
+  id: '2H_Tang_Dao_Black_Dragon',
+  name: 'Tang Dao Black Dragon',
+  picture: '',
+} as const
+
+export const WEAPONS_2H_TANG_DAO_DAMASCUS: Item = {
+  id: '2H_Tang_Dao_Damascus',
+  name: 'Tang Dao Damascus',
+  picture: '',
+} as const
