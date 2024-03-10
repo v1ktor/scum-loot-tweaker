c77254b827bcf8c22241c21106d419cd83e4e1a0
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-10 23:44:18 +0200

    Add hoodies to the items dropdown

---
+++ src/app/items/clothes/hoodies.ts
@@ -0,0 +1,43 @@
+import { Item } from "../item.ts";
+
+export const HOODIE_01: Item = {
+  id: 'Hoodie_01',
+  name: 'Hoodie 01',
+  picture: ''
+} as const;
+
+export const HOODIE_02: Item = {
+  id: 'Hoodie_02',
+  name: 'Hoodie 02',
+  picture: ''
+} as const;
+
+export const HOODIE_03: Item = {
+  id: 'Hoodie_03',
+  name: 'Hoodie 03',
+  picture: ''
+} as const;
+
+export const HOODIE_04: Item = {
+  id: 'Hoodie_04',
+  name: 'Hoodie 04',
+  picture: ''
+} as const;
+
+export const HOODIE_05: Item = {
+  id: 'Hoodie_05',
+  name: 'Hoodie 05',
+  picture: ''
+} as const;
+
+export const HOODIE_INMATE: Item = {
+  id: 'Inmate_Hoodie_01',
+  name: 'Inmate Hoodie',
+  picture: ''
+} as const;
+
+export const HOODIE_RK: Item = {
+  id: 'RK_Hoodie_01',
+  name: 'RayKit Hoodie',
+  picture: ''
+} as const;
