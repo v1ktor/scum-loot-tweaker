9eb1d5204d9822309feb025fdcbfc6431df51cda
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-10 17:05:35 +0200

    Add ghillie to the items dropdown

---
+++ src/app/items/clothes/ghillie.ts
@@ -0,0 +1,79 @@
+import { Item } from "../item.ts";
+
+export const GHILLIE_WEAPON_DESERT: Item = {
+  id: 'WeaponGhillie_Desert',
+  name: 'Weapon Ghillie Desert',
+  picture: ''
+} as const;
+
+export const GHILLIE_WEAPON_WINTER: Item = {
+  id: 'WeaponGhillie_Winter',
+  name: 'Weapon Ghillie Winter',
+  picture: ''
+} as const;
+
+export const GHILLIE_WEAPON_WOODLAND: Item = {
+  id: 'WeaponGhillie_Woodland',
+  name: 'Weapon Ghillie Woodland',
+  picture: ''
+} as const;
+
+export const GHILLIE_SUIT_JACKET_01: Item = {
+  id: 'Ghillie_Suit_Jacket_01',
+  name: 'Ghillie Suit Jacket 01',
+  picture: ''
+} as const;
+
+export const GHILLIE_SUIT_JACKET_02: Item = {
+  id: 'Ghillie_Suit_Jacket_02',
+  name: 'Ghillie Suit Jacket 02',
+  picture: ''
+} as const;
+
+export const GHILLIE_SUIT_JACKET_03: Item = {
+  id: 'Ghillie_Suit_Jacket_03',
+  name: 'Ghillie Suit Jacket 03',
+  picture: ''
+} as const;
+
+export const GHILLIE_SUIT_JACKET_04: Item = {
+  id: 'Ghillie_Suit_Jacket_04',
+  name: 'Ghillie Suit Jacket 04',
+  picture: ''
+} as const;
+
+export const GHILLIE_SUIT_JACKET_05: Item = {
+  id: 'Ghillie_Suit_Jacket_05',
+  name: 'Ghillie Suit Jacket 05',
+  picture: ''
+} as const;
+
+export const GHILLIE_SUIT_PANTS_01: Item = {
+  id: 'Ghillie_Suit_Pants_01',
+  name: 'Ghillie Suit Pants 01',
+  picture: ''
+} as const;
+
+export const GHILLIE_SUIT_PANTS_02: Item = {
+  id: 'Ghillie_Suit_Pants_02',
+  name: 'Ghillie Suit Pants 02',
+  picture: ''
+} as const;
+
+export const GHILLIE_SUIT_PANTS_03: Item = {
+  id: 'Ghillie_Suit_Pants_03',
+  name: 'Ghillie Suit Pants 03',
+  picture: ''
+} as const;
+
+export const GHILLIE_SUIT_PANTS_04: Item = {
+  id: 'Ghillie_Suit_Pants_04',
+  name: 'Ghillie Suit Pants 04',
+  picture: ''
+} as const;
+
+export const GHILLIE_SUIT_PANTS_05: Item = {
+  id: 'Ghillie_Suit_Pants_05',
+  name: 'Ghillie Suit Pants 05',
+  picture: ''
+} as const;
