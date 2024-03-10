126294fda783379971e3807b113a04c9f684c7e0
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-10 20:45:37 +0200

    Add pants to the items dropdown

---
+++ src/app/items/clothes/pants.ts
@@ -0,0 +1,571 @@
+import { Item } from "../item.ts";
+
+export const PANTS_CHRISTMAS_01: Item = {
+  id: 'Christmas_Pants_01',
+  name: 'Christmas Pants 01',
+  picture: ''
+} as const;
+
+export const PANTS_CHRISTMAS_02: Item = {
+  id: 'Christmas_Pants_02',
+  name: 'Christmas Pants 02',
+  picture: ''
+} as const;
+
+export const PANTS_CONSTRUCTION_01: Item = {
+  id: 'Construction_Pants_01',
+  name: 'Construction Pants 01',
+  picture: ''
+} as const;
+
+export const PANTS_CONSTRUCTION_02: Item = {
+  id: 'Construction_Pants_02',
+  name: 'Construction Pants 02',
+  picture: ''
+} as const;
+
+export const PANTS_CONSTRUCTION_03_CHILD1: Item = {
+  id: 'Construction_Pants_03_Child1',
+  name: 'Construction Pants 03',
+  picture: ''
+} as const;
+
+export const PANTS_HIPPIE_01: Item = {
+  id: 'Hippie_Pants_01',
+  name: 'Hippie Pants 01',
+  picture: ''
+} as const;
+
+export const PANTS_HIPPIE_02: Item = {
+  id: 'Hippie_Pants_02',
+  name: 'Hippie Pants 02',
+  picture: ''
+} as const;
+
+export const PANTS_HIPPIE_03: Item = {
+  id: 'Hippie_Pants_03',
+  name: 'Hippie Pants 03',
+  picture: ''
+} as const;
+
+export const PANTS_HIPPIE_04: Item = {
+  id: 'Hippie_Pants_04',
+  name: 'Hippie Pants 04',
+  picture: ''
+} as const;
+
+export const PANTS_HIPPIE_05: Item = {
+  id: 'Hippie_Pants_05',
+  name: 'Hippie Pants 05',
+  picture: ''
+} as const;
+
+export const PANTS_HIPPIE_06: Item = {
+  id: 'Hippie_Pants_06',
+  name: 'Hippie Pants 06',
+  picture: ''
+} as const;
+
+export const PANTS_HIPPIE_07: Item = {
+  id: 'Hippie_Pants_07',
+  name: 'Hippie Pants 07',
+  picture: ''
+} as const;
+
+export const PANTS_HIPPIE_08: Item = {
+  id: 'Hippie_Pants_08',
+  name: 'Hippie Pants 08',
+  picture: ''
+} as const;
+
+export const PANTS_HOSPITAL: Item = {
+  id: 'Hospital_Pants',
+  name: 'Hospital Pants',
+  picture: ''
+} as const;
+
+export const PANTS_INMATE: Item = {
+  id: 'Inmate_pants',
+  name: 'Inmate Pants',
+  picture: ''
+} as const;
+
+export const PANTS_MILITARY_01: Item = {
+  id: 'MilitaryPants_01',
+  name: 'Military Pants 01',
+  picture: ''
+} as const;
+
+export const PANTS_MILITARY_02: Item = {
+  id: 'MilitaryPants_02',
+  name: 'Military Pants 02',
+  picture: ''
+} as const;
+
+export const PANTS_MILITARY_03: Item = {
+  id: 'MilitaryPants_03',
+  name: 'Military Pants 03',
+  picture: ''
+} as const;
+
+export const PANTS_MILITARY_04: Item = {
+  id: 'MilitaryPants_04',
+  name: 'Military Pants 04',
+  picture: ''
+} as const;
+
+export const PANTS_MILITARY_05: Item = {
+  id: 'MilitaryPants_05',
+  name: 'Military Pants 05',
+  picture: ''
+} as const;
+
+export const PANTS_MILITARY_06: Item = {
+  id: 'MilitaryPants_06',
+  name: 'Military Pants 06',
+  picture: ''
+} as const;
+
+export const PANTS_MILITARY_07: Item = {
+  id: 'MilitaryPants_07',
+  name: 'Military Pants 07',
+  picture: ''
+} as const;
+
+export const PANTS_MILITARY_08: Item = {
+  id: 'MilitaryPants_08',
+  name: 'Military Pants 08',
+  picture: ''
+} as const;
+
+export const PANTS_MILITARY_09: Item = {
+  id: 'MilitaryPants_09',
+  name: 'Military Pants 09',
+  picture: ''
+} as const;
+
+export const PANTS_MOTORCYCLE_01: Item = {
+  id: 'Motorcycle_Pants_01',
+  name: 'Motorcycle Pants 01',
+  picture: ''
+} as const;
+
+export const PANTS_MOTORCYCLE_02: Item = {
+  id: 'Motorcycle_Pants_02',
+  name: 'Motorcycle Pants 02',
+  picture: ''
+} as const;
+
+export const PANTS_MOTORCYCLE_03: Item = {
+  id: 'Motorcycle_Pants_03',
+  name: 'Motorcycle Pants 03',
+  picture: ''
+} as const;
+
+export const PANTS_PAJAMA: Item = {
+  id: 'Pajama_Pants',
+  name: 'Pajama Pants',
+  picture: ''
+} as const;
+
+export const PANTS_SANTA: Item = {
+  id: 'Santa_Pants',
+  name: 'Santa Pants',
+  picture: ''
+} as const;
+
+export const PANTS_SHIMORO: Item = {
+  id: 'Shimoro_Pants',
+  name: 'Shimoro Pants',
+  picture: ''
+} as const;
+
+export const PANTS_SHORTS_01_01: Item = {
+  id: 'Shorts_Pants_01_01',
+  name: 'Shorts Pants 01 01',
+  picture: ''
+} as const;
+
+export const PANTS_SHORTS_01_10: Item = {
+  id: 'Shorts_Pants_01_10',
+  name: 'Shorts Pants 01 10',
+  picture: ''
+} as const;
+
+export const PANTS_SHORTS_01_02: Item = {
+  id: 'Shorts_Pants_01_02',
+  name: 'Shorts Pants 01 02',
+  picture: ''
+} as const;
+
+export const PANTS_SHORTS_01_03: Item = {
+  id: 'Shorts_Pants_01_03',
+  name: 'Shorts Pants 01 03',
+  picture: ''
+} as const;
+
+export const PANTS_SHORTS_01_04: Item = {
+  id: 'Shorts_Pants_01_04',
+  name: 'Shorts Pants 01 04',
+  picture: ''
+} as const;
+
+export const PANTS_SHORTS_01_05: Item = {
+  id: 'Shorts_Pants_01_05',
+  name: 'Shorts Pants 01 05',
+  picture: ''
+} as const;
+
+export const PANTS_SHORTS_01_06: Item = {
+  id: 'Shorts_Pants_01_06',
+  name: 'Shorts Pants 01 06',
+  picture: ''
+} as const;
+
+export const PANTS_SHORTS_01_07: Item = {
+  id: 'Shorts_Pants_01_07',
+  name: 'Shorts Pants 01 07',
+  picture: ''
+} as const;
+
+export const PANTS_SHORTS_01_08: Item = {
+  id: 'Shorts_Pants_01_08',
+  name: 'Shorts Pants 01 08',
+  picture: ''
+} as const;
+
+export const PANTS_SHORTS_01_09: Item = {
+  id: 'Shorts_Pants_01_09',
+  name: 'Shorts Pants 01 09',
+  picture: ''
+} as const;
+
+export const PANTS_SUITE_01: Item = {
+  id: 'Suite_Pants_01',
+  name: 'Suite Pants 01',
+  picture: ''
+} as const;
+
+export const PANTS_TANG_01: Item = {
+  id: 'Tang_pants_01',
+  name: 'Tang Pants 01',
+  picture: ''
+} as const;
+
+export const PANTS_TANG_02: Item = {
+  id: 'Tang_pants_02',
+  name: 'Tang Pants 02',
+  picture: ''
+} as const;
+
+export const PANTS_TANG_03: Item = {
+  id: 'Tang_pants_03',
+  name: 'Tang Pants 03',
+  picture: ''
+} as const;
+
+export const PANTS_TANG_04: Item = {
+  id: 'Tang_pants_04',
+  name: 'Tang Pants 04',
+  picture: ''
+} as const;
+
+export const PANTS_TANG_05: Item = {
+  id: 'Tang_pants_05',
+  name: 'Tang Pants 05',
+  picture: ''
+} as const;
+
+export const PANTS_TRACKSUIT_01_01: Item = {
+  id: 'Tracksuit_Pants_01_01',
+  name: 'Tracksuit Pants 01 01',
+  picture: ''
+} as const;
+
+export const PANTS_TRACKSUIT_02_01: Item = {
+  id: 'Tracksuit_Pants_02_01',
+  name: 'Tracksuit Pants 02 01',
+  picture: ''
+} as const;
+
+export const PANTS_TRACKSUIT_01_02: Item = {
+  id: 'Tracksuit_Pants_01_02',
+  name: 'Tracksuit Pants 01 02',
+  picture: ''
+} as const;
+
+export const PANTS_TRACKSUIT_01_03: Item = {
+  id: 'Tracksuit_Pants_01_03',
+  name: 'Tracksuit Pants 01 03',
+  picture: ''
+} as const;
+
+export const PANTS_TRACKSUIT_01_04: Item = {
+  id: 'Tracksuit_Pants_01_04',
+  name: 'Tracksuit Pants 01 04',
+  picture: ''
+} as const;
+
+export const PANTS_TRACKSUIT_01_05: Item = {
+  id: 'Tracksuit_Pants_01_05',
+  name: 'Tracksuit Pants 01 05',
+  picture: ''
+} as const;
+
+export const PANTS_TRACKSUIT_02_02: Item = {
+  id: 'Tracksuit_Pants_02_02',
+  name: 'Tracksuit Pants 02 02',
+  picture: ''
+} as const;
+
+export const PANTS_TRACKSUIT_02_03: Item = {
+  id: 'Tracksuit_Pants_02_03',
+  name: 'Tracksuit Pants 02 03',
+  picture: ''
+} as const;
+
+export const PANTS_TRACKSUIT_02_04: Item = {
+  id: 'Tracksuit_Pants_02_04',
+  name: 'Tracksuit Pants 02 04',
+  picture: ''
+} as const;
+
+export const PANTS_TRACKSUIT_02_05: Item = {
+  id: 'Tracksuit_Pants_02_05',
+  name: 'Tracksuit Pants 02 05',
+  picture: ''
+} as const;
+
+export const PANTS_UNDERPANTS_01: Item = {
+  id: 'Underpants_01',
+  name: 'Underpants 01',
+  picture: ''
+} as const;
+
+export const PANTS_UNDERPANTS_02: Item = {
+  id: 'Underpants_02',
+  name: 'Underpants 02',
+  picture: ''
+} as const;
+
+export const PANTS_UNDERPANTS_03: Item = {
+  id: 'Underpants_03',
+  name: 'Underpants 03',
+  picture: ''
+} as const;
+
+export const PANTS_UNDERPANTS_04: Item = {
+  id: 'Underpants_04',
+  name: 'Underpants 04',
+  picture: ''
+} as const;
+
+export const PANTS_UNDERPANTS_05: Item = {
+  id: 'Underpants_05',
+  name: 'Underpants 05',
+  picture: ''
+} as const;
+
+export const PANTS_WORKING_01_01: Item = {
+  id: 'Working_pants_01_01',
+  name: 'Working Pants 01 01',
+  picture: ''
+} as const;
+
+export const PANTS_WORKING_01_02: Item = {
+  id: 'Working_pants_01_02',
+  name: 'Working Pants 01 02',
+  picture: ''
+} as const;
+
+export const PANTS_WORKING_01_03: Item = {
+  id: 'Working_pants_01_03',
+  name: 'Working Pants 01 03',
+  picture: ''
+} as const;
+
+export const PANTS_PANTYHOSE_01: Item = {
+  id: 'Pantyhose_01',
+  name: 'Pantyhose 01',
+  picture: ''
+} as const;
+
+export const PANTS_SKIRT_01_01: Item = {
+  id: 'Skirt_01_01',
+  name: 'Skirt 01',
+  picture: ''
+} as const;
+
+export const PANTS_SKIRT_01_02: Item = {
+  id: 'Skirt_01_02',
+  name: 'Skirt 02',
+  picture: ''
+} as const;
+
+export const PANTS_SKIRT_01_03: Item = {
+  id: 'Skirt_01_03',
+  name: 'Skirt 03',
+  picture: ''
+} as const;
+
+export const PANTS_SHORT_TROUSERS_01_01: Item = {
+  id: 'Short_Trousers_01_01',
+  name: 'Short Trousers 01',
+  picture: ''
+} as const;
+
+export const PANTS_SHORT_TROUSERS_01_02: Item = {
+  id: 'Short_Trousers_01_02',
+  name: 'Short Trousers 02',
+  picture: ''
+} as const;
+
+export const PANTS_SHORT_TROUSERS_01_03: Item = {
+  id: 'Short_Trousers_01_03',
+  name: 'Short Trousers 03',
+  picture: ''
+} as const;
+
+export const PANTS_SHORT_TROUSERS_01_04: Item = {
+  id: 'Short_Trousers_01_04',
+  name: 'Short Trousers 04',
+  picture: ''
+} as const;
+
+export const PANTS_SHORT_TROUSERS_01_05: Item = {
+  id: 'Short_Trousers_01_05',
+  name: 'Short Trousers 05',
+  picture: ''
+} as const;
+
+export const PANTS_SEXY_JEANS_SHORTS_01: Item = {
+  id: 'Sexy_Jeans_Shorts_01',
+  name: 'Sexy Jeans Shorts 01',
+  picture: ''
+} as const;
+
+export const PANTS_SEXY_JEANS_SHORTS_02: Item = {
+  id: 'Sexy_Jeans_Shorts_02',
+  name: 'Sexy Jeans Shorts 02',
+  picture: ''
+} as const;
+
+export const PANTS_SEXY_JEANS_SHORTS_03: Item = {
+  id: 'Sexy_Jeans_Shorts_03',
+  name: 'Sexy Jeans Shorts 03',
+  picture: ''
+} as const;
+
+export const PANTS_SEXY_JEANS_SHORTS_04: Item = {
+  id: 'Sexy_Jeans_Shorts_04',
+  name: 'Sexy Jeans Shorts 04',
+  picture: ''
+} as const;
+
+export const PANTS_SEXY_JEANS_SHORTS_05: Item = {
+  id: 'Sexy_Jeans_Shorts_05',
+  name: 'Sexy Jeans Shorts 05',
+  picture: ''
+} as const;
+
+export const PANTS_SEXY_JEANS_SHORTS_06: Item = {
+  id: 'Sexy_Jeans_Shorts_06',
+  name: 'Sexy Jeans Shorts 06',
+  picture: ''
+} as const;
+
+export const PANTS_SEXY_JEANS_SHORTS_07: Item = {
+  id: 'Sexy_Jeans_Shorts_07',
+  name: 'Sexy Jeans Shorts 07',
+  picture: ''
+} as const;
+
+export const PANTS_JEANS_01_01: Item = {
+  id: 'Jeans_01_01',
+  name: 'Jeans 01 01',
+  picture: ''
+} as const;
+
+export const PANTS_JEANS_01_02: Item = {
+  id: 'Jeans_01_02',
+  name: 'Jeans 01 02',
+  picture: ''
+} as const;
+
+export const PANTS_JEANS_01_03: Item = {
+  id: 'Jeans_01_03',
+  name: 'Jeans 01 03',
+  picture: ''
+} as const;
+
+export const PANTS_JEANS_01_04: Item = {
+  id: 'Jeans_01_04',
+  name: 'Jeans 01 04',
+  picture: ''
+} as const;
+
+export const PANTS_JEANS_02_01_V2: Item = {
+  id: 'Jeans_02_01_V2',
+  name: 'Jeans 02 01 V2',
+  picture: ''
+} as const;
+
+export const PANTS_JEANS_02_02: Item = {
+  id: 'Jeans_02_02',
+  name: 'Jeans 02 02',
+  picture: ''
+} as const;
+
+export const PANTS_JEANS_02_03: Item = {
+  id: 'Jeans_02_03',
+  name: 'Jeans 02 03',
+  picture: ''
+} as const;
+
+export const PANTS_JEANS_02_04: Item = {
+  id: 'Jeans_02_04',
+  name: 'Jeans 02 04',
+  picture: ''
+} as const;
+
+export const PANTS_JEANS_02_05: Item = {
+  id: 'Jeans_02_05',
+  name: 'Jeans 02 05',
+  picture: ''
+} as const;
+
+export const PANTS_JEANS_02_06: Item = {
+  id: 'Jeans_02_06',
+  name: 'Jeans 02 06',
+  picture: ''
+} as const;
+
+export const PANTS_JEANS_02_07: Item = {
+  id: 'Jeans_02_07',
+  name: 'Jeans 02 07',
+  picture: ''
+} as const;
+
+export const PANTS_JEANS_02_08: Item = {
+  id: 'Jeans_02_08',
+  name: 'Jeans 02 08',
+  picture: ''
+} as const;
+
+export const PANTS_JEANS_02_09: Item = {
+  id: 'Jeans_02_09',
+  name: 'Jeans 02 09',
+  picture: ''
+} as const;
+
+export const PANTS_JEANS_JERSEY_JACKET_01: Item = {
+  id: 'Jeans_Jersey_Jacket_01',
+  name: 'Jeans Jersey Jacket 01',
+  picture: ''
+} as const;
+
+export const PANTS_JEANS_JERSEY_JACKET_02: Item = {
+  id: 'Jeans_Jersey_Jacket_02',
+  name: 'Jeans Jersey Jacket 02',
+  picture: ''
+} as const;
