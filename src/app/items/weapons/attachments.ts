8fc56fa8a23708b2465a1d33ff17e03de954d9ab
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-10 19:37:25 +0200

    Add attachments to the items dropdown

---
+++ src/app/items/weapons/attachments.ts
@@ -0,0 +1,43 @@
+import { Item } from "../item.ts";
+
+export const ATTACHMENT_DW715_RAIL: Item = {
+  id: 'DW715_Rail',
+  name: 'DW715 Rail',
+  picture: ''
+} as const;
+
+export const ATTACHMENT_M1911_RAIL: Item = {
+  id: 'M1911_Rail',
+  name: 'M1911 Rail',
+  picture: ''
+} as const;
+
+export const ATTACHMENT_MP5_RAIL_SHORT: Item = {
+  id: 'MP5_Rail_Short',
+  name: 'MP5 Rail Short',
+  picture: ''
+} as const;
+
+export const ATTACHMENT_SCOPE_RAIL_AK47: Item = {
+  id: 'ScopeRail_AK47',
+  name: 'Scope Rail AK47',
+  picture: ''
+} as const;
+
+export const ATTACHMENT_WEAPON_FLASHLIGHT_AS_VAL: Item = {
+  id: 'WeaponFlashlight_AS_Val',
+  name: 'Weapon Flashlight AS Val',
+  picture: ''
+} as const;
+
+export const ATTACHMENT_WEAPON_FLASHLIGHT_DESERT_EAGLE: Item = {
+  id: 'WeaponFlashlight_DesertEagle',
+  name: 'Weapon Flashlight Desert Eagle',
+  picture: ''
+} as const;
+
+export const ATTACHMENT_WEAPON_FLASHLIGHT_M9: Item = {
+  id: 'WeaponFlashlight_M9',
+  name: 'Weapon Flashlight M9',
+  picture: ''
+} as const;
