42f1cefe66abe98dba7c7f4aa627082ea30acf2f
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-11 00:39:46 +0200

    Add jackets to the items dropdown

---
+++ src/app/items/clothes/jackets.ts
@@ -0,0 +1,241 @@
+import { Item } from "../item.ts";
+
+export const JACKET_JOCK_01: Item = {
+  id: 'Jock_Jacket_01',
+  name: 'Jock Jacket 01',
+  picture: ''
+} as const;
+
+export const JACKET_JOCK_02: Item = {
+  id: 'Jock_Jacket_02',
+  name: 'Jock Jacket 02',
+  picture: ''
+} as const;
+
+export const JACKET_JOCK_03: Item = {
+  id: 'Jock_Jacket_03',
+  name: 'Jock Jacket 03',
+  picture: ''
+} as const;
+
+export const JACKET_JOCK_04: Item = {
+  id: 'Jock_Jacket_04',
+  name: 'Jock Jacket 04',
+  picture: ''
+} as const;
+
+export const JACKET_LEATHER: Item = {
+  id: 'Leather_Jacket',
+  name: 'Leather Jacket',
+  picture: ''
+} as const;
+
+export const JACKET_MOTORCYCLE_01: Item = {
+  id: 'Motorcycle_Jacket_01',
+  name: 'Motorcycle Jacket 01',
+  picture: ''
+} as const;
+
+export const JACKET_MOTORCYCLE_02: Item = {
+  id: 'Motorcycle_Jacket_02',
+  name: 'Motorcycle Jacket 02',
+  picture: ''
+} as const;
+
+export const JACKET_MOTORCYCLE_03: Item = {
+  id: 'Motorcycle_Jacket_03',
+  name: 'Motorcycle Jacket 03',
+  picture: ''
+} as const;
+
+export const JACKET_NEGAN: Item = {
+  id: 'Negan_Jacket',
+  name: 'Negan Jacket',
+  picture: ''
+} as const;
+
+export const JACKET_PILOT: Item = {
+  id: 'Pilot_Jacket',
+  name: 'Pilot Jacket',
+  picture: ''
+} as const;
+
+export const JACKET_POLICE: Item = {
+  id: 'Police_Jacket_01',
+  name: 'Police Jacket',
+  picture: ''
+} as const;
+
+export const JACKET_RH: Item = {
+  id: 'RH_Jacket',
+  name: 'RH Jacket',
+  picture: ''
+} as const;
+
+export const JACKET_SANTA: Item = {
+  id: 'Santa_Jacket',
+  name: 'Santa Jacket',
+  picture: ''
+} as const;
+
+export const JACKET_TACTICAL_01_01: Item = {
+  id: 'Tactical_Jacket_01_01',
+  name: 'Tactical Jacket 01',
+  picture: ''
+} as const;
+
+export const JACKET_TACTICAL_01_02: Item = {
+  id: 'Tactical_Jacket_01_02',
+  name: 'Tactical Jacket 02',
+  picture: ''
+} as const;
+
+export const JACKET_TACTICAL_01_03: Item = {
+  id: 'Tactical_Jacket_01_03',
+  name: 'Tactical Jacket 03',
+  picture: ''
+} as const;
+
+export const JACKET_TACTICAL_01_04: Item = {
+  id: 'Tactical_Jacket_01_04',
+  name: 'Tactical Jacket 04',
+  picture: ''
+} as const;
+
+export const JACKET_TACTICAL_01_05: Item = {
+  id: 'Tactical_Jacket_01_05',
+  name: 'Tactical Jacket 05',
+  picture: ''
+} as const;
+
+export const JACKET_TACTICAL_01_06: Item = {
+  id: 'Tactical_Jacket_01_06',
+  name: 'Tactical Jacket 06',
+  picture: ''
+} as const;
+
+export const JACKET_TACTICAL_01_07: Item = {
+  id: 'Tactical_Jacket_01_07',
+  name: 'Tactical Jacket 07',
+  picture: ''
+} as const;
+
+export const JACKET_TACTICAL_01_09: Item = {
+  id: 'Tactical_Jacket_01_09',
+  name: 'Tactical Jacket 09',
+  picture: ''
+} as const;
+
+export const JACKET_STRAIT: Item = {
+  id: 'Strait_Jacket',
+  name: 'Strait Jacket',
+  picture: ''
+} as const;
+
+export const JACKET_TRUCKER_01: Item = {
+  id: 'Trucker_Jacket_01',
+  name: 'Trucker Jacket 01',
+  picture: ''
+} as const;
+
+export const JACKET_TRUCKER_02: Item = {
+  id: 'Trucker_Jacket_02',
+  name: 'Trucker Jacket 02',
+  picture: ''
+} as const;
+
+export const JACKET_TRUCKER_03: Item = {
+  id: 'Trucker_Jacket_03',
+  name: 'Trucker Jacket 03',
+  picture: ''
+} as const;
+
+export const JACKET_TRUCKER_04: Item = {
+  id: 'Trucker_Jacket_04',
+  name: 'Trucker Jacket 04',
+  picture: ''
+} as const;
+
+export const JACKET_WORK_01: Item = {
+  id: 'Work_Jacket_01',
+  name: 'Work Jacket 01',
+  picture: ''
+} as const;
+
+export const JACKET_WORK_02: Item = {
+  id: 'Work_Jacket_02',
+  name: 'Work Jacket 02',
+  picture: ''
+} as const;
+
+export const JACKET_WORK_03: Item = {
+  id: 'Work_Jacket_03',
+  name: 'Work Jacket 03',
+  picture: ''
+} as const;
+
+export const JACKET_CAMOUFLAGE_01: Item = {
+  id: 'Camouflage_Jacket_01',
+  name: 'Camouflage Jacket 01',
+  picture: ''
+} as const;
+
+export const JACKET_CAMOUFLAGE_02: Item = {
+  id: 'Camouflage_Jacket_02',
+  name: 'Camouflage Jacket 02',
+  picture: ''
+} as const;
+
+export const JACKET_CAMOUFLAGE_03: Item = {
+  id: 'Camouflage_Jacket_03',
+  name: 'Camouflage Jacket 03',
+  picture: ''
+} as const;
+
+export const JACKET_COLLEGE_01: Item = {
+  id: 'College_Jacket_01',
+  name: 'College Jacket 01',
+  picture: ''
+} as const;
+
+export const JACKET_COLLEGE_02: Item = {
+  id: 'College_Jacket_02',
+  name: 'College Jacket 02',
+  picture: ''
+} as const;
+
+export const JACKET_COLLEGE_03: Item = {
+  id: 'College_Jacket_03',
+  name: 'College Jacket 03',
+  picture: ''
+} as const;
+
+export const JACKET_COLLEGE_04: Item = {
+  id: 'College_Jacket_04',
+  name: 'College Jacket 04',
+  picture: ''
+} as const;
+
+export const JACKET_COLLEGE_05: Item = {
+  id: 'College_Jacket_05',
+  name: 'College Jacket 05',
+  picture: ''
+} as const;
+
+export const JACKET_COLLEGE_06: Item = {
+  id: 'College_Jacket_06',
+  name: 'College Jacket 06',
+  picture: ''
+} as const;
+
+export const JACKET_COLLEGE_07: Item = {
+  id: 'College_Jacket_07',
+  name: 'College Jacket 07',
+  picture: ''
+} as const;
+
+export const JACKET_COLLEGE_08: Item = {
+  id: 'College_Jacket_08',
+  name: 'College Jacket 08',
+  picture: ''
+} as const;
