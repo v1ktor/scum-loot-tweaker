1243a88d4de604c3cdecfdd490611bf1b2c78a86
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-10 23:32:59 +0200

    Add masks to the items dropdown

---
+++ src/app/items/clothes/masks.ts
@@ -0,0 +1,193 @@
+import { Item } from "../item.ts";
+
+export const MASK_DISPOSABLE: Item = {
+  id: 'Disposable_Mask',
+  name: 'Disposable Mask',
+  picture: ''
+} as const;
+
+export const MASK_DIVING: Item = {
+  id: 'Diving_Mask',
+  name: 'Diving Mask',
+  picture: ''
+} as const;
+
+export const MASK_DIVING_WITH_BREATHING_TUBE: Item = {
+  id: 'Diving_Mask_With_Breathing_Tube',
+  name: 'Diving Mask With Breathing Tube',
+  picture: ''
+} as const;
+
+export const MASK_GAS_01: Item = {
+  id: 'Gas_Mask_01',
+  name: 'Gas Mask 01',
+  picture: ''
+} as const;
+
+export const MASK_GAS_02: Item = {
+  id: 'Gas_Mask_02',
+  name: 'Gas Mask 02',
+  picture: ''
+} as const;
+
+export const MASK_GAS_03: Item = {
+  id: 'Gas_Mask_03',
+  name: 'Gas Mask 03',
+  picture: ''
+} as const;
+
+export const MASK_GAS_04: Item = {
+  id: 'Gas_Mask_04',
+  name: 'Gas Mask 04',
+  picture: ''
+} as const;
+
+export const MASK_HALLOWEEN_ANONYMOUS_01: Item = {
+  id: 'Halloween_Anonymous_Mask_01',
+  name: 'Halloween Anonymous Mask',
+  picture: ''
+} as const;
+
+export const MASK_HALLOWEEN_GREEK: Item = {
+  id: 'Halloween_Mask_Greek',
+  name: 'Halloween Mask Greek',
+  picture: ''
+} as const;
+
+export const MASK_HALLOWEEN_KNIFE: Item = {
+  id: 'Halloween_Mask_Knife',
+  name: 'Halloween Mask Knife',
+  picture: ''
+} as const;
+
+export const MASK_HALLOWEEN_SCREAM: Item = {
+  id: 'Halloween_Mask_Scream',
+  name: 'Halloween Mask Scream',
+  picture: ''
+} as const;
+
+export const MASK_HALLOWEEN_BABYDOLL_02: Item = {
+  id: 'Halloween_Mask_BabyDoll_02',
+  name: 'Halloween Mask BabyDoll',
+  picture: ''
+} as const;
+
+export const MASK_HALLOWEEN_CLOWN_03: Item = {
+  id: 'Halloween_Mask_Clown_03',
+  name: 'Halloween Mask Clown',
+  picture: ''
+} as const;
+
+export const MASK_HALLOWEEN_HOCKEY_04: Item = {
+  id: 'Halloween_Mask_Hockey_04',
+  name: 'Halloween Mask Hockey',
+  picture: ''
+} as const;
+
+export const MASK_HALLOWEEN_JOHNDOE_05: Item = {
+  id: 'Halloween_Mask_JohnDoe_05',
+  name: 'Halloween Mask JohnDoe',
+  picture: ''
+} as const;
+
+export const MASK_HALLOWEEN_LECTER_06: Item = {
+  id: 'Halloween_Mask_Lecter_06',
+  name: 'Halloween Mask Lecter',
+  picture: ''
+} as const;
+
+export const MASK_HALLOWEEN_PLAGUE_07: Item = {
+  id: 'Halloween_Mask_Plague_07',
+  name: 'Halloween Mask Plague',
+  picture: ''
+} as const;
+
+export const MASK_HALLOWEEN_SMILEY_08: Item = {
+  id: 'Halloween_Mask_Smiley_08',
+  name: 'Halloween Mask Smiley',
+  picture: ''
+} as const;
+
+export const MASK_HALLOWEEN_WOODEN_09: Item = {
+  id: 'Halloween_Mask_Wooden_09',
+  name: 'Halloween Mask Wooden',
+  picture: ''
+} as const;
+
+export const MASK_RABBIT_01: Item = {
+  id: 'Rabbit_Mask_01',
+  name: 'Rabbit Mask',
+  picture: ''
+} as const;
+
+export const MASK_MILITARY_01: Item = {
+  id: 'Military_Mask_01',
+  name: 'Military Mask 01',
+  picture: ''
+} as const;
+
+export const MASK_MILITARY_02: Item = {
+  id: 'Military_Mask_02',
+  name: 'Military Mask 02',
+  picture: ''
+} as const;
+
+export const MASK_MILITARY_03: Item = {
+  id: 'Military_Mask_03',
+  name: 'Military Mask 03',
+  picture: ''
+} as const;
+
+export const MASK_MILITARY_04: Item = {
+  id: 'Military_Mask_04',
+  name: 'Military Mask 04',
+  picture: ''
+} as const;
+
+export const MASK_MILITARY_05: Item = {
+  id: 'Military_Mask_05',
+  name: 'Military Mask 05',
+  picture: ''
+} as const;
+
+export const MASK_MILITARY_06: Item = {
+  id: 'Military_Mask_06',
+  name: 'Military Mask 06',
+  picture: ''
+} as const;
+
+export const MASK_MILITARY_07: Item = {
+  id: 'Military_Mask_07',
+  name: 'Military Mask 07',
+  picture: ''
+} as const;
+
+export const MASK_MILITARY_08: Item = {
+  id: 'Military_Mask_08',
+  name: 'Military Mask 08',
+  picture: ''
+} as const;
+
+export const MASK_MOTORCYCLE_01: Item = {
+  id: 'Motorcycle_Mask_01',
+  name: 'Motorcycle Mask 01',
+  picture: ''
+} as const;
+
+export const MASK_MOTORCYCLE_02: Item = {
+  id: 'Motorcycle_Mask_02',
+  name: 'Motorcycle Mask 02',
+  picture: ''
+} as const;
+
+export const MASK_POLICE_01: Item = {
+  id: 'Police_Mask_01',
+  name: 'Police Mask 01',
+  picture: ''
+} as const;
+
+export const MASK_TYRONE_SHOELACEZ: Item = {
+  id: 'Tyrone_Shoelacez_Mask',
+  name: 'Tyrone Shoelacez Mask',
+  picture: ''
+} as const;
