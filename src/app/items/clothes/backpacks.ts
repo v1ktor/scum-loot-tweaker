bce5bcdfa35925f34e3bb3098e3c24b7a9026b5b
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-10 19:24:49 +0200

    Add backpacks to the items dropdown

---
+++ src/app/items/clothes/backpacks.ts
@@ -0,0 +1,283 @@
+import { Item } from "../item.ts";
+
+export const BACKPACK_MILITARY_01_01: Item = {
+  id: 'Military_Backpack_01_01',
+  name: 'Military Backpack 01 01',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_01_02: Item = {
+  id: 'Military_Backpack_01_02',
+  name: 'Military Backpack 01 02',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_01_03: Item = {
+  id: 'Military_Backpack_01_03',
+  name: 'Military Backpack 01 03',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_01_04: Item = {
+  id: 'Military_Backpack_01_04',
+  name: 'Military Backpack 01 04',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_01_05: Item = {
+  id: 'Military_Backpack_01_05',
+  name: 'Military Backpack 01 05',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_01_06: Item = {
+  id: 'Military_Backpack_01_06',
+  name: 'Military Backpack 01 06',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_01_07: Item = {
+  id: 'Military_Backpack_01_07',
+  name: 'Military Backpack 01 07',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_01_08: Item = {
+  id: 'Military_Backpack_01_08',
+  name: 'Military Backpack 01 08',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_02_01: Item = {
+  id: 'Military_Backpack_02_01',
+  name: 'Military Backpack 02 01',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_02_02: Item = {
+  id: 'Military_Backpack_02_02',
+  name: 'Military Backpack 02 02',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_02_03: Item = {
+  id: 'Military_Backpack_02_03',
+  name: 'Military Backpack 02 03',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_02_04: Item = {
+  id: 'Military_Backpack_02_04',
+  name: 'Military Backpack 02 04',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_02_05: Item = {
+  id: 'Military_Backpack_02_05',
+  name: 'Military Backpack 02 05',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_02_06: Item = {
+  id: 'Military_Backpack_02_06',
+  name: 'Military Backpack 02 06',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_02_07: Item = {
+  id: 'Military_Backpack_02_07',
+  name: 'Military Backpack 02 07',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_03_01: Item = {
+  id: 'Military_Backpack_03_01',
+  name: 'Military Backpack 03 01',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_03_02: Item = {
+  id: 'Military_Backpack_03_02',
+  name: 'Military Backpack 03 02',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_03_03: Item = {
+  id: 'Military_Backpack_03_03',
+  name: 'Military Backpack 03 03',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_03_04: Item = {
+  id: 'Military_Backpack_03_04',
+  name: 'Military Backpack 03 04',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_03_05: Item = {
+  id: 'Military_Backpack_03_05',
+  name: 'Military Backpack 03 05',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_03_06: Item = {
+  id: 'Military_Backpack_03_06',
+  name: 'Military Backpack 03 06',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_03_07: Item = {
+  id: 'Military_Backpack_03_07',
+  name: 'Military Backpack 03 07',
+  picture: ''
+} as const;
+
+export const BACKPACK_MILITARY_03_08: Item = {
+  id: 'Military_Backpack_03_08',
+  name: 'Military Backpack 03 08',
+  picture: ''
+} as const;
+
+export const BACKPACK_02_01: Item = {
+  id: 'Backpack_02_01',
+  name: 'Backpack 02 01',
+  picture: ''
+} as const;
+
+export const BACKPACK_02_02: Item = {
+  id: 'Backpack_02_02',
+  name: 'Backpack 02 02',
+  picture: ''
+} as const;
+
+export const BACKPACK_02_03: Item = {
+  id: 'Backpack_02_03',
+  name: 'Backpack 02 03',
+  picture: ''
+} as const;
+
+export const BACKPACK_02_04: Item = {
+  id: 'Backpack_02_04',
+  name: 'Backpack 02 04',
+  picture: ''
+} as const;
+
+export const BACKPACK_02_05: Item = {
+  id: 'Backpack_02_05',
+  name: 'Backpack 02 05',
+  picture: ''
+} as const;
+
+export const BACKPACK_HIKING_01_01: Item = {
+  id: 'Hiking_Backpack_01_01',
+  name: 'Hiking Backpack 01 01',
+  picture: ''
+} as const;
+
+export const BACKPACK_HIKING_01_02: Item = {
+  id: 'Hiking_Backpack_01_02',
+  name: 'Hiking Backpack 01 02',
+  picture: ''
+} as const;
+
+export const BACKPACK_HIKING_01_03: Item = {
+  id: 'Hiking_Backpack_01_03',
+  name: 'Hiking Backpack 01 03',
+  picture: ''
+} as const;
+
+export const BACKPACK_HIKING_01_04: Item = {
+  id: 'Hiking_Backpack_01_04',
+  name: 'Hiking Backpack 01 04',
+  picture: ''
+} as const;
+
+export const BACKPACK_HIKING_01_05: Item = {
+  id: 'Hiking_Backpack_01_05',
+  name: 'Hiking Backpack 01 05',
+  picture: ''
+} as const;
+
+export const BACKPACK_HIKING_TYRONE: Item = {
+  id: 'Hiking_Backpack_Tyrone',
+  name: 'Hiking Backpack Tyrone',
+  picture: ''
+} as const;
+
+export const BACKPACK_LEATHER_01: Item = {
+  id: 'Leather_Backpack_01',
+  name: 'Leather Backpack 01',
+  picture: ''
+} as const;
+
+export const BACKPACK_LEATHER_02: Item = {
+  id: 'Leather_Backpack_02',
+  name: 'Leather Backpack 02',
+  picture: ''
+} as const;
+
+export const BACKPACK_LEATHER_03: Item = {
+  id: 'Leather_Backpack_03',
+  name: 'Leather Backpack 03',
+  picture: ''
+} as const;
+
+export const BACKPACK_POLICE_01: Item = {
+  id: 'Police_Backpack_01',
+  name: 'Police Backpack 01',
+  picture: ''
+} as const;
+
+export const BACKPACK_POLICE_02: Item = {
+  id: 'Police_Backpack_02',
+  name: 'Police Backpack 02',
+  picture: ''
+} as const;
+
+export const BACKPACK_POLICE_03: Item = {
+  id: 'Police_Backpack_03',
+  name: 'Police Backpack 03',
+  picture: ''
+} as const;
+
+export const BACKPACK_SCHOOL_01: Item = {
+  id: 'School_Backpack_01',
+  name: 'School Backpack 01',
+  picture: ''
+} as const;
+
+export const BACKPACK_SCHOOL_BOY_02: Item = {
+  id: 'School_Backpack_Boy_02',
+  name: 'School Backpack Boy 02',
+  picture: ''
+} as const;
+
+export const BACKPACK_SCHOOL_BOY_03: Item = {
+  id: 'School_Backpack_Boy_03',
+  name: 'School Backpack Boy 03',
+  picture: ''
+} as const;
+
+export const BACKPACK_SCHOOL_GIRL_04: Item = {
+  id: 'School_Backpack_Girl_04',
+  name: 'School Backpack Girl 04',
+  picture: ''
+} as const;
+
+export const BACKPACK_SCHOOL_GIRL_05: Item = {
+  id: 'School_Backpack_Girl_05',
+  name: 'School Backpack Girl 05',
+  picture: ''
+} as const;
+
+export const BACKPACK_SCHOOL_GIRL_06: Item = {
+  id: 'School_Backpack_Girl_06',
+  name: 'School Backpack Girl 06',
+  picture: ''
+} as const;
+
+export const BACKPACK_TC: Item = {
+  id: 'TC_Backpack',
+  name: 'TC Backpack',
+  picture: ''
+} as const;
