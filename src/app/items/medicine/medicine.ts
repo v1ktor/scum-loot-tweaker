ee2e4bb185b2df354c9e58bb05a1331d46eb90f5
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-10 16:57:56 +0200

    Add medicine to the items dropdown

---
+++ src/app/items/medicine/medicine.ts
@@ -0,0 +1,175 @@
+import { Item } from "../item.ts";
+
+export const MEDICINE_ACTIVATED_CHARCOAL_SMALL: Item = {
+  id: 'ActivatedCharcoal_01',
+  name: 'Activated Charcoal Small',
+  picture: ''
+} as const;
+
+export const MEDICINE_ACTIVATED_CHARCOAL_MEDIUM: Item = {
+  id: 'ActivatedCharcoal_02',
+  name: 'Activated Charcoal Medium',
+  picture: ''
+} as const;
+
+export const MEDICINE_ACTIVATED_CHARCOAL_BIG: Item = {
+  id: 'ActivatedCharcoal_03',
+  name: 'Activated Charcoal Big',
+  picture: ''
+} as const;
+
+export const MEDICINE_ANTIBIOTICS_SMALL: Item = {
+  id: 'Antibiotics_01',
+  name: 'Antibiotics Small',
+  picture: ''
+} as const;
+
+export const MEDICINE_ANTIBIOTICS_MEDIUM: Item = {
+  id: 'Antibiotics_02',
+  name: 'Antibiotics Medium',
+  picture: ''
+} as const;
+
+export const MEDICINE_ANTIBIOTICS_BIG: Item = {
+  id: 'Antibiotics_03',
+  name: 'Antibiotics Big',
+  picture: ''
+} as const;
+
+export const MEDICINE_EMERGENCY_BANDAGE: Item = {
+  id: 'Emergency_bandage',
+  name: 'Emergency bandage',
+  picture: ''
+} as const;
+
+export const MEDICINE_EMERGENCY_BANDAGE_BIG: Item = {
+  id: 'Emergency_bandage_Big',
+  name: 'Emergency bandage Big',
+  picture: ''
+} as const;
+
+export const MEDICINE_HEMOSTATIC_DRESSING: Item = {
+  id: 'Hemostatic_Dressing',
+  name: 'Hemostatic Dressing',
+  picture: ''
+} as const;
+
+export const MEDICINE_INTEA_LEVEL1: Item = {
+  id: 'IntEA_Level1',
+  name: 'Intelligence Enhancement Ampoule Level 1',
+  picture: ''
+} as const;
+
+export const MEDICINE_INTEA_LEVEL2: Item = {
+  id: 'IntEA_Level2',
+  name: 'Intelligence Enhancement Ampoule Level 2',
+  picture: ''
+} as const;
+
+export const MEDICINE_INTEA_LEVEL3: Item = {
+  id: 'IntEA_Level3',
+  name: 'Intelligence Enhancement Ampoule Level 3',
+  picture: ''
+} as const;
+
+export const MEDICINE_ISOPROPYL_ALCOHOL: Item = {
+  id: 'Isopropyl_alcohol',
+  name: 'Isopropyl alcohol',
+  picture: ''
+} as const;
+
+export const MEDICINE_MEMORY_MODULE_LEVEL1: Item = {
+  id: 'MemoryModule_Level1',
+  name: 'Memory Module Level 1',
+  picture: ''
+} as const;
+
+export const MEDICINE_MEMORY_MODULE_LEVEL2: Item = {
+  id: 'MemoryModule_Level2',
+  name: 'Memory Module Level 2',
+  picture: ''
+} as const;
+
+export const MEDICINE_MEMORY_MODULE_LEVEL3: Item = {
+  id: 'MemoryModule_Level3',
+  name: 'Memory Module Level 3',
+  picture: ''
+} as const;
+
+export const MEDICINE_MEMORY_MODULE_LEVEL4: Item = {
+  id: 'MemoryModule_Level4',
+  name: 'Memory Module Level 4',
+  picture: ''
+} as const;
+
+export const MEDICINE_PHOENIX_TEARS: Item = {
+  id: 'Phoenix_Tears',
+  name: 'Phoenix Tears',
+  picture: ''
+} as const;
+
+export const MEDICINE_PHOENIX_TEARS_SMALL: Item = {
+  id: 'Phoenix_Tears_Small',
+  name: 'Phoenix Tears Small',
+  picture: ''
+} as const;
+
+export const MEDICINE_PAINKILLERS_01: Item = {
+  id: 'Painkillers_01',
+  name: 'Painkillers Small',
+  picture: ''
+} as const;
+
+export const MEDICINE_PAINKILLERS_02: Item = {
+  id: 'Painkillers_02',
+  name: 'Painkillers Medium',
+  picture: ''
+} as const;
+
+export const MEDICINE_PAINKILLERS_03: Item = {
+  id: 'PainKillers_03',
+  name: 'PainKillers Big',
+  picture: ''
+} as const;
+
+export const MEDICINE_PRESSURE_DRESSING: Item = {
+  id: 'Pressure_Dressing',
+  name: 'Pressure Dressing',
+  picture: ''
+} as const;
+
+export const MEDICINE_RAGS: Item = {
+  id: 'Rags',
+  name: 'Rags',
+  picture: ''
+} as const;
+
+export const MEDICINE_SYRINGE: Item = {
+  id: 'Syringe',
+  name: 'Syringe',
+  picture: ''
+} as const;
+
+export const MEDICINE_TOURNIQUET: Item = {
+  id: 'Tourniquet',
+  name: 'Tourniquet',
+  picture: ''
+} as const;
+
+export const MEDICINE_VITAMINS_01: Item = {
+  id: 'Vitamins_01',
+  name: 'Vitamins Small',
+  picture: ''
+} as const;
+
+export const MEDICINE_VITAMINS_02: Item = {
+  id: 'Vitamins_02',
+  name: 'Vitamins Medium',
+  picture: ''
+} as const;
+
+export const MEDICINE_VITAMINS_03: Item = {
+  id: 'Vitamins_03',
+  name: 'Vitamins Big',
+  picture: ''
+} as const;
