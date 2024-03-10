1e57da0d428bda7928f447e30bc60074063ba7a6
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-11 00:10:24 +0200

    Add shirts to the items dropdown

---
+++ src/app/items/clothes/shirts.ts
@@ -0,0 +1,235 @@
+import { Item } from "../item.ts";
+
+export const SHIRT_F_UNDERSHIRT_BRA_01: Item = {
+  id: 'F_Undershirt_Bra_01',
+  name: 'Female Undershirt Bra 01',
+  picture: ''
+} as const;
+
+export const SHIRT_F_UNDERSHIRT_BRA_02: Item = {
+  id: 'F_Undershirt_Bra_02',
+  name: 'Female Undershirt Bra 02',
+  picture: ''
+} as const;
+
+export const SHIRT_F_UNDERSHIRT_BRA_03: Item = {
+  id: 'F_Undershirt_Bra_03',
+  name: 'Female Undershirt Bra 03',
+  picture: ''
+} as const;
+
+export const SHIRT_F_UNDERSHIRT_BRA_04: Item = {
+  id: 'F_Undershirt_Bra_04',
+  name: 'Female Undershirt Bra 04',
+  picture: ''
+} as const;
+
+export const SHIRT_F_UNDERSHIRT_BRA_05: Item = {
+  id: 'F_Undershirt_Bra_05',
+  name: 'Female Undershirt Bra 05',
+  picture: ''
+} as const;
+
+export const SHIRT_CLASSIC_01: Item = {
+  id: 'Classic_Shirt_01',
+  name: 'Classic Shirt 01',
+  picture: ''
+} as const;
+
+export const SHIRT_CLASSIC_02: Item = {
+  id: 'Classic_Shirt_02',
+  name: 'Classic Shirt 02',
+  picture: ''
+} as const;
+
+export const SHIRT_HIPPIE_01: Item = {
+  id: 'Hippie_Shirt_01',
+  name: 'Hippie Shirt 01',
+  picture: ''
+} as const;
+
+export const SHIRT_HIPPIE_02: Item = {
+  id: 'Hippie_Shirt_02',
+  name: 'Hippie Shirt 02',
+  picture: ''
+} as const;
+
+export const SHIRT_HOSPITAL: Item = {
+  id: 'Hospital_Shirt_01',
+  name: 'Hospital Shirt',
+  picture: ''
+} as const;
+
+export const SHIRT_INMATE: Item = {
+  id: 'Inmate_shirt_01',
+  name: 'Inmate Shirt',
+  picture: ''
+} as const;
+
+export const SHIRT_MILITARY_01: Item = {
+  id: 'Military_Shirt_01',
+  name: 'Military Shirt 01',
+  picture: ''
+} as const;
+
+export const SHIRT_MILITARY_02: Item = {
+  id: 'Military_Shirt_02',
+  name: 'Military Shirt 02',
+  picture: ''
+} as const;
+
+export const SHIRT_MILITARY_03: Item = {
+  id: 'Military_Shirt_03',
+  name: 'Military Shirt 03',
+  picture: ''
+} as const;
+
+export const SHIRT_MILITARY_04: Item = {
+  id: 'Military_Shirt_04',
+  name: 'Military Shirt 04',
+  picture: ''
+} as const;
+
+export const SHIRT_MILITARY_05: Item = {
+  id: 'Military_Shirt_05',
+  name: 'Military Shirt 05',
+  picture: ''
+} as const;
+
+export const SHIRT_MILITARY_06: Item = {
+  id: 'Military_Shirt_06',
+  name: 'Military Shirt 06',
+  picture: ''
+} as const;
+
+export const SHIRT_MILITARY_07: Item = {
+  id: 'Military_Shirt_07',
+  name: 'Military Shirt 07',
+  picture: ''
+} as const;
+
+export const SHIRT_PAJAMA: Item = {
+  id: 'Pajama_Shirt_01',
+  name: 'Pajama Shirt',
+  picture: ''
+} as const;
+
+export const SHIRT_POLICE: Item = {
+  id: 'Police_Shirt_01',
+  name: 'Police Shirt',
+  picture: ''
+} as const;
+
+export const SHIRT_01: Item = {
+  id: 'Shirt_01',
+  name: 'Shirt 01',
+  picture: ''
+} as const;
+
+export const SHIRT_02: Item = {
+  id: 'Shirt_02',
+  name: 'Shirt 02',
+  picture: ''
+} as const;
+
+export const SHIRT_03: Item = {
+  id: 'Shirt_03',
+  name: 'Shirt 03',
+  picture: ''
+} as const;
+
+export const SHIRT_SLEEVELESS_CAMO_01: Item = {
+  id: 'Sleeveless_Camo_Shirt_01',
+  name: 'Sleeveless Camo Shirt 01',
+  picture: ''
+} as const;
+
+export const SHIRT_SLEEVELESS_CAMO_02: Item = {
+  id: 'Sleeveless_Camo_Shirt_02',
+  name: 'Sleeveless Camo Shirt 02',
+  picture: ''
+} as const;
+
+export const SHIRT_SLEEVELESS_CAMO_03: Item = {
+  id: 'Sleeveless_Camo_Shirt_03',
+  name: 'Sleeveless Camo Shirt 03',
+  picture: ''
+} as const;
+
+export const SHIRT_SLEEVELESS_CAMO_04: Item = {
+  id: 'Sleeveless_Camo_Shirt_04',
+  name: 'Sleeveless Camo Shirt 04',
+  picture: ''
+} as const;
+
+export const SHIRT_SLEEVELESS_CAMO_05: Item = {
+  id: 'Sleeveless_Camo_Shirt_05',
+  name: 'Sleeveless Camo Shirt 05',
+  picture: ''
+} as const;
+
+export const SHIRT_SLEEVELESS_01: Item = {
+  id: 'Sleeveless_Shirt_01',
+  name: 'Sleeveless Shirt 01',
+  picture: ''
+} as const;
+
+export const SHIRT_SLEEVELESS_02: Item = {
+  id: 'Sleeveless_Shirt_02',
+  name: 'Sleeveless Shirt 02',
+  picture: ''
+} as const;
+
+export const SHIRT_TANG_01: Item = {
+  id: 'Tang_shirt_01',
+  name: 'Tang Shirt 01',
+  picture: ''
+} as const;
+
+export const SHIRT_TANG_02: Item = {
+  id: 'Tang_shirt_02',
+  name: 'Tang Shirt 02',
+  picture: ''
+} as const;
+
+export const SHIRT_TANG_03: Item = {
+  id: 'Tang_shirt_03',
+  name: 'Tang Shirt 03',
+  picture: ''
+} as const;
+
+export const SHIRT_TANG_04: Item = {
+  id: 'Tang_shirt_04',
+  name: 'Tang Shirt 04',
+  picture: ''
+} as const;
+
+export const SHIRT_TANG_05: Item = {
+  id: 'Tang_shirt_05',
+  name: 'Tang Shirt 05',
+  picture: ''
+} as const;
+
+export const SHIRT_UNDERSHIRT: Item = {
+  id: 'Undershirt_01',
+  name: 'Undershirt',
+  picture: ''
+} as const;
+
+export const SHIRT_WOODCUTTERS_01: Item = {
+  id: 'Woodcuters_Shirt_01',
+  name: 'Woodcutters Shirt 01',
+  picture: ''
+} as const;
+
+export const SHIRT_WOODCUTTERS_02: Item = {
+  id: 'Woodcuters_Shirt_02',
+  name: 'Woodcutters Shirt 02',
+  picture: ''
+} as const;
+
+export const SHIRT_WOODCUTTERS_03: Item = {
+  id: 'Woodcuters_Shirt_03',
+  name: 'Woodcutters Shirt 03',
+  picture: ''
+} as const;
