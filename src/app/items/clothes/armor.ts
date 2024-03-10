00ea7ca2635cc932af7b7ffc94964f1d85b907a7
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-10 18:24:59 +0200

    Add armor to the items dropdown

---
+++ src/app/items/clothes/armor.ts
@@ -0,0 +1,313 @@
+import { Item } from "../item.ts";
+
+export const ARMOR_POLICE_VEST_01: Item = {
+  id: 'Armor_Police_Vest_01',
+  name: 'Armor Police Vest',
+  picture: ''
+} as const;
+
+export const ARMOR_TACTICAL_VEST_01_01: Item = {
+  id: 'Armor_Tactical_Vest_01_01',
+  name: 'Armor Tactical Vest 01',
+  picture: ''
+} as const;
+
+export const ARMOR_TACTICAL_VEST_01_02: Item = {
+  id: 'Armor_Tactical_Vest_01_02',
+  name: 'Armor Tactical Vest 02',
+  picture: ''
+} as const;
+
+export const ARMOR_TACTICAL_VEST_01_03: Item = {
+  id: 'Armor_Tactical_Vest_01_03',
+  name: 'Armor Tactical Vest 03',
+  picture: ''
+} as const;
+
+export const ARMOR_TACTICAL_VEST_01_04: Item = {
+  id: 'Armor_Tactical_Vest_01_04',
+  name: 'Armor Tactical Vest 04',
+  picture: ''
+} as const;
+
+export const ARMOR_TACTICAL_VEST_01_05: Item = {
+  id: 'Armor_Tactical_Vest_01_05',
+  name: 'Armor Tactical Vest 05',
+  picture: ''
+} as const;
+
+export const ARMOR_BULLETPROOF_VEST_01: Item = {
+  id: 'Bulletproof_Vest_01',
+  name: 'Bulletproof Vest 01',
+  picture: ''
+} as const;
+
+export const ARMOR_BULLETPROOF_VEST_01_02: Item = {
+  id: 'Bulletproof_Vest_01_02',
+  name: 'Bulletproof Vest 02',
+  picture: ''
+} as const;
+
+export const ARMOR_BULLETPROOF_VEST_01_03: Item = {
+  id: 'Bulletproof_Vest_01_03',
+  name: 'Bulletproof Vest 03',
+  picture: ''
+} as const;
+
+export const ARMOR_BULLETPROOF_VEST_01_04: Item = {
+  id: 'Bulletproof_Vest_01_04',
+  name: 'Bulletproof Vest 04',
+  picture: ''
+} as const;
+
+export const ARMOR_BULLETPROOF_VEST_01_05: Item = {
+  id: 'Bulletproof_Vest_01_05',
+  name: 'Bulletproof Vest 05',
+  picture: ''
+} as const;
+
+export const ARMOR_COMBAT_HELMET_01: Item = {
+  id: 'Combat_Helmet_01',
+  name: 'Combat Helmet 01',
+  picture: ''
+} as const;
+
+export const ARMOR_COMBAT_HELMET_02: Item = {
+  id: 'Combat_Helmet_02',
+  name: 'Combat Helmet 02',
+  picture: ''
+} as const;
+
+export const ARMOR_COMBAT_HELMET_03: Item = {
+  id: 'Combat_Helmet_03',
+  name: 'Combat Helmet 03',
+  picture: ''
+} as const;
+
+export const ARMOR_COMBAT_HELMET_04: Item = {
+  id: 'Combat_Helmet_04',
+  name: 'Combat Helmet 04',
+  picture: ''
+} as const;
+
+export const ARMOR_K6_3_HELMET: Item = {
+  id: 'K6-3_Helmet',
+  name: 'K6-3 Helmet',
+  picture: ''
+} as const;
+
+export const ARMOR_MILITARY_HELMET_01_01: Item = {
+  id: 'Military_Helmet_01_01',
+  name: 'Military Helmet 01',
+  picture: ''
+} as const;
+
+export const ARMOR_MILITARY_HELMET_01_02: Item = {
+  id: 'Military_Helmet_01_02',
+  name: 'Military Helmet 02',
+  picture: ''
+} as const;
+
+export const ARMOR_MILITARY_HELMET_01_03: Item = {
+  id: 'Military_Helmet_01_03',
+  name: 'Military Helmet 03',
+  picture: ''
+} as const;
+
+export const ARMOR_MILITARY_HELMET_01_04: Item = {
+  id: 'Military_Helmet_01_04',
+  name: 'Military Helmet 04',
+  picture: ''
+} as const;
+
+export const ARMOR_MILITARY_HELMET_01_05: Item = {
+  id: 'Military_Helmet_01_05',
+  name: 'Military Helmet 05',
+  picture: ''
+} as const;
+
+export const ARMOR_MILITARY_HELMET_01_06: Item = {
+  id: 'Military_Helmet_01_06',
+  name: 'Military Helmet 06',
+  picture: ''
+} as const;
+
+export const ARMOR_MILITARY_HELMET_01_07: Item = {
+  id: 'Military_Helmet_01_07',
+  name: 'Military Helmet 07',
+  picture: ''
+} as const;
+
+export const ARMOR_MILITARY_HELMET_01_08: Item = {
+  id: 'Military_Helmet_01_08',
+  name: 'Military Helmet 08',
+  picture: ''
+} as const;
+
+export const ARMOR_MILITARY_HELMET_01_09: Item = {
+  id: 'Military_Helmet_01_09',
+  name: 'Military Helmet 09',
+  picture: ''
+} as const;
+
+export const ARMOR_POLICE_HELMET_01: Item = {
+  id: 'Police_Helmet_01',
+  name: 'Police Helmet',
+  picture: ''
+} as const;
+
+export const ARMOR_EXTREME_HELMET_01: Item = {
+  id: 'Extreme_Helmet_01',
+  name: 'Extreme Helmet',
+  picture: ''
+} as const;
+
+export const ARMOR_RACING_HELMET_01: Item = {
+  id: 'Racing_Helmet_01',
+  name: 'Racing Helmet',
+  picture: ''
+} as const;
+
+export const ARMOR_RETRO_HELMET_01: Item = {
+  id: 'Retro_Helmet_01',
+  name: 'Retro Helmet',
+  picture: ''
+} as const;
+
+export const ARMOR_RIDER_HELMET_01: Item = {
+  id: 'Rider_Helmet_01',
+  name: 'Rider Helmet 01',
+  picture: ''
+} as const;
+
+export const ARMOR_RIDER_HELMET_02: Item = {
+  id: 'Rider_Helmet_02',
+  name: 'Rider Helmet 02',
+  picture: ''
+} as const;
+
+export const ARMOR_RIDER_HELMET_03: Item = {
+  id: 'Rider_Helmet_03',
+  name: 'Rider Helmet 03',
+  picture: ''
+} as const;
+
+export const ARMOR_FIREMAN_HELMET_01: Item = {
+  id: 'Fireman_Helmet_01',
+  name: 'Fireman Helmet',
+  picture: ''
+} as const;
+
+export const ARMOR_M1_MEDICAL_HELMET: Item = {
+  id: 'M1_Medical_Helmet',
+  name: 'M1 Medical Helmet',
+  picture: ''
+} as const;
+
+export const ARMOR_M1_VIETNAM_HELMET: Item = {
+  id: 'M1_Vietnam_Helmet',
+  name: 'M1 Vietnam Helmet',
+  picture: ''
+} as const;
+
+export const ARMOR_MEDIEVAL_HELMET_01: Item = {
+  id: 'Medieval_Helmet_01',
+  name: 'Medieval Helmet',
+  picture: ''
+} as const;
+
+export const ARMOR_RIOT_HELMET_02: Item = {
+  id: 'Riot_Helmet_02',
+  name: 'Riot Helmet',
+  picture: ''
+} as const;
+
+export const ARMOR_WORKING_HELMET_01: Item = {
+  id: 'Working_Helmet_01',
+  name: 'Working Helmet 01',
+  picture: ''
+} as const;
+
+export const ARMOR_WORKING_HELMET_02: Item = {
+  id: 'Working_Helmet_02',
+  name: 'Working Helmet 02',
+  picture: ''
+} as const;
+
+export const ARMOR_WORKING_HELMET_03: Item = {
+  id: 'Working_Helmet_03',
+  name: 'Working Helmet 03',
+  picture: ''
+} as const;
+
+export const ARMOR_WORKING_HELMET_04: Item = {
+  id: 'Working_Helmet_04',
+  name: 'Working Helmet 04',
+  picture: ''
+} as const;
+
+export const ARMOR_WW2_GERMAN_HELMET_01: Item = {
+  id: 'WW2_German_Helmet_01',
+  name: 'WW2 German Helmet 01',
+  picture: ''
+} as const;
+
+export const ARMOR_POLICE_BULLETPROOF_VEST_01: Item = {
+  id: 'Police_Bulletproof_Vest_01',
+  name: 'Police Bulletproof Vest',
+  picture: ''
+} as const;
+
+export const ARMOR_STABP_ROOF_VEST_01_01: Item = {
+  id: 'Stabproof_Vest_01_01',
+  name: 'Stabp-roof Vest 01',
+  picture: ''
+} as const;
+
+export const ARMOR_STABP_ROOF_VEST_01_02: Item = {
+  id: 'Stabproof_Vest_01_02',
+  name: 'Stabp-roof Vest 02',
+  picture: ''
+} as const;
+
+export const ARMOR_STAB_PROOF_VEST_01_03: Item = {
+  id: 'Stabproof_Vest_01_03',
+  name: 'Stab-proof Vest 03',
+  picture: ''
+} as const;
+
+export const ARMOR_STAB_PROOF_VEST_01_04: Item = {
+  id: 'Stabproof_Vest_01_04',
+  name: 'Stab-proof Vest 04',
+  picture: ''
+} as const;
+
+export const ARMOR_STAB_PROOF_VEST_01_05: Item = {
+  id: 'Stabproof_Vest_01_05',
+  name: 'Stab-proof Vest 05',
+  picture: ''
+} as const;
+
+export const ARMOR_STAB_PROOF_VEST_01_06: Item = {
+  id: 'Stabproof_Vest_01_06',
+  name: 'Stab-proof Vest 06',
+  picture: ''
+} as const;
+
+export const ARMOR_STAB_PROOF_VEST_01_07: Item = {
+  id: 'Stabproof_Vest_01_07',
+  name: 'Stab-proof Vest 07',
+  picture: ''
+} as const;
+
+export const ARMOR_STAB_PROOF_VEST_01_08: Item = {
+  id: 'Stabproof_Vest_01_08',
+  name: 'Stab-proof Vest 08',
+  picture: ''
+} as const;
+
+export const ARMOR_STAB_PROOF_VEST_01_09: Item = {
+  id: 'Stabproof_Vest_01_09',
+  name: 'Stab-proof Vest 09',
+  picture: ''
+} as const;
