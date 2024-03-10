d45d3ebfca497d51be2fa999b9dfc2880822fbe4
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-10 17:37:39 +0200

    Add drinks to the items dropdown

---
+++ src/app/items/food/drinks.ts
@@ -0,0 +1,151 @@
+import { Item } from "../item.ts";
+
+export const DRINKS_ABSINTHE: Item = {
+  id: 'Absinthe',
+  name: 'Absinthe',
+  picture: ''
+} as const;
+
+export const DRINKS_BEER: Item = {
+  id: 'Beer',
+  name: 'Beer',
+  picture: ''
+} as const;
+
+export const DRINKS_COFFEE_CUP: Item = {
+  id: 'Coffee_Cup',
+  name: 'Coffee Cup',
+  picture: ''
+} as const;
+
+export const DRINKS_ENERGY_DRINK_RED_GHOUL: Item = {
+  id: 'Energy_Drink_Red_Ghoul',
+  name: 'Energy Drink Red Ghoul',
+  picture: ''
+} as const;
+
+export const DRINKS_KICMOVACA: Item = {
+  id: 'Kicmovaca',
+  name: 'Kicmovaca',
+  picture: ''
+} as const;
+
+export const DRINKS_MILK: Item = {
+  id: 'Milk',
+  name: 'Milk',
+  picture: ''
+} as const;
+
+export const DRINKS_PET_BOTTLE_01: Item = {
+  id: 'PETBottle01',
+  name: 'PET Bottle 01',
+  picture: ''
+} as const;
+
+export const DRINKS_PET_BOTTLE_02: Item = {
+  id: 'PETBottle02',
+  name: 'PET Bottle 02',
+  picture: ''
+} as const;
+
+export const DRINKS_PET_BOTTLE_03: Item = {
+  id: 'PETBottle03',
+  name: 'PET Bottle 03',
+  picture: ''
+} as const;
+
+export const DRINKS_PET_BOTTLE_04: Item = {
+  id: 'PETBottle04',
+  name: 'PET Bottle 04',
+  picture: ''
+} as const;
+
+export const DRINKS_SCUM_SUNRISE: Item = {
+  id: 'Scum_Sunrise',
+  name: 'Scum Sunrise',
+  picture: ''
+} as const;
+
+export const DRINKS_SCUMKIN_SPICE_LATTE: Item = {
+  id: 'SCUMkin_Spice_Latte',
+  name: 'SCUMkin Spice Latte',
+  picture: ''
+} as const;
+
+export const DRINKS_SLAV_REVIVER: Item = {
+  id: 'Slav_Reviver',
+  name: 'Slav Reviver',
+  picture: ''
+} as const;
+
+export const DRINKS_SLAVIC_GENT: Item = {
+  id: 'Slavic_Gent',
+  name: 'Slavic Gent',
+  picture: ''
+} as const;
+
+export const DRINKS_THE_ISLAND_SPECIAL: Item = {
+  id: 'The_Island_Special',
+  name: 'The Island Special',
+  picture: ''
+} as const;
+
+export const DRINKS_TEQUILA: Item = {
+  id: 'Tequila',
+  name: 'Tequila',
+  picture: ''
+} as const;
+
+export const DRINKS_VODKA: Item = {
+  id: 'Vodka',
+  name: 'Vodka',
+  picture: ''
+} as const;
+
+export const DRINKS_WATER_05L: Item = {
+  id: 'Water_05l',
+  name: 'Water 0.5l',
+  picture: ''
+} as const;
+
+export const DRINKS_WHISKEY: Item = {
+  id: 'Whiskey',
+  name: 'Whiskey',
+  picture: ''
+} as const;
+
+export const DRINKS_WINE: Item = {
+  id: 'Wine',
+  name: 'Wine',
+  picture: ''
+} as const;
+
+export const DRINKS_CAN01: Item = {
+  id: 'Can01',
+  name: 'Can 01',
+  picture: ''
+} as const;
+
+export const DRINKS_CAN02: Item = {
+  id: 'Can02',
+  name: 'Can 02',
+  picture: ''
+} as const;
+
+export const DRINKS_CAN03: Item = {
+  id: 'Can03',
+  name: 'Can 03',
+  picture: ''
+} as const;
+
+export const DRINKS_CAN04: Item = {
+  id: 'Can04',
+  name: 'Can 04',
+  picture: ''
+} as const;
+
+export const DRINKS_CANTEEN: Item = {
+  id: 'Canteen',
+  name: 'Canteen',
+  picture: ''
+} as const;
