0e52334d76a79a154fa9e38961d3db0be527366e
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-10 21:06:07 +0200

    Add vehicle repair kits, wheels and batteries to the items dropdown

---
+++ src/app/items/vehicles/vehicle-misc.ts
@@ -0,0 +1,79 @@
+import { Item } from "../item.ts";
+
+export const VEHICLE_MISC_CAR_BATTERY: Item = {
+  id: 'Car_Battery',
+  name: 'Car Battery',
+  picture: ''
+} as const;
+
+export const VEHICLE_MISC_CAR_BATTERY_CABLES: Item = {
+  id: 'Car_Battery_Cables',
+  name: 'Car Battery Cables',
+  picture: ''
+} as const;
+
+export const VEHICLE_MISC_CAR_JACK: Item = {
+  id: 'Car_Jack',
+  name: 'Car Jack',
+  picture: ''
+} as const;
+
+export const VEHICLE_MISC_CAR_REPAIR_KIT: Item = {
+  id: 'Car_Repair_Kit',
+  name: 'Car Repair Kit',
+  picture: ''
+} as const;
+
+export const VEHICLE_MISC_TIRE_REPAIR_KIT: Item = {
+  id: 'Tire_Repair_Kit',
+  name: 'Tire Repair Kit',
+  picture: ''
+} as const;
+
+export const VEHICLE_MISC_AEROPLANE_REPAIR_KIT: Item = {
+  id: 'Aeroplane_Repair_Kit',
+  name: 'Aeroplane Repair Kit',
+  picture: ''
+} as const;
+
+export const VEHICLE_MISC_BICYCLE_WHEEL_54_622_ITEM: Item = {
+  id: 'Bicycle_Wheel_54_622_Item',
+  name: 'Bicycle Wheel 54 622',
+  picture: ''
+} as const;
+
+export const VEHICLE_MISC_BICYCLE_WHEEL_60_622_ITEM: Item = {
+  id: 'Bicycle_Wheel_60_622_Item',
+  name: 'Bicycle Wheel 60 622',
+  picture: ''
+} as const;
+
+export const VEHICLE_MISC_DIRT_BIKE_WHEEL_110_80_R19_ITEM: Item = {
+  id: 'Dirtbike_Wheel_110_80_R19_Item',
+  name: 'Dirt bike Wheel 110 80 R19',
+  picture: ''
+} as const;
+
+export const VEHICLE_MISC_DIRT_BIKE_WHEEL_90_90_R21_ITEM: Item = {
+  id: 'Dirtbike_Wheel_90_90_R21_Item',
+  name: 'Dirt bike Wheel 90 90 R21',
+  picture: ''
+} as const;
+
+export const VEHICLE_MISC_WHEEL_155_R65_ITEM: Item = {
+  id: 'Wheel_155_R65_Item',
+  name: 'Wheel 155 R65',
+  picture: ''
+} as const;
+
+export const VEHICLE_MISC_WHEEL_255_55_R16_ITEM: Item = {
+  id: 'Wheel_255_55_R16_Item',
+  name: 'Wheel 255 55 R16',
+  picture: ''
+} as const;
+
+export const VEHICLE_MISC_WHEEL_265_60_R18_ITEM: Item = {
+  id: 'Wheel_265_60_R18_Item',
+  name: 'Wheel 265 60 R18',
+  picture: ''
+} as const;
