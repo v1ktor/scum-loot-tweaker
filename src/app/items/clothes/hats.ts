72c2a39ee355d8e8af80f85781dd4ae8ad3ac507
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-19 21:08:55 +0200

    Add hats to the items dropdown

---
+++ src/app/items/clothes/hats.ts
@@ -0,0 +1,481 @@
+import { Item } from "../item.ts";
+
+export const HATS_MILITARY_BEANIE_01: Item = {
+  id: 'Military_Beanie_01',
+  name: 'Military Beanie 01',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_BEANIE_02: Item = {
+  id: 'Military_Beanie_02',
+  name: 'Military Beanie 02',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_BEANIE_03: Item = {
+  id: 'Military_Beanie_03',
+  name: 'Military Beanie 03',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_BEANIE_04: Item = {
+  id: 'Military_Beanie_04',
+  name: 'Military Beanie 04',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_BEANIE_05: Item = {
+  id: 'Military_Beanie_05',
+  name: 'Military Beanie 05',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_BEANIE_06: Item = {
+  id: 'Military_Beanie_06',
+  name: 'Military Beanie 06',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_BEANIE_07: Item = {
+  id: 'Military_Beanie_07',
+  name: 'Military Beanie 07',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_BEANIE_08: Item = {
+  id: 'Military_Beanie_08',
+  name: 'Military Beanie 08',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_BOONIE_HAT_01: Item = {
+  id: 'Military_Boonie_Hat_01',
+  name: 'Military Boonie Hat 01',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_BOONIE_HAT_02: Item = {
+  id: 'Military_Boonie_Hat_02',
+  name: 'Military Boonie Hat 02',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_BOONIE_HAT_03: Item = {
+  id: 'Military_Boonie_Hat_03',
+  name: 'Military Boonie Hat 03',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_BOONIE_HAT_04: Item = {
+  id: 'Military_Boonie_Hat_04',
+  name: 'Military Boonie Hat 04',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_BOONIE_HAT_05: Item = {
+  id: 'Military_Boonie_Hat_05',
+  name: 'Military Boonie Hat 05',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_BOONIE_HAT_06: Item = {
+  id: 'Military_Boonie_Hat_06',
+  name: 'Military Boonie Hat 06',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_BOONIE_HAT_07: Item = {
+  id: 'Military_Boonie_Hat_07',
+  name: 'Military Boonie Hat 07',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_BOONIE_HAT_08: Item = {
+  id: 'Military_Boonie_Hat_08',
+  name: 'Military Boonie Hat 08',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_CAP_01: Item = {
+  id: 'Military_Cap_01',
+  name: 'Military Cap 01',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_CAP_02: Item = {
+  id: 'Military_Cap_02',
+  name: 'Military Cap 02',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_CAP_03: Item = {
+  id: 'Military_Cap_03',
+  name: 'Military Cap 03',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_CAP_04: Item = {
+  id: 'Military_Cap_04',
+  name: 'Military Cap 04',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_CAP_05: Item = {
+  id: 'Military_Cap_05',
+  name: 'Military Cap 05',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_CAP_06: Item = {
+  id: 'Military_Cap_06',
+  name: 'Military Cap 06',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_CAP_07: Item = {
+  id: 'Military_Cap_07',
+  name: 'Military Cap 07',
+  picture: ''
+} as const;
+
+export const HATS_MILITARY_CAP_08: Item = {
+  id: 'Military_Cap_08',
+  name: 'Military Cap 08',
+  picture: ''
+} as const;
+
+export const HATS_PILOT_HAT: Item = {
+  id: 'Pilot_Hat_01',
+  name: 'Pilot Hat',
+  picture: ''
+} as const;
+
+export const HATS_PIRATE_HAT: Item = {
+  id: 'Pirate_Hat_01',
+  name: 'Pirate Hat',
+  picture: ''
+} as const;
+
+export const HATS_PIRATE_HAT_SPARROW: Item = {
+  id: 'Pirate_Hat_Sparrow_02',
+  name: 'Pirate Hat Sparrow',
+  picture: ''
+} as const;
+
+export const HATS_POLICE_BEANIE: Item = {
+  id: 'Police_Beanie_01',
+  name: 'Police Beanie',
+  picture: ''
+} as const;
+
+export const HATS_POLICE_BOONIE_HAT: Item = {
+  id: 'Police_Boonie_Hat_01',
+  name: 'Police Boonie Hat',
+  picture: ''
+} as const;
+
+export const HATS_POLICE_CAP_01: Item = {
+  id: 'Police_Cap_01',
+  name: 'Police Cap 01',
+  picture: ''
+} as const;
+
+export const HATS_POLICE_CAP_02: Item = {
+  id: 'Police_Cap_02',
+  name: 'Police Cap 02',
+  picture: ''
+} as const;
+
+export const HATS_REDNECK_HAT: Item = {
+  id: 'Redneck_Hat_07',
+  name: 'Redneck Hat',
+  picture: ''
+} as const;
+
+export const HATS_SANTA_HAT: Item = {
+  id: 'Santa_Hat_01',
+  name: 'Santa Hat',
+  picture: ''
+} as const;
+
+export const HATS_SLOUCH_CAMO_HAT_01: Item = {
+  id: 'Slouch_Camo_Hat_01',
+  name: 'Slouch Camo Hat 01',
+  picture: ''
+} as const;
+
+export const HATS_SLOUCH_CAMO_HAT_02: Item = {
+  id: 'Slouch_Camo_Hat_02',
+  name: 'Slouch Camo Hat 02',
+  picture: ''
+} as const;
+
+export const HATS_SLOUCH_CAMO_HAT_03: Item = {
+  id: 'Slouch_Camo_Hat_03',
+  name: 'Slouch Camo Hat 03',
+  picture: ''
+} as const;
+
+export const HATS_SLOUCH_CAMO_HAT_04: Item = {
+  id: 'Slouch_Camo_Hat_04',
+  name: 'Slouch Camo Hat 04',
+  picture: ''
+} as const;
+
+export const HATS_SLOUCH_CAMO_HAT_05: Item = {
+  id: 'Slouch_Camo_Hat_05',
+  name: 'Slouch Camo Hat 05',
+  picture: ''
+} as const;
+
+export const HATS_SLOUCH_HAT_01: Item = {
+  id: 'Slouch_Hat_01',
+  name: 'Slouch Hat 01',
+  picture: ''
+} as const;
+
+export const HATS_SLOUCH_HAT_02: Item = {
+  id: 'Slouch_Hat_02',
+  name: 'Slouch Hat 02',
+  picture: ''
+} as const;
+
+export const HATS_BAMBOO_HAT_01: Item = {
+  id: 'Bamboo_Hat_01',
+  name: 'Bamboo Hat 01',
+  picture: ''
+} as const;
+
+export const HATS_BAMBOO_HAT_02: Item = {
+  id: 'Bamboo_Hat_02',
+  name: 'Bamboo Hat 02',
+  picture: ''
+} as const;
+
+export const HATS_BOONIE_HAT_01: Item = {
+  id: 'Boonie_Hat_01',
+  name: 'Boonie Hat 01',
+  picture: ''
+} as const;
+
+export const HATS_BOONIE_HAT_02: Item = {
+  id: 'Boonie_Hat_02',
+  name: 'Boonie Hat 02',
+  picture: ''
+} as const;
+
+export const HATS_BOONIE_HAT_03: Item = {
+  id: 'Boonie_Hat_03',
+  name: 'Boonie Hat 03',
+  picture: ''
+} as const;
+
+export const HATS_BOONIE_HAT_04: Item = {
+  id: 'Boonie_Hat_04',
+  name: 'Boonie Hat 04',
+  picture: ''
+} as const;
+
+export const HATS_BOONIE_HAT_05: Item = {
+  id: 'Boonie_Hat_05',
+  name: 'Boonie Hat 05',
+  picture: ''
+} as const;
+
+export const HATS_BOONIE_HAT_06: Item = {
+  id: 'Boonie_Hat_06',
+  name: 'Boonie Hat 06',
+  picture: ''
+} as const;
+
+export const HATS_BASEBALL_CAP_11: Item = {
+  id: 'Baseball_Cap_11',
+  name: 'Baseball Cap 11',
+  picture: ''
+} as const;
+
+export const HATS_BASEBALL_CAP_12: Item = {
+  id: 'Baseball_Cap_12',
+  name: 'Baseball Cap 12',
+  picture: ''
+} as const;
+
+export const HATS_BASEBALL_CAP_13: Item = {
+  id: 'Baseball_Cap_13',
+  name: 'Baseball Cap 13',
+  picture: ''
+} as const;
+
+export const HATS_BASEBALL_CAP_01: Item = {
+  id: 'Baseball_Cap_01',
+  name: 'Baseball Cap 01',
+  picture: ''
+} as const;
+
+export const HATS_BASEBALL_CAP_10: Item = {
+  id: 'Baseball_Cap_10',
+  name: 'Baseball Cap 10',
+  picture: ''
+} as const;
+
+export const HATS_BASEBALL_CAP_02: Item = {
+  id: 'Baseball_Cap_02',
+  name: 'Baseball Cap 02',
+  picture: ''
+} as const;
+
+export const HATS_BASEBALL_CAP_03: Item = {
+  id: 'Baseball_Cap_03',
+  name: 'Baseball Cap 03',
+  picture: ''
+} as const;
+
+export const HATS_BASEBALL_CAP_04: Item = {
+  id: 'Baseball_Cap_04',
+  name: 'Baseball Cap 04',
+  picture: ''
+} as const;
+
+export const HATS_BASEBALL_CAP_05: Item = {
+  id: 'Baseball_Cap_05',
+  name: 'Baseball Cap 05',
+  picture: ''
+} as const;
+
+export const HATS_BASEBALL_CAP_06: Item = {
+  id: 'Baseball_Cap_06',
+  name: 'Baseball Cap 06',
+  picture: ''
+} as const;
+
+export const HATS_BASEBALL_CAP_07: Item = {
+  id: 'Baseball_Cap_07',
+  name: 'Baseball Cap 07',
+  picture: ''
+} as const;
+
+export const HATS_BASEBALL_CAP_08: Item = {
+  id: 'Baseball_Cap_08',
+  name: 'Baseball Cap 08',
+  picture: ''
+} as const;
+
+export const HATS_BASEBALL_CAP_09: Item = {
+  id: 'Baseball_Cap_09',
+  name: 'Baseball Cap 09',
+  picture: ''
+} as const;
+
+export const HATS_BIRTHDAY_CAP: Item = {
+  id: 'Birthday_Cap_01',
+  name: 'Birthday Cap',
+  picture: ''
+} as const;
+
+export const HATS_RK_BEANIE: Item = {
+  id: 'RK_Beanie_01',
+  name: 'RK Beanie',
+  picture: ''
+} as const;
+
+export const HATS_CHRISTMAS_BEANIE: Item = {
+  id: 'Christmas_Beanie_02',
+  name: 'Christmas Beanie',
+  picture: ''
+} as const;
+
+export const HATS_MARINE_CAP_01: Item = {
+  id: 'Marine_Cap_01',
+  name: 'Marine Cap 01',
+  picture: ''
+} as const;
+
+export const HATS_MARINE_CAP_02: Item = {
+  id: 'Marine_Cap_02',
+  name: 'Marine Cap 02',
+  picture: ''
+} as const;
+
+export const HATS_MARINE_CAP_03: Item = {
+  id: 'Marine_Cap_03',
+  name: 'Marine Cap 03',
+  picture: ''
+} as const;
+
+export const HATS_MARINE_CAP_04: Item = {
+  id: 'Marine_Cap_04',
+  name: 'Marine Cap 04',
+  picture: ''
+} as const;
+
+export const HATS_MARINE_CAP_05: Item = {
+  id: 'Marine_Cap_05',
+  name: 'Marine Cap 05',
+  picture: ''
+} as const;
+
+export const HATS_MARINE_CAP_06: Item = {
+  id: 'Marine_Cap_06',
+  name: 'Marine Cap 06',
+  picture: ''
+} as const;
+
+export const HATS_MARINE_CAP_07: Item = {
+  id: 'Marine_Cap_07',
+  name: 'Marine Cap 07',
+  picture: ''
+} as const;
+
+export const HATS_BEANIE_01: Item = {
+  id: 'Beenie_01',
+  name: 'Beanie 01',
+  picture: ''
+} as const;
+
+export const HATS_BEANIE_02: Item = {
+  id: 'Beenie_02',
+  name: 'Beanie 02',
+  picture: ''
+} as const;
+
+export const HATS_BEANIE_03: Item = {
+  id: 'Beenie_03',
+  name: 'Beanie 03',
+  picture: ''
+} as const;
+
+export const HATS_BEANIE_04: Item = {
+  id: 'Beenie_04',
+  name: 'Beanie 04',
+  picture: ''
+} as const;
+
+export const HATS_BEANIE_05: Item = {
+  id: 'Beenie_05',
+  name: 'Beanie 05',
+  picture: ''
+} as const;
+
+export const HATS_BEANIE_06: Item = {
+  id: 'Beenie_06',
+  name: 'Beanie 06',
+  picture: ''
+} as const;
+
+export const HATS_BEANIE_07: Item = {
+  id: 'Beenie_07',
+  name: 'Beanie 07',
+  picture: ''
+} as const;
+
+export const HATS_BEANIE_08: Item = {
+  id: 'Beenie_08',
+  name: 'Beanie 08',
+  picture: ''
+} as const;
+
+export const HATS_BEANIE_09: Item = {
+  id: 'Beenie_09',
+  name: 'Beanie 09',
+  picture: ''
+} as const;
