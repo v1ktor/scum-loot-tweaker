4e221c35423d15fb419b4da3995a7071c2975245
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-19 21:34:08 +0200

    Add boots to the items dropdown

---
+++ src/app/items/clothes/boots.ts
@@ -0,0 +1,277 @@
+import { Item } from "../item.ts";
+
+export const BOOTS_06_01: Item = {
+  id: 'Boots_06_01',
+  name: 'Boots 06 01',
+  picture: ''
+} as const;
+
+export const BOOTS_06_02: Item = {
+  id: 'Boots_06_02',
+  name: 'Boots 06 02',
+  picture: ''
+} as const;
+
+export const BOOTS_07_01: Item = {
+  id: 'Boots_07_01',
+  name: 'Boots 07 01',
+  picture: ''
+} as const;
+
+export const BOOTS_07_02: Item = {
+  id: 'Boots_07_02',
+  name: 'Boots 07 02',
+  picture: ''
+} as const;
+
+export const BOOTS_07_03: Item = {
+  id: 'Boots_07_03',
+  name: 'Boots 07 03',
+  picture: ''
+} as const;
+
+export const BOOTS_COMBAT: Item = {
+  id: 'CombatBoots',
+  name: 'Combat Boots',
+  picture: ''
+} as const;
+
+export const BOOTS_HH: Item = {
+  id: 'HH_Boots_01',
+  name: 'HH Boots',
+  picture: ''
+} as const;
+
+export const BOOTS_HIKING_01: Item = {
+  id: 'Hiking_Boots_01',
+  name: 'Hiking Boots 01',
+  picture: ''
+} as const;
+
+export const BOOTS_HIKING_02: Item = {
+  id: 'Hiking_Boots_02',
+  name: 'Hiking Boots 02',
+  picture: ''
+} as const;
+
+export const BOOTS_HIKING_03: Item = {
+  id: 'Hiking_Boots_03',
+  name: 'Hiking Boots 03',
+  picture: ''
+} as const;
+
+export const BOOTS_HIKING_04: Item = {
+  id: 'Hiking_Boots_04',
+  name: 'Hiking Boots 04',
+  picture: ''
+} as const;
+
+export const BOOTS_MILITARY_JNA: Item = {
+  id: 'MilitaryBoots_JNA',
+  name: 'Military Boots JNA',
+  picture: ''
+} as const;
+
+export const BOOTS_MILITARY_PEACE: Item = {
+  id: 'MilitaryBoots_Peace',
+  name: 'Military Boots Peace',
+  picture: ''
+} as const;
+
+export const BOOTS_MOTORCYCLE_01: Item = {
+  id: 'Motorcycle_Boots_01',
+  name: 'Motorcycle Boots 01',
+  picture: ''
+} as const;
+
+export const BOOTS_MOTORCYCLE_02: Item = {
+  id: 'Motorcycle_Boots_02',
+  name: 'Motorcycle Boots 02',
+  picture: ''
+} as const;
+
+export const BOOTS_MOTORCYCLE_03: Item = {
+  id: 'Motorcycle_Boots_03',
+  name: 'Motorcycle Boots 03',
+  picture: ''
+} as const;
+
+export const BOOTS_RH_SHOE: Item = {
+  id: 'RH_Shoe_01',
+  name: 'RH Shoe',
+  picture: ''
+} as const;
+
+export const BOOTS_BEIJING_SHOES_01: Item = {
+  id: 'Beijing_Shoes_01',
+  name: 'Beijing Shoes 01',
+  picture: ''
+} as const;
+
+export const BOOTS_BEIJING_SHOES_02: Item = {
+  id: 'Beijing_Shoes_02',
+  name: 'Beijing Shoes 02',
+  picture: ''
+} as const;
+
+export const BOOTS_BEIJING_SHOES_03: Item = {
+  id: 'Beijing_Shoes_03',
+  name: 'Beijing Shoes 03',
+  picture: ''
+} as const;
+
+export const BOOTS_BEIJING_SHOES_04: Item = {
+  id: 'Beijing_Shoes_04',
+  name: 'Beijing Shoes 04',
+  picture: ''
+} as const;
+
+export const BOOTS_HH_SHOES_01_01: Item = {
+  id: 'HH_Shoes_01_01',
+  name: 'HH Shoes 01 01',
+  picture: ''
+} as const;
+
+export const BOOTS_HH_SHOES_02_01: Item = {
+  id: 'HH_Shoes_02_01',
+  name: 'HH Shoes 02 01',
+  picture: ''
+} as const;
+
+export const BOOTS_HH_SHOES_03_01: Item = {
+  id: 'HH_Shoes_03_01',
+  name: 'HH Shoes 03 01',
+  picture: ''
+} as const;
+
+export const BOOTS_HH_SHOES_04_01: Item = {
+  id: 'HH_Shoes_04_01',
+  name: 'HH Shoes 04 01',
+  picture: ''
+} as const;
+
+export const BOOTS_HH_SHOES_05_01: Item = {
+  id: 'HH_Shoes_05_01',
+  name: 'HH Shoes 05 01',
+  picture: ''
+} as const;
+
+export const BOOTS_HH_SHOES_01_02: Item = {
+  id: 'HH_Shoes_01_02',
+  name: 'HH Shoes 01 02',
+  picture: ''
+} as const;
+
+export const BOOTS_HH_SHOES_01_03: Item = {
+  id: 'HH_Shoes_01_03',
+  name: 'HH Shoes 01 03',
+  picture: ''
+} as const;
+
+export const BOOTS_HH_SHOES_01_04: Item = {
+  id: 'HH_Shoes_01_04',
+  name: 'HH Shoes 01 04',
+  picture: ''
+} as const;
+
+export const BOOTS_HH_SHOES_02_02: Item = {
+  id: 'HH_Shoes_02_02',
+  name: 'HH Shoes 02 02',
+  picture: ''
+} as const;
+
+export const BOOTS_HH_SHOES_02_03: Item = {
+  id: 'HH_Shoes_02_03',
+  name: 'HH Shoes 02 03',
+  picture: ''
+} as const;
+
+export const BOOTS_HH_SHOES_03_02: Item = {
+  id: 'HH_Shoes_03_02',
+  name: 'HH Shoes 03 02',
+  picture: ''
+} as const;
+
+export const BOOTS_HH_SHOES_03_03: Item = {
+  id: 'HH_Shoes_03_03',
+  name: 'HH Shoes 03 03',
+  picture: ''
+} as const;
+
+export const BOOTS_HH_SHOES_03_04: Item = {
+  id: 'HH_Shoes_03_04',
+  name: 'HH Shoes 03 04',
+  picture: ''
+} as const;
+
+export const BOOTS_HH_SHOES_03_05: Item = {
+  id: 'HH_Shoes_03_05',
+  name: 'HH Shoes 03 05',
+  picture: ''
+} as const;
+
+export const BOOTS_HH_SHOES_03_06: Item = {
+  id: 'HH_Shoes_03_06',
+  name: 'HH Shoes 03 06',
+  picture: ''
+} as const;
+
+export const BOOTS_HH_SHOES_03_07: Item = {
+  id: 'HH_Shoes_03_07',
+  name: 'HH Shoes 03 07',
+  picture: ''
+} as const;
+
+export const BOOTS_HH_SHOES_04_02: Item = {
+  id: 'HH_Shoes_04_02',
+  name: 'HH Shoes 04 02',
+  picture: ''
+} as const;
+
+export const BOOTS_HH_SHOES_04_03: Item = {
+  id: 'HH_Shoes_04_03',
+  name: 'HH Shoes 04 03',
+  picture: ''
+} as const;
+
+export const BOOTS_HH_SHOES_04_04: Item = {
+  id: 'HH_Shoes_04_04',
+  name: 'HH Shoes 04 04',
+  picture: ''
+} as const;
+
+export const BOOTS_HH_SHOES_05_02: Item = {
+  id: 'HH_Shoes_05_02',
+  name: 'HH Shoes 05 02',
+  picture: ''
+} as const;
+
+export const BOOTS_HH_SHOES_05_03: Item = {
+  id: 'HH_Shoes_05_03',
+  name: 'HH Shoes 05 03',
+  picture: ''
+} as const;
+
+export const BOOTS_HIGH_TOP_SHOES: Item = {
+  id: 'HighTop_Shoes',
+  name: 'High Top Shoes',
+  picture: ''
+} as const;
+
+export const BOOTS_KILT_BROGUE_SHOE: Item = {
+  id: 'Kilt_Brogue_Shoe_01',
+  name: 'Kilt Brogue Shoe',
+  picture: ''
+} as const;
+
+export const BOOTS_LEATHER_SHOES_01: Item = {
+  id: 'Leather_Shoes',
+  name: 'Leather Shoes 01',
+  picture: ''
+} as const;
+
+export const BOOTS_LEATHER_SHOES_02: Item = {
+  id: 'Leather_Shoes_02',
+  name: 'Leather Shoes 02',
+  picture: ''
+} as const;
