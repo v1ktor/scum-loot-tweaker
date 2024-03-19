d0c7dbb6ccd7e4212e20ec3d12c1f7be24c94230
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-19 20:31:43 +0200

    Add gloves to the items dropdown

---
+++ src/app/items/clothes/gloves.ts
@@ -0,0 +1,211 @@
+import { Item } from "../item.ts";
+
+export const GLOVES_OPEN_FINGER_01: Item = {
+  id: 'Open_Finger_Gloves_01',
+  name: 'Open Finger Gloves 01',
+  picture: ''
+} as const;
+
+export const GLOVES_OPEN_FINGER_02: Item = {
+  id: 'Open_Finger_Gloves_02',
+  name: 'Open Finger Gloves 02',
+  picture: ''
+} as const;
+
+export const GLOVES_OPEN_FINGER_03: Item = {
+  id: 'Open_Finger_Gloves_03',
+  name: 'Open Finger Gloves 03',
+  picture: ''
+} as const;
+
+export const GLOVES_OPEN_FINGER_04: Item = {
+  id: 'Open_Finger_Gloves_04',
+  name: 'Open Finger Gloves 04',
+  picture: ''
+} as const;
+
+export const GLOVES_OPEN_FINGER_05: Item = {
+  id: 'Open_Finger_Gloves_05',
+  name: 'Open Finger Gloves 05',
+  picture: ''
+} as const;
+
+export const GLOVES_OPEN_FINGER_06: Item = {
+  id: 'Open_Finger_Gloves_06',
+  name: 'Open Finger Gloves 06',
+  picture: ''
+} as const;
+
+export const GLOVES_POLICE: Item = {
+  id: 'Police_Gloves_01',
+  name: 'Police Gloves',
+  picture: ''
+} as const;
+
+export const GLOVES_POLICE_OPEN_FINGER: Item = {
+  id: 'Police_Open_Finger_Gloves_01',
+  name: 'Police Open Finger Gloves',
+  picture: ''
+} as const;
+
+export const GLOVES_TACTICAL_01_01: Item = {
+  id: 'Tactical_Gloves_01_01',
+  name: 'Tactical Gloves 01',
+  picture: ''
+} as const;
+
+export const GLOVES_TACTICAL_01_02: Item = {
+  id: 'Tactical_Gloves_01_02',
+  name: 'Tactical Gloves 02',
+  picture: ''
+} as const;
+
+export const GLOVES_TACTICAL_01_03: Item = {
+  id: 'Tactical_Gloves_01_03',
+  name: 'Tactical Gloves 03',
+  picture: ''
+} as const;
+
+export const GLOVES_TACTICAL_01_04: Item = {
+  id: 'Tactical_Gloves_01_04',
+  name: 'Tactical Gloves 04',
+  picture: ''
+} as const;
+
+export const GLOVES_TACTICAL_01_05: Item = {
+  id: 'Tactical_Gloves_01_05',
+  name: 'Tactical Gloves 05',
+  picture: ''
+} as const;
+
+export const GLOVES_TACTICAL_01_06: Item = {
+  id: 'Tactical_Gloves_01_06',
+  name: 'Tactical Gloves 06',
+  picture: ''
+} as const;
+
+export const GLOVES_TACTICAL_01_07: Item = {
+  id: 'Tactical_Gloves_01_07',
+  name: 'Tactical Gloves 07',
+  picture: ''
+} as const;
+
+export const GLOVES_TACTICAL_01_08: Item = {
+  id: 'Tactical_Gloves_01_08',
+  name: 'Tactical Gloves 08',
+  picture: ''
+} as const;
+
+export const GLOVES_WOOL_01_01: Item = {
+  id: 'Wool_Gloves_01_01',
+  name: 'Wool Gloves 01',
+  picture: ''
+} as const;
+
+export const GLOVES_WOOL_01_02: Item = {
+  id: 'Wool_Gloves_01_02',
+  name: 'Wool Gloves 02',
+  picture: ''
+} as const;
+
+export const GLOVES_WOOL_01_03: Item = {
+  id: 'Wool_Gloves_01_03',
+  name: 'Wool Gloves 03',
+  picture: ''
+} as const;
+
+export const GLOVES_WOOL_01_04: Item = {
+  id: 'Wool_Gloves_01_04',
+  name: 'Wool Gloves 04',
+  picture: ''
+} as const;
+
+export const GLOVES_WOOL_01_05: Item = {
+  id: 'Wool_Gloves_01_05',
+  name: 'Wool Gloves 05',
+  picture: ''
+} as const;
+
+export const GLOVES_WOOL_01_06: Item = {
+  id: 'Wool_Gloves_01_06',
+  name: 'Wool Gloves 06',
+  picture: ''
+} as const;
+
+export const GLOVES_WOOL_01_07: Item = {
+  id: 'Wool_Gloves_01_07',
+  name: 'Wool Gloves 07',
+  picture: ''
+} as const;
+
+export const GLOVES_CONSTRUCTION_01_01: Item = {
+  id: 'Construction_Gloves_01_01',
+  name: 'Construction Gloves 01',
+  picture: ''
+} as const;
+
+export const GLOVES_CONSTRUCTION_01_02: Item = {
+  id: 'Construction_Gloves_01_02',
+  name: 'Construction Gloves 02',
+  picture: ''
+} as const;
+
+export const GLOVES_CONSTRUCTION_01_03: Item = {
+  id: 'Construction_Gloves_01_03',
+  name: 'Construction Gloves 03',
+  picture: ''
+} as const;
+
+export const GLOVES_DRIVING_01_01: Item = {
+  id: 'Driving_Gloves_01_01',
+  name: 'Driving Gloves 01',
+  picture: ''
+} as const;
+
+export const GLOVES_DRIVING_01_02: Item = {
+  id: 'Driving_Gloves_01_02',
+  name: 'Driving Gloves 02',
+  picture: ''
+} as const;
+
+export const GLOVES_DRIVING_01_03: Item = {
+  id: 'Driving_Gloves_01_03',
+  name: 'Driving Gloves 03',
+  picture: ''
+} as const;
+
+export const GLOVES_DRIVING_01_04: Item = {
+  id: 'Driving_Gloves_01_04',
+  name: 'Driving Gloves 04',
+  picture: ''
+} as const;
+
+export const GLOVES_ELECTRICIAN_01_01: Item = {
+  id: 'Electrician_Glove_01_01',
+  name: 'Electrician Gloves 01',
+  picture: ''
+} as const;
+
+export const GLOVES_ELECTRICIAN_01_02: Item = {
+  id: 'Electrician_Glove_01_02',
+  name: 'Electrician Gloves 02',
+  picture: ''
+} as const;
+
+export const GLOVES_ELECTRICIAN_01_03: Item = {
+  id: 'Electrician_Glove_01_03',
+  name: 'Electrician Gloves 03',
+  picture: ''
+} as const;
+
+export const GLOVES_MEDICAL: Item = {
+  id: 'Medical_Glove_01',
+  name: 'Medical Gloves',
+  picture: ''
+} as const;
+
+export const GLOVES_MMA: Item = {
+  id: 'MMA_Glove_01',
+  name: 'MMA Gloves',
+  picture: ''
+} as const;
