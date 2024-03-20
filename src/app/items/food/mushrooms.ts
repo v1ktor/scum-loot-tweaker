c5b77617d534ae26445a538e2e55af7da3fd8bf2
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-20 04:14:11 +0200

    Add mushrooms to the items dropdown

---
+++ src/app/items/food/mushrooms.ts
@@ -0,0 +1,91 @@
+import { Item } from "../item.ts";
+
+export const MUSHROOMS_AGARICUS_AUGUSTUS: Item = {
+  id: 'Agaricus_Augustus',
+  name: 'Agaricus Augustus',
+  picture: ''
+} as const;
+
+export const MUSHROOMS_AGARICUS_CAMPESTRIS: Item = {
+  id: 'Agaricus_Campestris',
+  name: 'Agaricus Campestris',
+  picture: ''
+} as const;
+
+export const MUSHROOMS_AMANITA_MUSCARIA: Item = {
+  id: 'Amanita_Muscaria',
+  name: 'Amanita Muscaria',
+  picture: ''
+} as const;
+
+export const MUSHROOMS_AMANITA_PANTHERINA: Item = {
+  id: 'Amanita_Pantherina',
+  name: 'Amanita Pantherina',
+  picture: ''
+} as const;
+
+export const MUSHROOMS_AMANITA_PHALLOIDES: Item = {
+  id: 'Amanita_Phalloides',
+  name: 'Amanita Phalloides',
+  picture: ''
+} as const;
+
+export const MUSHROOMS_AMANITA_VIROSA: Item = {
+  id: 'Amanita_Virosa',
+  name: 'Amanita Virosa',
+  picture: ''
+} as const;
+
+export const MUSHROOMS_ARMILLARIA_MELLEA: Item = {
+  id: 'Armillaria_Mellea',
+  name: 'Armillaria Mellea',
+  picture: ''
+} as const;
+
+export const MUSHROOMS_BOLETUS_EDULIS: Item = {
+  id: 'Boletus_Edulis',
+  name: 'Boletus Edulis',
+  picture: ''
+} as const;
+
+export const MUSHROOMS_CANTHARELLUS_CIBARIUS: Item = {
+  id: 'Cantharellus_Cibarius',
+  name: 'Cantharellus Cibarius',
+  picture: ''
+} as const;
+
+export const MUSHROOMS_COPRINUS_COMATUS: Item = {
+  id: 'Coprinus_Comatus',
+  name: 'Coprinus Comatus',
+  picture: ''
+} as const;
+
+export const MUSHROOMS_GANODERMA_LUCIDUM: Item = {
+  id: 'Ganoderma_Lucidum',
+  name: 'Ganoderma Lucidum',
+  picture: ''
+} as const;
+
+export const MUSHROOMS_LEPIOTA_PROCERA: Item = {
+  id: 'Lepiota_Procera',
+  name: 'Lepiota Procera',
+  picture: ''
+} as const;
+
+export const MUSHROOMS_LYCOPERDON_GIGANTEA: Item = {
+  id: 'Lycoperdon_Gigantea',
+  name: 'Lycoperdon Gigantea',
+  picture: ''
+} as const;
+
+export const MUSHROOMS_OPUNTIA_FIG: Item = {
+  id: 'Opuntia_Fig',
+  name: 'Opuntia Fig',
+  picture: ''
+} as const;
+
+export const MUSHROOMS_PSILOCYBE_CYANESCENS: Item = {
+  id: 'Psilocybe_Cyanescens',
+  name: 'Psilocybe Cyanescens',
+  picture: ''
+} as const;
