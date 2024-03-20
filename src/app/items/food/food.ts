b641575fe6484a0e32f3ca2ff6d229ca52cec991
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-03-20 03:52:10 +0200

    Add food to the items dropdown

---
+++ src/app/items/food/food.ts
@@ -0,0 +1,625 @@
+import { Item } from "../item.ts";
+
+export const FOOD_AFRICAN_BILTONG: Item = {
+  id: 'African_Biltong',
+  name: 'African Biltong',
+  picture: ''
+} as const;
+
+export const FOOD_APPLE: Item = {
+  id: 'Apple',
+  name: 'Apple',
+  picture: ''
+} as const;
+
+export const FOOD_APPLE_2: Item = {
+  id: 'Apple_2',
+  name: 'Apple 2',
+  picture: ''
+} as const;
+
+export const FOOD_BAG_OF_ALMONDS: Item = {
+  id: 'Bag_of_Almonds',
+  name: 'Bag of Almonds',
+  picture: ''
+} as const;
+
+export const FOOD_BABY_FOOD: Item = {
+  id: 'BabyFood',
+  name: 'Baby Food',
+  picture: ''
+} as const;
+
+export const FOOD_BAKED_BEANS: Item = {
+  id: 'BakedBeans',
+  name: 'Baked Beans',
+  picture: ''
+} as const;
+
+export const FOOD_BANANA: Item = {
+  id: 'Banana',
+  name: 'Banana',
+  picture: ''
+} as const;
+
+export const FOOD_BARBERRY: Item = {
+  id: 'Barberry',
+  name: 'Barberry',
+  picture: ''
+} as const;
+
+export const FOOD_BEEF_RAVIOLI: Item = {
+  id: 'BeefRavioli',
+  name: 'Beef Ravioli',
+  picture: ''
+} as const;
+
+export const FOOD_BEEF_STEW: Item = {
+  id: 'BeefStew',
+  name: 'Beef Stew',
+  picture: ''
+} as const;
+
+export const FOOD_BEET: Item = {
+  id: 'Beet',
+  name: 'Beet',
+  picture: ''
+} as const;
+
+export const FOOD_BLACKBERRY: Item = {
+  id: 'Blackberry',
+  name: 'Blackberry',
+  picture: ''
+} as const;
+
+export const FOOD_BLACK_OLIVES: Item = {
+  id: 'BlackOlives',
+  name: 'Black Olives',
+  picture: ''
+} as const;
+
+export const FOOD_BLUEBERRIES: Item = {
+  id: 'Blueberries',
+  name: 'Blueberries',
+  picture: ''
+} as const;
+
+export const FOOD_BROCCOLI: Item = {
+  id: 'Broccoli',
+  name: 'Broccoli',
+  picture: ''
+} as const;
+
+export const FOOD_CABBAGE: Item = {
+  id: 'Cabbage',
+  name: 'Cabbage',
+  picture: ''
+} as const;
+
+export const FOOD_BUTTER: Item = {
+  id: 'Butter',
+  name: 'Butter',
+  picture: ''
+} as const;
+
+export const FOOD_CANDY_BOB: Item = {
+  id: 'Candy_Bob',
+  name: 'Candy Bob',
+  picture: ''
+} as const;
+
+export const FOOD_CANDY_CORN: Item = {
+  id: 'CandyCorn_01',
+  name: 'CandyCorn',
+  picture: ''
+} as const;
+
+export const FOOD_CANTALOUPE_MELON: Item = {
+  id: 'CantaloupeMelon',
+  name: 'Cantaloupe Melon',
+  picture: ''
+} as const;
+
+export const FOOD_CARROT: Item = {
+  id: 'Carrot',
+  name: 'Carrot',
+  picture: ''
+} as const;
+
+export const FOOD_CAULIFLOWER: Item = {
+  id: 'Cauliflower',
+  name: 'Cauliflower',
+  picture: ''
+} as const;
+
+export const FOOD_CELERY: Item = {
+  id: 'Celery',
+  name: 'Celery',
+  picture: ''
+} as const;
+
+export const FOOD_CEREAL_BUNNY_STARS: Item = {
+  id: 'Cereal_BunnyStars',
+  name: 'Cereal Bunny Stars',
+  picture: ''
+} as const;
+
+export const FOOD_CEREAL_UP_N_GO: Item = {
+  id: 'Cereal_UpNGo',
+  name: 'Cereal Up\'N\'Go',
+  picture: ''
+} as const;
+
+export const FOOD_CHEESE: Item = {
+  id: 'Cheese',
+  name: 'Cheese',
+  picture: ''
+} as const;
+
+export const FOOD_CHEESE_PACKED: Item = {
+  id: 'CheesePacked',
+  name: 'Cheese Packed',
+  picture: ''
+} as const;
+
+export const FOOD_CHILI_PEPPER: Item = {
+  id: 'Chilipeper',
+  name: 'Chili Pepper',
+  picture: ''
+} as const;
+
+export const FOOD_CHIPS: Item = {
+  id: 'Chips',
+  name: 'Chips',
+  picture: ''
+} as const;
+
+export const FOOD_CHIPS_2: Item = {
+  id: 'Chips2',
+  name: 'Chips 2',
+  picture: ''
+} as const;
+
+export const FOOD_CHIPS_3: Item = {
+  id: 'Chips3',
+  name: 'Chips 3',
+  picture: ''
+} as const;
+
+export const FOOD_CHIPS_4: Item = {
+  id: 'Chips4',
+  name: 'Chips 4',
+  picture: ''
+} as const;
+
+export const FOOD_CHOCOLATE_CANDY_01: Item = {
+  id: 'ChocolateCandy_01',
+  name: 'Chocolate Candy 01',
+  picture: ''
+} as const;
+
+export const FOOD_CHOCOLATE_CANDY_03: Item = {
+  id: 'ChocolateCandy_03',
+  name: 'Chocolate Candy 03',
+  picture: ''
+} as const;
+
+export const FOOD_CHOCOLATE_CANDY_04: Item = {
+  id: 'ChocolateCandy_04',
+  name: 'Chocolate Candy 04',
+  picture: ''
+} as const;
+
+export const FOOD_CHOCOLATE_CANDY_05: Item = {
+  id: 'ChocolateCandy_05',
+  name: 'Chocolate Candy 05',
+  picture: ''
+} as const;
+
+export const FOOD_CHOCOLATE_CANDY_06: Item = {
+  id: 'ChocolateCandy_06',
+  name: 'Chocolate Candy 06',
+  picture: ''
+} as const;
+
+export const FOOD_CHOCOLATE_CANDY_07: Item = {
+  id: 'ChocolateCandy_07',
+  name: 'Chocolate Candy 07',
+  picture: ''
+} as const;
+
+export const FOOD_CHOCOLATE_CANDY_08: Item = {
+  id: 'ChocolateCandy_08',
+  name: 'Chocolate Candy 08',
+  picture: ''
+} as const;
+
+export const FOOD_CHOCOLATE_CANDY_09: Item = {
+  id: 'ChocolateCandy_09',
+  name: 'Chocolate Candy 09',
+  picture: ''
+} as const;
+
+export const FOOD_CHOCOLATE_CANDY_10: Item = {
+  id: 'ChocolateCandy_10',
+  name: 'Chocolate Candy 10',
+  picture: ''
+} as const;
+
+export const FOOD_CHOCOLATE_CANDY_11: Item = {
+  id: 'ChocolateCandy_11',
+  name: 'Chocolate Candy 11',
+  picture: ''
+} as const;
+
+export const FOOD_COCOA_POWDER: Item = {
+  id: 'Cocoa_Powder',
+  name: 'Cocoa Powder',
+  picture: ''
+} as const;
+
+export const FOOD_COCONUT: Item = {
+  id: 'Coconut',
+  name: 'Coconut',
+  picture: ''
+} as const;
+
+export const FOOD_COFFEE: Item = {
+  id: 'Coffee',
+  name: 'Coffee',
+  picture: ''
+} as const;
+
+export const FOOD_CORN: Item = {
+  id: 'Corn',
+  name: 'Corn',
+  picture: ''
+} as const;
+
+export const FOOD_CORN_FLAKES: Item = {
+  id: 'Corn_flakes',
+  name: 'Corn Flakes',
+  picture: ''
+} as const;
+
+export const FOOD_CRAB_APPLE: Item = {
+  id: 'Crab_Apple',
+  name: 'Crab Apple',
+  picture: ''
+} as const;
+
+export const FOOD_CRACKERS: Item = {
+  id: 'Crackers',
+  name: 'Crackers',
+  picture: ''
+} as const;
+
+export const FOOD_CREAM: Item = {
+  id: 'Cream',
+  name: 'Cream',
+  picture: ''
+} as const;
+
+export const FOOD_CUCUMBER: Item = {
+  id: 'Cucumber',
+  name: 'Cucumber',
+  picture: ''
+} as const;
+
+export const FOOD_EGG: Item = {
+  id: 'Egg',
+  name: 'Egg',
+  picture: ''
+} as const;
+
+export const FOOD_GARLIC: Item = {
+  id: 'Garlic',
+  name: 'Garlic',
+  picture: ''
+} as const;
+
+export const FOOD_GRAPEFRUIT: Item = {
+  id: 'GrapeFruit',
+  name: 'Grapefruit',
+  picture: ''
+} as const;
+
+export const FOOD_GRAPES_RAW: Item = {
+  id: 'Grapes_Raw',
+  name: 'Grapes Raw',
+  picture: ''
+} as const;
+
+export const FOOD_HAZELNUTS: Item = {
+  id: 'Hazelnuts',
+  name: 'Hazelnuts',
+  picture: ''
+} as const;
+
+export const FOOD_INSTANT_OATMEAL: Item = {
+  id: 'Instant_OatMeal',
+  name: 'Instant Oat Meal',
+  picture: ''
+} as const;
+
+export const FOOD_KETCHUP: Item = {
+  id: 'Ketchup',
+  name: 'Ketchup',
+  picture: ''
+} as const;
+
+export const FOOD_KIWI: Item = {
+  id: 'Kiwi',
+  name: 'Kiwi',
+  picture: ''
+} as const;
+
+export const FOOD_LEMON: Item = {
+  id: 'Lemon',
+  name: 'Lemon',
+  picture: ''
+} as const;
+
+export const FOOD_LETTUCE: Item = {
+  id: 'Lettuce',
+  name: 'Lettuce',
+  picture: ''
+} as const;
+
+export const FOOD_LIME: Item = {
+  id: 'Lime',
+  name: 'Lime',
+  picture: ''
+} as const;
+
+export const FOOD_MANGO: Item = {
+  id: 'Mango',
+  name: 'Mango',
+  picture: ''
+} as const;
+
+export const FOOD_MAYONNAISE: Item = {
+  id: 'Mayonnaise',
+  name: 'Mayonnaise',
+  picture: ''
+} as const;
+
+export const FOOD_MEAT_SNACK_01: Item = {
+  id: 'MeatSnack_01',
+  name: 'Meat Snack 01',
+  picture: ''
+} as const;
+
+export const FOOD_MEAT_SNACK_02: Item = {
+  id: 'MeatSnack_02',
+  name: 'Meat Snack 02',
+  picture: ''
+} as const;
+
+export const FOOD_MRE_CHEESEBURGER: Item = {
+  id: 'MRE_Cheeseburger',
+  name: 'MRE Cheeseburger',
+  picture: ''
+} as const;
+
+export const FOOD_MRE_STEW: Item = {
+  id: 'MRE_Stew',
+  name: 'MRE Stew',
+  picture: ''
+} as const;
+
+export const FOOD_MRE_TUNA_SALAD: Item = {
+  id: 'MRE_TunaSalad',
+  name: 'MRE TunaSalad',
+  picture: ''
+} as const;
+
+export const FOOD_MULBERRY: Item = {
+  id: 'Mulberry',
+  name: 'Mulberry',
+  picture: ''
+} as const;
+
+export const FOOD_MUSTARD: Item = {
+  id: 'Mustard',
+  name: 'Mustard',
+  picture: ''
+} as const;
+
+export const FOOD_OAK_NUTS: Item = {
+  id: 'Oak_Nuts',
+  name: 'Oak Nuts',
+  picture: ''
+} as const;
+
+export const FOOD_OLIVES: Item = {
+  id: 'Olives',
+  name: 'Olives',
+  picture: ''
+} as const;
+
+export const FOOD_ONION: Item = {
+  id: 'Onion',
+  name: 'Onion',
+  picture: ''
+} as const;
+
+export const FOOD_ORANGE: Item = {
+  id: 'Orange',
+  name: 'Orange',
+  picture: ''
+} as const;
+
+export const FOOD_PARSLEY: Item = {
+  id: 'Parsley',
+  name: 'Parsley',
+  picture: ''
+} as const;
+
+export const FOOD_PASTA: Item = {
+  id: 'Pasta',
+  name: 'Pasta',
+  picture: ''
+} as const;
+
+export const FOOD_PEACH: Item = {
+  id: 'Peach',
+  name: 'Peach',
+  picture: ''
+} as const;
+
+export const FOOD_PEAR: Item = {
+  id: 'Pear',
+  name: 'Pear',
+  picture: ''
+} as const;
+
+export const FOOD_PINEAPPLE: Item = {
+  id: 'Pineapple',
+  name: 'Pineapple',
+  picture: ''
+} as const;
+
+export const FOOD_POTATO: Item = {
+  id: 'Potato',
+  name: 'Potato',
+  picture: ''
+} as const;
+
+export const FOOD_PUMPKIN: Item = {
+  id: 'Pumpkin',
+  name: 'Pumpkin',
+  picture: ''
+} as const;
+
+export const FOOD_RAISINS: Item = {
+  id: 'Raisins',
+  name: 'Raisins',
+  picture: ''
+} as const;
+
+export const FOOD_RICE: Item = {
+  id: 'Rice',
+  name: 'Rice',
+  picture: ''
+} as const;
+
+export const FOOD_RICHARDS_CHEESE: Item = {
+  id: 'Richards_Cheese',
+  name: 'Richards Cheese',
+  picture: ''
+} as const;
+
+export const FOOD_SOY_SAUCE: Item = {
+  id: 'SoySauce',
+  name: 'Soy Sauce',
+  picture: ''
+} as const;
+
+export const FOOD_SPICES_MIX_01: Item = {
+  id: 'SpicesMix_01',
+  name: 'Spices Mix 01',
+  picture: ''
+} as const;
+
+export const FOOD_SPICES_MIX_02: Item = {
+  id: 'SpicesMix_02',
+  name: 'Spices Mix 02',
+  picture: ''
+} as const;
+
+export const FOOD_SPINACH: Item = {
+  id: 'Spinach',
+  name: 'Spinach',
+  picture: ''
+} as const;
+
+export const FOOD_SUGAR: Item = {
+  id: 'Sugar',
+  name: 'Sugar',
+  picture: ''
+} as const;
+
+export const FOOD_SUGAR_BEET: Item = {
+  id: 'SugarBeet',
+  name: 'Sugar Beet',
+  picture: ''
+} as const;
+
+export const FOOD_TANGERINE: Item = {
+  id: 'Tangerine',
+  name: 'Tangerine',
+  picture: ''
+} as const;
+
+export const FOOD_TEA_PACK: Item = {
+  id: 'Tea_Pack',
+  name: 'Tea Pack',
+  picture: ''
+} as const;
+
+export const FOOD_TOASTER_PASTRIES: Item = {
+  id: 'Toaster_Pastries',
+  name: 'Toaster Pastries',
+  picture: ''
+} as const;
+
+export const FOOD_TOASTER_PASTRIES_2: Item = {
+  id: 'Toaster_Pastries2',
+  name: 'Toaster Pastries 2',
+  picture: ''
+} as const;
+
+export const FOOD_TOBACCO: Item = {
+  id: 'Tobacco',
+  name: 'Tobacco',
+  picture: ''
+} as const;
+
+export const FOOD_TOMATO: Item = {
+  id: 'Tomato',
+  name: 'Tomato',
+  picture: ''
+} as const;
+
+export const FOOD_TOMATO_PELATE: Item = {
+  id: 'TomatoPelate',
+  name: 'Tomato Pelate',
+  picture: ''
+} as const;
+
+export const FOOD_TOMATO_SAUCE: Item = {
+  id: 'TomatoSouce',
+  name: 'Tomato Sauce',
+  picture: ''
+} as const;
+
+export const FOOD_WALNUT: Item = {
+  id: 'Walnut',
+  name: 'Walnut',
+  picture: ''
+} as const;
+
+export const FOOD_WATERMELON: Item = {
+  id: 'Watermelon',
+  name: 'Watermelon',
+  picture: ''
+} as const;
+
+export const FOOD_WILD_CHERRY: Item = {
+  id: 'Wild_Cherry',
+  name: 'Wild Cherry',
+  picture: ''
+} as const;
+
+export const FOOD_WILD_STRAWBERRY: Item = {
+  id: 'Wild_Strawberry',
+  name: 'Wild Strawberry',
+  picture: ''
+} as const;
+
+export const FOOD_ZUCCHINI: Item = {
+  id: 'Zucchini',
+  name: 'Zucchini',
+  picture: ''
+} as const;
