f3aa8990fb33c4ee15e443d885b4950719bf349c
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-07-02 21:18:28 +0300

    Add handgun holsters to items dropdown

---
+++ src/app/items/clothes/holsters.ts
@@ -0,0 +1,55 @@
+import { Item } from "../item.ts";
+
+export const HOLSTER_TACTICAL_HANDGUN_01: Item = {
+  id: 'Tactical_Handgun_Holster_01',
+  name: 'Tactical Handgun Holster 01',
+  picture: ''
+} as const;
+
+export const HOLSTER_TACTICAL_HANDGUN_02: Item = {
+  id: 'Tactical_Handgun_Holster_02',
+  name: 'Tactical Handgun Holster 02',
+  picture: ''
+} as const;
+
+export const HOLSTER_TACTICAL_HANDGUN_03: Item = {
+  id: 'Tactical_Handgun_Holster_03',
+  name: 'Tactical Handgun Holster 03',
+  picture: ''
+} as const;
+
+export const HOLSTER_TACTICAL_HANDGUN_04: Item = {
+  id: 'Tactical_Handgun_Holster_04',
+  name: 'Tactical Handgun Holster 04',
+  picture: ''
+} as const;
+
+export const HOLSTER_TACTICAL_HANDGUN_05: Item = {
+  id: 'Tactical_Handgun_Holster_05',
+  name: 'Tactical Handgun Holster 05',
+  picture: ''
+} as const;
+
+export const HOLSTER_TACTICAL_HANDGUN_06: Item = {
+  id: 'Tactical_Handgun_Holster_06',
+  name: 'Tactical Handgun Holster 06',
+  picture: ''
+} as const;
+
+export const HOLSTER_TACTICAL_HANDGUN_07: Item = {
+  id: 'Tactical_Handgun_Holster_07',
+  name: 'Tactical Handgun Holster 07',
+  picture: ''
+} as const;
+
+export const HOLSTER_BELT_PISTOL: Item = {
+  id: 'Belt_Pistol_Holster',
+  name: 'Belt Pistol Holster',
+  picture: ''
+} as const;
+
+export const HOLSTER_POLICE_HANDGUN: Item = {
+  id: 'Police_Handgun_Holster',
+  name: 'Police Handgun Holster',
+  picture: ''
+} as const;
