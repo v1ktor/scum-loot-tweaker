ae73b3c6035ce7f6718aa33a792042af22d171ba
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-07-03 22:17:37 +0300

    Add goggles to items dropdown

---
+++ src/app/items/clothes/goggles.ts
@@ -0,0 +1,79 @@
+import { Item } from "../item.ts";
+
+export const GOGGLES_GEEK: Item = {
+  id: 'Geek_Goggels',
+  name: 'Geek Goggles',
+  picture: ''
+} as const;
+
+export const GOGGLES_MOTORCYCLE_GLASSES_01: Item = {
+  id: 'Motorcycle_Glasses_01',
+  name: 'Motorcycle Glasses 01',
+  picture: ''
+} as const;
+
+export const GOGGLES_MOTORCYCLE_GLASSES_02: Item = {
+  id: 'Motorcycle_Glasses_02',
+  name: 'Motorcycle Glasses 02',
+  picture: ''
+} as const;
+
+export const GOGGLES_MILITARY_01: Item = {
+  id: 'Military_Goggles_01',
+  name: 'Military Goggles 01',
+  picture: ''
+} as const;
+
+export const GOGGLES_NIGHT_VISION_01: Item = {
+  id: 'Night_Vision_Googles_01',
+  name: 'Night Vision Goggles 01',
+  picture: ''
+} as const;
+
+export const GOGGLES_PARTY_GLASSES_01: Item = {
+  id: 'Party_Glasses_01',
+  name: 'Party Glasses 01',
+  picture: ''
+} as const;
+
+export const GOGGLES_PARTY_GLASSES_02: Item = {
+  id: 'Party_Glasses_02',
+  name: 'Party Glasses 02',
+  picture: ''
+} as const;
+
+export const GOGGLES_PILOT_GLASSES: Item = {
+  id: 'Pilot_Glasses',
+  name: 'Pilot Glasses',
+  picture: ''
+} as const;
+
+export const GOGGLES_PIRATE_EYE_PATCH: Item = {
+  id: 'Pirate_EyePatch',
+  name: 'Pirate Eye Patch',
+  picture: ''
+} as const;
+
+export const GOGGLES_ROUND_SUNGLASSES: Item = {
+  id: 'Round_Sunglasses',
+  name: 'Round Sunglasses',
+  picture: ''
+} as const;
+
+export const GOGGLES_SAFETY: Item = {
+  id: 'Safety_Goggles',
+  name: 'Safety Goggles',
+  picture: ''
+} as const;
+
+export const GOGGLES_TACTICAL_SUNGLASSES_01: Item = {
+  id: 'Tactical_Sunglasses_01',
+  name: 'Tactical Sunglasses 01',
+  picture: ''
+} as const;
+
+export const GOGGLES_TACTICAL_SUNGLASSES_02: Item = {
+  id: 'Tactical_Sunglasses_02',
+  name: 'Tactical Sunglasses 02',
+  picture: ''
+} as const;
