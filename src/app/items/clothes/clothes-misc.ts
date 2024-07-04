fdcf4ba83038c4c4902a8e8c53b8932a8091317f
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-07-04 20:14:04 +0300

    Add misc clothes to items dropdown

---
+++ src/app/items/clothes/clothes-misc.ts
@@ -0,0 +1,85 @@
+import { Item } from "../item.ts";
+
+export const CLOTHES_MISC_PARACHUTE: Item = {
+  id: 'Parachute',
+  name: 'Parachute',
+  picture: ''
+} as const;
+
+export const CLOTHES_MISC_PENIS_WARMER_01: Item = {
+  id: 'PenisWarmer_01',
+  name: 'Penis Warmer 01',
+  picture: ''
+} as const;
+
+export const CLOTHES_MISC_PENIS_WARMER_02: Item = {
+  id: 'PenisWarmer_02',
+  name: 'Penis Warmer 02',
+  picture: ''
+} as const;
+
+export const CLOTHES_MISC_PENIS_WARMER_03: Item = {
+  id: 'PenisWarmer_03',
+  name: 'Penis Warmer 03',
+  picture: ''
+} as const;
+
+export const CLOTHES_MISC_RH_ARM_GUARD: Item = {
+  id: 'RH_Arm_Guard',
+  name: 'RH Arm Guard',
+  picture: ''
+} as const;
+
+export const CLOTHES_MISC_SHEMAGH_SCARF_01: Item = {
+  id: 'Shemagh_Scarf_01',
+  name: 'Shemagh Scarf 01',
+  picture: ''
+} as const;
+
+export const CLOTHES_MISC_SHEMAGH_SCARF_02: Item = {
+  id: 'Shemagh_Scarf_02',
+  name: 'Shemagh Scarf 02',
+  picture: ''
+} as const;
+
+export const CLOTHES_MISC_SHEMAGH_SCARF_03: Item = {
+  id: 'Shemagh_Scarf_03',
+  name: 'Shemagh Scarf 03',
+  picture: ''
+} as const;
+
+export const CLOTHES_MISC_SHEMAGH_SCARF_04: Item = {
+  id: 'Shemagh_Scarf_04',
+  name: 'Shemagh Scarf 04',
+  picture: ''
+} as const;
+
+export const CLOTHES_MISC_SHEMAGH_SCARF_05: Item = {
+  id: 'Shemagh_Scarf_05',
+  name: 'Shemagh Scarf 05',
+  picture: ''
+} as const;
+
+export const CLOTHES_MISC_SHEMAGH_SCARF_06: Item = {
+  id: 'Shemagh_Scarf_06',
+  name: 'Shemagh Scarf 06',
+  picture: ''
+} as const;
+
+export const CLOTHES_MISC_SHEMAGH_SCARF_07: Item = {
+  id: 'Shemagh_Scarf_07',
+  name: 'Shemagh Scarf 07',
+  picture: ''
+} as const;
+
+export const CLOTHES_MISC_SHEMAGH_SCARF_08: Item = {
+  id: 'Shemagh_Scarf_08',
+  name: 'Shemagh Scarf 08',
+  picture: ''
+} as const;
+
+export const CLOTHES_MISC_ZOMBIE_IMPERSONATOR_SUIT: Item = {
+  id: 'Zombie_Impersonator_Suit',
+  name: 'Zombie Impersonator Suit',
+  picture: ''
+} as const;
