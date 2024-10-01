49c0d4875a92f0d08c4eb5171f03210f6992aeca
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-10-01 04:38:28 +0300

    Add utilities to the items dropdown

---
+++ src/app/items/misc/utilities.ts
@@ -0,0 +1,37 @@
+import { Item } from "../item.ts";
+
+export const UTILITIES_ELECTRICITY_GENERATOR: Item = {
+  id: 'ElectricityGenerator',
+  name: 'Electricity Generator',
+  picture: ''
+} as const;
+
+export const UTILITIES_ELECTRICITY_GENERATOR_SMALL: Item = {
+  id: 'ElectricityGenerator_Small',
+  name: 'Electricity Generator Small',
+  picture: ''
+} as const;
+
+export const UTILITIES_REFRIGERATOR_PORTABLE_SMALL: Item = {
+  id: 'Refrigerator_Portable_Small',
+  name: 'Refrigerator Portable Small',
+  picture: ''
+} as const;
+
+export const UTILITIES_PORTABLE_ELECTRIC_STOVE: Item = {
+  id: 'Portable_Electric_Stove',
+  name: 'Portable Electric Stove',
+  picture: ''
+} as const;
+
+export const UTILITIES_PORTABLE_GAS_STOVE: Item = {
+  id: 'Portable_Gas_Stove',
+  name: 'Portable Gas Stove',
+  picture: ''
+} as const;
+
+export const UTILITIES_GRILL_GRID: Item = {
+  id: 'GrillGrid',
+  name: 'Grill Grid',
+  picture: ''
+} as const;
