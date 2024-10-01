9c2695a83c6a1caaa7acc1215e2f5e37008a49ce
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-10-01 05:21:56 +0300

    Add crafting items

---
+++ src/app/items/misc/crafting.ts
@@ -0,0 +1,295 @@
+import { Item } from "../item.ts";
+
+export const CRAFTING_BARBED_WIRE: Item = {
+  id: 'Barbed_Wire',
+  name: 'Barbed Wire',
+  picture: ''
+} as const;
+
+export const CRAFTING_BATTERIES: Item = {
+  id: 'Batteries',
+  name: 'Batteries',
+  picture: ''
+} as const;
+
+export const CRAFTING_BOLTS: Item = {
+  id: 'Bolts',
+  name: 'Bolts',
+  picture: ''
+} as const;
+
+export const CRAFTING_BOLTS_PACKAGE_BOX: Item = {
+  id: 'Bolts_Package_Box',
+  name: 'Bolts Package Box',
+  picture: ''
+} as const;
+
+export const CRAFTING_BRICK: Item = {
+  id: 'Brick',
+  name: 'Brick',
+  picture: ''
+} as const;
+
+export const CRAFTING_BUNDLE_OF_BRICKS: Item = {
+  id: 'Bundle_Of_Bricks',
+  name: 'Bundle Of Bricks',
+  picture: ''
+} as const;
+
+export const CRAFTING_CEMENT_BAG: Item = {
+  id: 'CementBag',
+  name: 'Cement Bag',
+  picture: ''
+} as const;
+
+export const CRAFTING_EMPTY_BAG: Item = {
+  id: 'EmptyBag',
+  name: 'Empty Bag',
+  picture: ''
+} as const;
+
+export const CRAFTING_GRAVEL_BAG: Item = {
+  id: 'GravelBag',
+  name: 'Gravel Bag',
+  picture: ''
+} as const;
+
+export const CRAFTING_LONG_WOODEN_STICK: Item = {
+  id: 'Long_wooden_stick',
+  name: 'Long Wooden Stick',
+  picture: ''
+} as const;
+
+export const CRAFTING_METAL_SCRAP_01: Item = {
+  id: 'Metal_Scrap_01',
+  name: 'Metal Scrap 01',
+  picture: ''
+} as const;
+
+export const CRAFTING_METAL_SCRAP_02: Item = {
+  id: 'Metal_Scrap_02',
+  name: 'Metal Scrap 02',
+  picture: ''
+} as const;
+
+export const CRAFTING_METAL_SCRAP_03: Item = {
+  id: 'Metal_Scrap_03',
+  name: 'Metal Scrap 03',
+  picture: ''
+} as const;
+
+export const CRAFTING_METAL_SCRAP_04: Item = {
+  id: 'Metal_Scrap_04',
+  name: 'Metal Scrap 04',
+  picture: ''
+} as const;
+
+export const CRAFTING_METAL_SCRAP_05: Item = {
+  id: 'Metal_Scrap_05',
+  name: 'Metal Scrap 05',
+  picture: ''
+} as const;
+
+export const CRAFTING_NAILS: Item = {
+  id: 'Nails',
+  name: 'Nails',
+  picture: ''
+} as const;
+
+export const CRAFTING_NAILS_PACKAGE_BOX: Item = {
+  id: 'Nails_Package_Box',
+  name: 'Nails Package Box',
+  picture: ''
+} as const;
+
+export const CRAFTING_ROPE_1: Item = {
+  id: 'Rope1',
+  name: 'Rope 1',
+  picture: ''
+} as const;
+
+export const CRAFTING_TREE_BRANCHES: Item = {
+  id: 'Tree_Branches',
+  name: 'Tree Branches',
+  picture: ''
+} as const;
+
+export const CRAFTING_WOODEN_LOG_01: Item = {
+  id: 'Wooden_Log_01',
+  name: 'Wooden Log 01',
+  picture: ''
+} as const;
+
+export const CRAFTING_WOODEN_LOG_02: Item = {
+  id: 'Wooden_Log_02',
+  name: 'Wooden Log 02',
+  picture: ''
+} as const;
+
+export const CRAFTING_WOODEN_LOG_SMALL_01: Item = {
+  id: 'Wooden_Log_Small_01',
+  name: 'Wooden Log Small 01',
+  picture: ''
+} as const;
+
+export const CRAFTING_WOODEN_LOG_SMALL_02: Item = {
+  id: 'Wooden_Log_Small_02',
+  name: 'Wooden Log Small 02',
+  picture: ''
+} as const;
+
+export const CRAFTING_WOODEN_LOG_SMALL_03: Item = {
+  id: 'Wooden_Log_Small_03',
+  name: 'Wooden Log Small 03',
+  picture: ''
+} as const;
+
+export const CRAFTING_WOODEN_PLANK: Item = {
+  id: 'Wooden_Plank',
+  name: 'Wooden Plank',
+  picture: ''
+} as const;
+
+export const CRAFTING_SMARTPHONE_BATTERY: Item = {
+  id: 'Smartphone_Battery',
+  name: 'Smartphone Battery',
+  picture: ''
+} as const;
+
+export const CRAFTING_SOAP: Item = {
+  id: 'Soap',
+  name: 'Soap',
+  picture: ''
+} as const;
+
+export const CRAFTING_STONE: Item = {
+  id: 'Stone',
+  name: 'Stone',
+  picture: ''
+} as const;
+
+export const CRAFTING_STONE_LARGE: Item = {
+  id: 'Stone_Large',
+  name: 'Stone Large',
+  picture: ''
+} as const;
+
+export const CRAFTING_STONE_SMALL: Item = {
+  id: 'Stone_Small',
+  name: 'Stone Small',
+  picture: ''
+} as const;
+
+export const CRAFTING_SUPER_GLUE: Item = {
+  id: 'Super_Glue',
+  name: 'Super Glue',
+  picture: ''
+} as const;
+
+export const CRAFTING_THREAD: Item = {
+  id: 'Thread',
+  name: 'Thread',
+  picture: ''
+} as const;
+
+export const CRAFTING_DUCT_TAPE: Item = {
+  id: 'Duct_Tape',
+  name: 'Duct Tape',
+  picture: ''
+} as const;
+
+export const CRAFTING_FEATHER: Item = {
+  id: 'Feather',
+  name: 'Feather',
+  picture: ''
+} as const;
+
+export const CRAFTING_GUN_POWDER: Item = {
+  id: 'Gun_Powder',
+  name: 'Gun Powder',
+  picture: ''
+} as const;
+
+export const CRAFTING_LEAD_PLATE: Item = {
+  id: 'Lead_Plate',
+  name: 'Lead Plate',
+  picture: ''
+} as const;
+
+export const CRAFTING_MOBILE_PHONE: Item = {
+  id: 'Mobile_Phone',
+  name: 'Mobile Phone',
+  picture: ''
+} as const;
+
+export const CRAFTING_NEEDLE: Item = {
+  id: 'Needle',
+  name: 'Needle',
+  picture: ''
+} as const;
+
+export const CRAFTING_PADLOCK: Item = {
+  id: 'Padlock',
+  name: 'Padlock',
+  picture: ''
+} as const;
+
+export const CRAFTING_PAPER: Item = {
+  id: 'Paper',
+  name: 'Paper',
+  picture: ''
+} as const;
+
+export const CRAFTING_PARKING_SENSOR: Item = {
+  id: 'Parking_Sensor',
+  name: 'Parking Sensor',
+  picture: ''
+} as const;
+
+export const CRAFTING_PEN: Item = {
+  id: 'Pen',
+  name: 'Pen',
+  picture: ''
+} as const;
+
+export const CRAFTING_RAG_STRIPES: Item = {
+  id: 'Rag_Stripes',
+  name: 'Rag Stripes',
+  picture: ''
+} as const;
+
+export const CRAFTING_WIRE: Item = {
+  id: 'Wire',
+  name: 'Wire',
+  picture: ''
+} as const;
+
+export const CRAFTING_CD_PLAYER: Item = {
+  id: 'CD_Player',
+  name: 'CD Player',
+  picture: ''
+} as const;
+
+export const CRAFTING_GLUE: Item = {
+  id: 'Glue',
+  name: 'Glue',
+  picture: ''
+} as const;
+
+export const CRAFTING_HOSE: Item = {
+  id: 'Hose',
+  name: 'Hose',
+  picture: ''
+} as const;
+
+export const CRAFTING_MAGNIFYING_GLASS: Item = {
+  id: 'Magnifying_Glass',
+  name: 'Magnifying Glass',
+  picture: ''
+} as const;
+
+export const CRAFTING_RUBBER_BAND: Item = {
+  id: 'Rubber_band',
+  name: 'Rubber Band',
+  picture: ''
+} as const;
