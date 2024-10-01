5be317fec14a9f6bc8ee57a4727bfe6baf4cefcb
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-10-01 04:16:09 +0300

    Add seeds to items dropdown

---
+++ src/app/items/food/seeds.ts
@@ -0,0 +1,289 @@
+import { Item } from "../item.ts";
+
+export const SEEDS_APPLE: Item = {
+  id: 'Apple_Seeds',
+  name: 'Apple Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_RED_ONION: Item = {
+  id: 'RedOnion_Seeds',
+  name: 'Red Onion Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_BROCCOLI: Item = {
+  id: 'Broccoli_Seeds',
+  name: 'Broccoli Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_CABBAGE: Item = {
+  id: 'Cabbage_Seeds',
+  name: 'Cabbage Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_CARROT: Item = {
+  id: 'Carrot_Seeds',
+  name: 'Carrot Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_CHERRY: Item = {
+  id: 'Cherry_Seeds',
+  name: 'Cherry Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_CHILLI_PEPPERS: Item = {
+  id: 'ChilliPeppers_Seeds',
+  name: 'Chilli Peppers Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_CORN: Item = {
+  id: 'Corn_Seeds',
+  name: 'Corn Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_CUCUMBER: Item = {
+  id: 'Cucumber_Seeds',
+  name: 'Cucumber Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_FIG: Item = {
+  id: 'Fig_Seeds',
+  name: 'Fig Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_GARLIC: Item = {
+  id: 'Garlic_Seeds',
+  name: 'Garlic Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_LEMON: Item = {
+  id: 'Lemon_Seeds',
+  name: 'Lemon Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_LETTUCE: Item = {
+  id: 'Lettuce_Seeds',
+  name: 'Lettuce Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_PEAR: Item = {
+  id: 'Pear_Seeds',
+  name: 'Pear Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_PEPPERS: Item = {
+  id: 'Peppers_Seeds',
+  name: 'Peppers Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_POTATOES: Item = {
+  id: 'Potatoes_Seeds',
+  name: 'Potatoes Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_PUMPKIN: Item = {
+  id: 'Pumpkin_Seeds',
+  name: 'Pumpkin Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_SPINACH: Item = {
+  id: 'Spinach_Seeds',
+  name: 'Spinach Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_SWEET_MELONS: Item = {
+  id: 'SweetMelons_Seeds',
+  name: 'Sweet Melons Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_TOMATOES: Item = {
+  id: 'Tomatoes_Seeds',
+  name: 'Tomatoes Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_WATERMELON: Item = {
+  id: 'Watermelon_Seeds',
+  name: 'Watermelon Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_ZUCCHINI: Item = {
+  id: 'Zucchini_Seeds',
+  name: 'Zucchini Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_CANNABIS: Item = {
+  id: 'Cannabis_Seeds',
+  name: 'Cannabis Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_TOBACCO: Item = {
+  id: 'Tobacco_Seeds',
+  name: 'Tobacco Seeds',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_APPLE: Item = {
+  id: 'Apple_Seed_Bag',
+  name: 'Apple Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_BROCCOLI: Item = {
+  id: 'Broccoli_Seed_Bag',
+  name: 'Broccoli Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_CABBAGE: Item = {
+  id: 'Cabbage_Seed_Bag',
+  name: 'Cabbage Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_CARROT: Item = {
+  id: 'Carrot_Seed_Bag',
+  name: 'Carrot Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_CHERRY: Item = {
+  id: 'Cherry_Seed_Bag',
+  name: 'Cherry Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_CHILLI_PEPPER: Item = {
+  id: 'Chillipepper_Seed_Bag',
+  name: 'Chilli pepper Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_CORN: Item = {
+  id: 'Corn_Seed_Bag',
+  name: 'Corn Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_CUCUMBER: Item = {
+  id: 'Cucumber_Seed_Bag',
+  name: 'Cucumber Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_FIG: Item = {
+  id: 'Fig_Seed_Bag',
+  name: 'Fig Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_GARLIC: Item = {
+  id: 'Garlic_Seed_Bag',
+  name: 'Garlic Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_LEMON: Item = {
+  id: 'Lemon_Seed_Bag',
+  name: 'Lemon Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_LETTUCE: Item = {
+  id: 'Lettuce_Seed_Bag',
+  name: 'Lettuce Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_PEAR: Item = {
+  id: 'Pear_Seed_Bag',
+  name: 'Pear Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_PEPPERS: Item = {
+  id: 'Peppers_Seed_Bag',
+  name: 'Peppers Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_POTATO: Item = {
+  id: 'Potato_Seed_Bag',
+  name: 'Potato Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_PUMPKIN: Item = {
+  id: 'Pumpkin_Seed_Bag',
+  name: 'Pumpkin Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_RED_ONION: Item = {
+  id: 'RedOnion_Seed_Bag',
+  name: 'Red Onion Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_SPINACH: Item = {
+  id: 'Spinach_Seed_Bag',
+  name: 'Spinach Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_SWEET_MELON: Item = {
+  id: 'SweetMelon_Seed_Bag',
+  name: 'Sweet Melon Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_TOMATO: Item = {
+  id: 'Tomato_Seed_Bag',
+  name: 'Tomato Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_WATERMELON: Item = {
+  id: 'Watermelon_Seed_Bag',
+  name: 'Watermelon Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_ZUCCHINI: Item = {
+  id: 'Zucchini_Seed_Bag',
+  name: 'Zucchini Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_CANNABIS: Item = {
+  id: 'Cannabis_Seed_Bag',
+  name: 'Cannabis Seed Bag',
+  picture: ''
+} as const;
+
+export const SEEDS_BAG_TOBACCO: Item = {
+  id: 'Tobacco_Seed_Bag',
+  name: 'Tobacco Seed Bag',
+  picture: ''
+} as const;
