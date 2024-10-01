e8bad8f3387e82aa61d936eba88c24d18d2359d0
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-10-01 05:45:56 +0300

    Add misc items

---
+++ src/app/items/misc/misc.ts
@@ -0,0 +1,427 @@
+import { Item } from "../item.ts";
+
+export const MISC_BANJO: Item = {
+  id: 'Banjo',
+  name: 'Banjo',
+  picture: ''
+} as const;
+
+export const MISC_BEDROLL: Item = {
+  id: 'Bedroll',
+  name: 'Bedroll',
+  picture: ''
+} as const;
+
+export const MISC_BINOCULARS: Item = {
+  id: 'Binoculars',
+  name: 'Binoculars',
+  picture: ''
+} as const;
+
+export const MISC_BOOM_BOX_01: Item = {
+  id: 'BoomBox_01',
+  name: 'Boom Box 01',
+  picture: ''
+} as const;
+
+export const MISC_BREAK_OIL: Item = {
+  id: 'Break_Oil',
+  name: 'Break Oil',
+  picture: ''
+} as const;
+
+export const MISC_CAMERA_LENS: Item = {
+  id: 'Camera_Lens',
+  name: 'Camera Lens',
+  picture: ''
+} as const;
+
+export const MISC_CANDLE_01: Item = {
+  id: 'Candle_01',
+  name: 'Candle 01',
+  picture: ''
+} as const;
+
+export const MISC_CANNABIS_BUD: Item = {
+  id: 'Cannabis_Bud',
+  name: 'Cannabis Bud',
+  picture: ''
+} as const;
+
+export const MISC_CASH: Item = {
+  id: 'Cash',
+  name: 'Cash',
+  picture: ''
+} as const;
+
+export const MISC_CHARCOAL: Item = {
+  id: 'Charcoal',
+  name: 'Charcoal',
+  picture: ''
+} as const;
+
+export const MISC_CHRISTMAS_BALL_01: Item = {
+  id: 'Christmas_Ball_01',
+  name: 'Christmas Ball 01',
+  picture: ''
+} as const;
+
+export const MISC_CHRISTMAS_BALL_02: Item = {
+  id: 'Christmas_Ball_02',
+  name: 'Christmas Ball 02',
+  picture: ''
+} as const;
+
+export const MISC_CHRISTMAS_BALL_03: Item = {
+  id: 'Christmas_Ball_03',
+  name: 'Christmas Ball 03',
+  picture: ''
+} as const;
+
+export const MISC_CHRISTMAS_BALL_04: Item = {
+  id: 'Christmas_Ball_04',
+  name: 'Christmas Ball 04',
+  picture: ''
+} as const;
+
+export const MISC_CHRISTMAS_BALL_05: Item = {
+  id: 'Christmas_Ball_05',
+  name: 'Christmas Ball 05',
+  picture: ''
+} as const;
+
+export const MISC_CHRISTMAS_BALL_06: Item = {
+  id: 'Christmas_Ball_06',
+  name: 'Christmas Ball 06',
+  picture: ''
+} as const;
+
+export const MISC_CHRISTMAS_BOX: Item = {
+  id: 'Christmas_Box',
+  name: 'Christmas Box',
+  picture: ''
+} as const;
+
+export const MISC_CIGARETTES: Item = {
+  id: 'Cigarettes',
+  name: 'Cigarettes',
+  picture: ''
+} as const;
+
+export const MISC_CIGARETTES1: Item = {
+  id: 'Cigarettes1',
+  name: 'Cigarettes 1',
+  picture: ''
+} as const;
+
+export const MISC_COAL_01: Item = {
+  id: 'Coal_01',
+  name: 'Coal 01',
+  picture: ''
+} as const;
+
+export const MISC_COAL_BAG: Item = {
+  id: 'CoalBag',
+  name: 'Coal Bag',
+  picture: ''
+} as const;
+
+export const MISC_CRUTCH: Item = {
+  id: 'Crutch',
+  name: 'Crutch',
+  picture: ''
+} as const;
+
+export const MISC_DIARY: Item = {
+  id: 'Diary',
+  name: 'Diary',
+  picture: ''
+} as const;
+
+export const MISC_DOUBLE_BANG_FIRECRACKER: Item = {
+  id: 'Double_Bang_Firecracker',
+  name: 'Double Bang Firecracker',
+  picture: ''
+} as const;
+
+export const MISC_FLARE_CARTRIDGE: Item = {
+  id: 'FlareCartridge',
+  name: 'Flare Cartridge',
+  picture: ''
+} as const;
+
+export const MISC_FLARE_ITEM: Item = {
+  id: 'FlareItem',
+  name: 'Flare Item',
+  picture: ''
+} as const;
+
+export const MISC_FLARE_ITEM_BLUE: Item = {
+  id: 'FlareItem_Blue',
+  name: 'Flare Item Blue',
+  picture: ''
+} as const;
+
+export const MISC_FLARE_ITEM_GREEN: Item = {
+  id: 'FlareItem_Green',
+  name: 'Flare Item Green',
+  picture: ''
+} as const;
+
+export const MISC_FLASHBANG: Item = {
+  id: 'Flashbang',
+  name: 'Flashbang',
+  picture: ''
+} as const;
+
+export const MISC_FUNGICIDE: Item = {
+  id: 'Fungicide',
+  name: 'Fungicide',
+  picture: ''
+} as const;
+
+export const MISC_GAS_LANTERN: Item = {
+  id: 'GasLantern',
+  name: 'Gas Lantern',
+  picture: ''
+} as const;
+
+export const MISC_GLASS: Item = {
+  id: 'Glass',
+  name: 'Glass',
+  picture: ''
+} as const;
+
+export const MISC_GLASS_BOTTLE_01: Item = {
+  id: 'GlassBottle01',
+  name: 'Glass Bottle 01',
+  picture: ''
+} as const;
+
+export const MISC_GLASS_BOTTLE_02: Item = {
+  id: 'GlassBottle02',
+  name: 'Glass Bottle 02',
+  picture: ''
+} as const;
+
+export const MISC_GLASS_BOTTLE_03: Item = {
+  id: 'GlassBottle03',
+  name: 'Glass Bottle 03',
+  picture: ''
+} as const;
+
+export const MISC_GLASS_BOTTLE_04: Item = {
+  id: 'GlassBottle04',
+  name: 'Glass Bottle 04',
+  picture: ''
+} as const;
+
+export const MISC_GUITAR: Item = {
+  id: 'Guitar',
+  name: 'Guitar',
+  picture: ''
+} as const;
+
+export const MISC_HAND_FAN_01: Item = {
+  id: 'Hand_Fan_01',
+  name: 'Hand Fan 01',
+  picture: ''
+} as const;
+
+export const MISC_HARMONICA: Item = {
+  id: 'Harmonica',
+  name: 'Harmonica',
+  picture: ''
+} as const;
+
+export const MISC_HERBICIDE: Item = {
+  id: 'Herbicide',
+  name: 'Herbicide',
+  picture: ''
+} as const;
+
+export const MISC_INDUSTRIAL_FERTILIZER: Item = {
+  id: 'Industrial_Fertilizer',
+  name: 'Industrial Fertilizer',
+  picture: ''
+} as const;
+
+export const MISC_JAR: Item = {
+  id: 'Jar',
+  name: 'Jar',
+  picture: ''
+} as const;
+
+export const MISC_JAR_02: Item = {
+  id: 'Jar_02',
+  name: 'Jar 02',
+  picture: ''
+} as const;
+
+export const MISC_KITCHEN_BOARD_01: Item = {
+  id: 'Kitchen_Board_01',
+  name: 'Kitchen Board 01',
+  picture: ''
+} as const;
+
+export const MISC_KITCHEN_BOARD_02: Item = {
+  id: 'Kitchen_Board_02',
+  name: 'Kitchen Board 02',
+  picture: ''
+} as const;
+
+export const MISC_KNIFE_SHEATH: Item = {
+  id: 'Knife_Sheath',
+  name: 'Knife Sheath',
+  picture: ''
+} as const;
+
+export const MISC_LONG_PADDLE: Item = {
+  id: 'Long_Paddle',
+  name: 'Long Paddle',
+  picture: ''
+} as const;
+
+export const MISC_OIL_FILTER: Item = {
+  id: 'Oil_Filter',
+  name: 'Oil Filter',
+  picture: ''
+} as const;
+
+export const MISC_ORGANIC_FERTILIZER: Item = {
+  id: 'Organic_Fertilizer',
+  name: 'Organic Fertilizer',
+  picture: ''
+} as const;
+
+export const MISC_PESTICIDE: Item = {
+  id: 'Pesticide',
+  name: 'Pesticide',
+  picture: ''
+} as const;
+
+export const MISC_PINE_CONE: Item = {
+  id: 'Pine_Cone',
+  name: 'Pine Cone',
+  picture: ''
+} as const;
+
+export const MISC_PORTABLE_WORK_LIGHT: Item = {
+  id: 'PortableWorkLight',
+  name: 'Portable Work Light',
+  picture: ''
+} as const;
+
+export const MISC_PREMIUM_BOILIES_PACK: Item = {
+  id: 'PremiumBoiliesPack',
+  name: 'Premium Boilies Pack',
+  picture: ''
+} as const;
+
+export const MISC_PUPPET_EYE: Item = {
+  id: 'Puppet_Eye',
+  name: 'Puppet Eye',
+  picture: ''
+} as const;
+
+export const MISC_RADIO: Item = {
+  id: 'Radio',
+  name: 'Radio',
+  picture: ''
+} as const;
+
+export const MISC_RAZOR_BLADE: Item = {
+  id: 'Razor_Blade',
+  name: 'Razor Blade',
+  picture: ''
+} as const;
+
+export const MISC_LIGHTER: Item = {
+  id: 'Lighter',
+  name: 'Lighter',
+  picture: ''
+} as const;
+
+export const MISC_MATCHES: Item = {
+  id: 'Matches',
+  name: 'Matches',
+  picture: ''
+} as const;
+
+export const MISC_FLINT_AND_STEEL: Item = {
+  id: 'Flint_And_Steel',
+  name: 'Flint And Steel',
+  picture: ''
+} as const;
+
+export const MISC_RESPIRATOR_01: Item = {
+  id: 'Respirator_01',
+  name: 'Respirator 01',
+  picture: ''
+} as const;
+
+export const MISC_RUBBER_SHEET: Item = {
+  id: 'Rubber_sheet',
+  name: 'Rubber Sheet',
+  picture: ''
+} as const;
+
+export const MISC_SAFETY_PIN: Item = {
+  id: 'Safety_pin',
+  name: 'Safety Pin',
+  picture: ''
+} as const;
+
+export const MISC_SCISSORS_01: Item = {
+  id: 'Scissors_01',
+  name: 'Scissors 01',
+  picture: ''
+} as const;
+
+export const MISC_SCISSORS_PLASTIC: Item = {
+  id: 'Scissors_Plastic',
+  name: 'Scissors Plastic',
+  picture: ''
+} as const;
+
+export const MISC_SHORT_PADDLE_A: Item = {
+  id: 'Short_Paddle_A',
+  name: 'Short Paddle A',
+  picture: ''
+} as const;
+
+export const MISC_SHORT_PADDLE_B: Item = {
+  id: 'Short_Paddle_B',
+  name: 'Short Paddle B',
+  picture: ''
+} as const;
+
+export const MISC_SMALL_OXYGEN_BOTTLE_01: Item = {
+  id: 'Small_Oxygen_Bottle_01',
+  name: 'Small Oxygen Bottle 01',
+  picture: ''
+} as const;
+
+export const MISC_THERMOMETER: Item = {
+  id: 'Thermometer',
+  name: 'Thermometer',
+  picture: ''
+} as const;
+
+export const MISC_WATERING_CAN: Item = {
+  id: 'Watering_Can',
+  name: 'Watering Can',
+  picture: ''
+} as const;
+
+export const MISC_WINE_CORK: Item = {
+  id: 'WineCork',
+  name: 'Wine Cork',
+  picture: ''
+} as const;
+
+export const MISC_WIRE_CUTTERS_01: Item = {
+  id: 'Wire_Cutters_01',
+  name: 'Wire Cutters 01',
+  picture: ''
+} as const;
