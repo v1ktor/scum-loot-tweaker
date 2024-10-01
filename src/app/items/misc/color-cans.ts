903a6f8456ba38a3291ca43bfec3d0767d8b45b6
Viktor Kalinin <kalinin.viktor@gmail.com>
2024-10-01 05:00:27 +0300

    Add color cans to the items dropdown

---
+++ src/app/items/misc/color-cans.ts
@@ -0,0 +1,205 @@
+import { Item } from "../item.ts";
+
+export const COLOR_CAN_BARON_BROWN: Item = {
+  id: 'ColorCan_BaronBrown',
+  name: 'Color Can Baron Brown',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_BLACK: Item = {
+  id: 'ColorCan_Black',
+  name: 'Color Can Black',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_COLD_CYAN: Item = {
+  id: 'ColorCan_ColdCyan',
+  name: 'Color Can Cold Cyan',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_COLD_GREY: Item = {
+  id: 'ColorCan_ColdGrey',
+  name: 'Color Can Cold Grey',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_DARK_BROWN: Item = {
+  id: 'ColorCan_DarkBrown',
+  name: 'Color Can Dark Brown',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_DARK_GREEN: Item = {
+  id: 'ColorCan_DarkGreen',
+  name: 'Color Can Dark Green',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_DARK_GREY: Item = {
+  id: 'ColorCan_DarkGrey',
+  name: 'Color Can Dark Grey',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_EARTH_GREEN: Item = {
+  id: 'ColorCan_EarthGreen',
+  name: 'Color Can Earth Green',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_EARTH_GREY: Item = {
+  id: 'ColorCan_EarthGrey',
+  name: 'Color Can Earth Grey',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_EARTH_RED: Item = {
+  id: 'ColorCan_EarthRed',
+  name: 'Color Can Earth Red',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_EARTH_YELLOW: Item = {
+  id: 'ColorCan_EarthYellow',
+  name: 'Color Can Earth Yellow',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_FLAT_BROWN: Item = {
+  id: 'ColorCan_FlatBrown',
+  name: 'Color Can Flat Brown',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_FLAT_GREY: Item = {
+  id: 'ColorCan_FlatGrey',
+  name: 'Color Can Flat Grey',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_FLAT_WHITE: Item = {
+  id: 'ColorCan_FlatWhite',
+  name: 'Color Can Flat White',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_FOREST_GREEN: Item = {
+  id: 'ColorCan_ForestGreen',
+  name: 'Color Can Forest Green',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_GALAXY_BLUE: Item = {
+  id: 'ColorCan_GalaxyBlue',
+  name: 'Color Can Galaxy Blue',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_GALAXY_GREY: Item = {
+  id: 'ColorCan_GalaxyGrey',
+  name: 'Color Can Galaxy Grey',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_LIGHT_BROWN: Item = {
+  id: 'ColorCan_LightBrown',
+  name: 'Color Can Light Brown',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_LIME_GREEN: Item = {
+  id: 'ColorCan_LimeGreen',
+  name: 'Color Can Lime Green',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_NAVY_BLUE: Item = {
+  id: 'ColorCan_NavyBlue',
+  name: 'Color Can Navy Blue',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_NAVY_GREY: Item = {
+  id: 'ColorCan_NavyGrey',
+  name: 'Color Can Navy Grey',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_SNOW_WHITE: Item = {
+  id: 'ColorCan_SnowWhite',
+  name: 'Color Can Snow White',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_VELVET_BROWN: Item = {
+  id: 'ColorCan_VelvetBrown',
+  name: 'Color Can Velvet Brown',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_AMBER_YELLOW: Item = {
+  id: 'ColorCan_AmberYellow',
+  name: 'Color Can Amber Yellow',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_CHERRY_RED: Item = {
+  id: 'ColorCan_CherryRed',
+  name: 'Color Can Cherry Red',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_COTTON_CANDY: Item = {
+  id: 'ColorCan_CottonCandy',
+  name: 'Color Can Cotton Candy',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_DARK_URBAN: Item = {
+  id: 'ColorCan_DarkUrban',
+  name: 'Color Can Dark Urban',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_DEEP_ORANGE: Item = {
+  id: 'ColorCan_DeepOrange',
+  name: 'Color Can Deep Orange',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_FIRE_ENGINE_RED: Item = {
+  id: 'ColorCan_FireEngineRed',
+  name: 'Color Can Fire Engine Red',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_HAZY_PURPLE: Item = {
+  id: 'ColorCan_HazyPurple',
+  name: 'Color Can Hazy Purple',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_LIGHT_URBAN: Item = {
+  id: 'ColorCan_LightUrban',
+  name: 'Color Can Light Urban',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_POLICE_BLUE: Item = {
+  id: 'ColorCan_PoliceBlue',
+  name: 'Color Can Police Blue',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_RED_CHOCOLATE: Item = {
+  id: 'ColorCan_RedChocolate',
+  name: 'Color Can Red Chocolate',
+  picture: ''
+} as const;
+
+export const COLOR_CAN_WARM_GREY: Item = {
+  id: 'ColorCan_WarmGrey',
+  name: 'Color Can Warm Grey',
+  picture: ''
+} as const;
